3b4c7d3726aa8eb30a925746de83d792152e4dd4
catherine meng <cmeng@catherines-MacBook-Pro.local>
2022-07-07 12:39:57 -0700

    fix: fix frontend test; remove backend comment; added token to pr open pipeline

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -14,9 +14,6 @@ async function bootstrap() {
       if (!origin || whitelist.indexOf(origin) !== -1) {
         callback(null, true);
       }
-      // else {
-      //   callback(new Error('Not allowed by CORS'));
-      // }
     },
   });
 
