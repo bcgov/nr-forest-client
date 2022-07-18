1ddc601c3ba03b5ca162921f4c10780bbd4f6026
catherine meng <cmeng@catherines-MacBook-Pro.local>
2022-07-18 12:19:48 -0700

    feat/pipeline: update project abbreviation name; temporary disabled oracle connection in backend

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -8,18 +8,18 @@ import { ClientPublicModule } from './clientpublic/clientPublic.module';
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    TypeOrmModule.forRoot({
-      type: 'oracle',
-      host: process.env.ORACLEDB_HOST || 'localhost',
-      port: 1521,
-      serviceName: process.env.ORACLEDB_SERVICENAME,
-      database: process.env.ORACLEDB_DATABASE || 'postgres',
-      username: process.env.ORACLEDB_USER || 'postgres',
-      password: process.env.ORACLEDB_PASSWORD,
-      autoLoadEntities: true, // Auto load all entities regiestered by typeorm forFeature method.
-      synchronize: false, // This changes the DB schema to match changes to entities, which we might not want.
-    }),
-    ClientPublicModule,
+    // TypeOrmModule.forRoot({
+    //   type: 'oracle',
+    //   host: process.env.ORACLEDB_HOST || 'localhost',
+    //   port: 1521,
+    //   serviceName: process.env.ORACLEDB_SERVICENAME,
+    //   database: process.env.ORACLEDB_DATABASE || 'postgres',
+    //   username: process.env.ORACLEDB_USER || 'postgres',
+    //   password: process.env.ORACLEDB_PASSWORD,
+    //   autoLoadEntities: true, // Auto load all entities regiestered by typeorm forFeature method.
+    //   synchronize: false, // This changes the DB schema to match changes to entities, which we might not want.
+    // }),
+    // ClientPublicModule,
   ],
   controllers: [AppController],
   providers: [AppService],
