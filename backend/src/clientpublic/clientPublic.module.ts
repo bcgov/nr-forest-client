8deccf87c1367b9ed4c2b0f057958cad605b5e15
catherine meng <cmeng@catherines-MacBook-Pro.local>
2022-07-19 11:58:45 -0700

    fix/backend: added microservice package, test new sqlnet file, and tcp connection, refs: #10

--- backend/src/clientpublic/clientPublic.module.ts
+++ backend/src/clientpublic/clientPublic.module.ts
@@ -1,11 +1,17 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+// import { Transport, ClientsModule } from '@nestjs/microservices';
 import { ClientPublicController } from './controllers/clientPublic.controller';
 import { ClientPublicEntity } from './entities/clientPublic.entity';
 import { ClientPublicService } from './services/clientPublic.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([ClientPublicEntity])],
+  imports: [
+    TypeOrmModule.forFeature([ClientPublicEntity]),
+    // ClientsModule.register([
+    //   { name: 'CLIENTPUBLIC_MICROSERVICE', transport: Transport.TCP },
+    // ]),
+  ],
   controllers: [ClientPublicController],
   providers: [ClientPublicService],
 })
