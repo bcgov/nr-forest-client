820c53ff4d2549b200373e28d688dc73e975c874
CATHERINE MENG <catherine@CATHERINEs-MacBook-Pro.local>
2022-08-04 14:43:58 -0700

    cleanup/backend: clean up code to only have oracle connection

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,7 +3,6 @@ import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ClientPublicModule } from './clientpublic/clientPublic.module';
 import { ClientPublicViewModule } from './clientpublicview/clientPublicView.module';
 
 @Module({
@@ -15,25 +14,14 @@ import { ClientPublicViewModule } from './clientpublicview/clientPublicView.modu
       port: Number(process.env.ORACLEDB_PORT || 1521),
       serviceName: process.env.ORACLEDB_SERVICENAME,
       //NOTE: Uncomment this out when deploying to OS
-      //connectString: `(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCPS)(HOST=nrcdb03.bcgov)(PORT=1543))) (CONNECT_DATA=(SERVER=DEDICATED)(SERVICE_NAME=FORTMP1.nrs.bcgov)))`,
-      database: process.env.ORACLEDB_DATABASE || 'postgres',
-      username: process.env.ORACLEDB_USER || 'postgres',
+      connectString: `(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCPS)(HOST=nrcdb03.bcgov)(PORT=1543))) (CONNECT_DATA=(SERVER=DEDICATED)(SERVICE_NAME=FORTMP1.nrs.bcgov)))`,
+      database: process.env.ORACLEDB_DATABASE || 'oracle',
+      username: process.env.ORACLEDB_USER || 'oracle',
       password: process.env.ORACLEDB_PASSWORD,
       autoLoadEntities: true, // Auto load all entities registered by typeorm forFeature method.
       synchronize: false, // This changes the DB schema to match changes to entities, which we might not want.
     }),
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.POSTGRESQL_HOST || 'localhost',
-      port: 5432,
-      database: process.env.POSTGRESQL_DATABASE || 'postgres',
-      username: process.env.POSTGRESQL_USER || 'postgres',
-      password: process.env.POSTGRESQL_PASSWORD,
-      autoLoadEntities: true,
-      synchronize: false,
-    }),
     ClientPublicViewModule,
-    ClientPublicModule,
   ],
   controllers: [AppController],
   providers: [AppService],
