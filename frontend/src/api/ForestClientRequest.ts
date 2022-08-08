6343307a5ea5563987afec198e002433c374dd1e
catherine meng <cmeng@catherines-MacBook-Pro.local>
2022-08-08 13:06:52 -0700

    fix/frontend: update frontend to use the latest api call, add the api example for search by client number, refs: #12

--- frontend/src/api/ForestClientRequest.ts
+++ frontend/src/api/ForestClientRequest.ts
@@ -1,20 +1,16 @@
 import axios from "axios";
 import { backendUrl } from "../core/CoreConstants";
 
-export const searchClientApi = (clientNumber?: string, clientName?: string) => {
-  let params = {};
-  if (clientNumber && clientNumber !== "") params = { clientNumber };
-  if (clientName && clientName !== "") params = { ...params, clientName };
-
+export const searchByClientNumberApi = (clientNumber: string) => {
   return axios
-    .get(backendUrl + "/clientView/findInViewBy", {
-      params,
+    .get(backendUrl + "/clientView/findInViewByNumber", {
+      params: { clientNumber },
     })
     .then((response) => {
       return response.data;
     })
     .catch((e) => {
-      console.log("failed to search client", e);
+      console.log("failed to search client by client number", e);
       return e;
     });
 };
--- frontend/src/pages/SearchPage.vue
+++ frontend/src/pages/SearchPage.vue
@@ -1,10 +1,25 @@
 <template>
-  <div style="margin: 40px; text-align: left; display: flex">
-    <div style="width: 48%">
-      <h5 style="margin-bottom: 20px">
-        Enter the client name or the client number to search:
-      </h5>
-      <FormInput
+  <SearchTemplate
+    :buttonDisabled="numberSearchDisabled"
+    title="Enter the client number to search:"
+    :loading="numberResultLoading"
+    :result="numberResult"
+    @onSearch="searchClientNumber"
+  >
+    <FormInput
+      :inputValue="clientNumber"
+      :fieldProps="clientNumberProps"
+      @updateFormData="
+        (id, newValue) => {
+          clientNumber = newValue;
+          if (newValue && newValue !== '') numberSearchDisabled = false;
+          else numberSearchDisabled = true;
+        }
+      "
+    />
+  </SearchTemplate>
+
+  <!-- <FormInput
         :inputValue="clientName"
         :fieldProps="clientNameProps"
         @updateFormData="
@@ -15,68 +30,46 @@
               buttonDisabled = true;
           }
         "
-      />
-      <FormInput
-        :inputValue="clientNumber"
-        :fieldProps="clientNumberProps"
-        @updateFormData="
-          (id, newValue) => {
-            clientNumber = newValue;
-            if (newValue && newValue !== '') buttonDisabled = false;
-            else if (clientName == '' && newValue == '') buttonDisabled = true;
-          }
-        "
-      />
-      <b-button
-        variant="primary"
-        :style="'background-color:' + primary + ';margin-top: 20px'"
-        :disabled="buttonDisabled"
-        @click="searchClient()"
-      >
-        Search
-      </b-button>
-    </div>
-    <div style="width: 48%; margin-left: 4%">
-      <h5 style="margin-bottom: 20px">Search Result:</h5>
-      <b-spinner v-if="loading" variant="primary"></b-spinner>
-      <b-card style="height: 100%">{{ result }}</b-card>
-    </div>
-  </div>
+      /> -->
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 import FormInput from "../common/FormInput.vue";
-import { searchClientApi } from "../api/ForestClientRequest";
-import { FormFieldTemplateType } from "../core/AppType";
+import SearchTemplate from "../common/SearchTemplate.vue";
+import { searchByClientNumberApi } from "../api/ForestClientRequest";
+import type { FormFieldTemplateType } from "../core/AppType";
 import { primary } from "../utils/color";
 
 export default defineComponent({
   components: {
     FormInput,
+    SearchTemplate,
   },
   data() {
     return {
       primary,
-      clientName: "" as string,
-      clientNameProps: {
-        label: "Client Name",
-      } as FormFieldTemplateType,
       clientNumber: "" as string,
       clientNumberProps: {
         label: "Client Number",
+        required: true,
       } as FormFieldTemplateType,
-      result: "" as string,
-      buttonDisabled: true as boolean,
-      loading: false as boolean,
+      numberResult: "" as string,
+      numberSearchDisabled: true as boolean,
+      numberResultLoading: false as boolean,
+
+      // clientName: "" as string,
+      // clientNameProps: {
+      //   label: "Client Name",
+      // } as FormFieldTemplateType,
     };
   },
   methods: {
-    searchClient() {
-      this.loading = true;
-      searchClientApi(this.clientNumber, this.clientName).then((response) => {
-        this.result = response;
-        this.loading = false;
+    searchClientNumber() {
+      this.numberResultLoading = true;
+      searchByClientNumberApi(this.clientNumber).then((response) => {
+        this.numberResult = response;
+        this.numberResultLoading = false;
       });
     },
   },
