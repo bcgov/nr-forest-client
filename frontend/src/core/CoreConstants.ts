fd886cf4778acebec699a7c3433f92da5a5e3b56
catherine meng <cmeng@catherines-MacBook-Pro.local>
2022-08-19 12:04:53 -0700

    feat/pipeline: config api services portal in pipeline, refs: #18

--- frontend/src/core/CoreConstants.ts
+++ frontend/src/core/CoreConstants.ts
@@ -1,5 +1,8 @@
 export const backendUrl =
   config.VITE_BACKEND_URL || import.meta.env.VITE_BACKEND_URL;
 
+export const apiKey =
+  config.VITE_X_API_KEY || import.meta.env.VITE_X_API_KEY || "";
+
 export const maxFileSizePerFile = 1000000 * 20; //20 mb
 export const maxTotalFileSize = 1000000 * 20 * 5; //100 mb
