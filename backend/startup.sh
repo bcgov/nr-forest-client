df739969a8c1fc44458202cda5cf696d0a723e66
maria.martinez@gov.bc.ca <maria.martinez@gov.bc.ca>
2022-11-01 17:45:52 -0700

    Renamed accordingly

--- backend/startup.sh
+++ backend/startup.sh
@@ -4,4 +4,4 @@ java --source 17 InstallCert.java --quiet "${ORACLEDB_HOST}:${ORACLEDB_PORT}"
 keytool -exportcert -alias "${ORACLEDB_HOST}-1" -keystore jssecacerts -storepass changeit -file oracle.cer
 keytool -importcert -alias orakey -noprompt -cacerts -storepass changeit -file oracle.cer
 
-java -jar target/nr-forest-client-api.jar
+java -jar target/nr-forest-client.jar
