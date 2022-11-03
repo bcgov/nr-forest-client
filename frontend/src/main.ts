6ed7c7f2ad56f0721297695b27fe6a7e68589669
catherine meng <mcatherine1994@gmail.com>
2022-11-03 15:47:50 -0700

    test/frontend: test frontend runtime env

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -5,7 +5,7 @@ import { BootstrapIconsPlugin } from "bootstrap-icons-vue";
 import VueKeycloakJs from "@dsb-norge/vue-keycloak-js";
 import type { KeycloakInstance } from "keycloak-js";
 import type { VueKeycloakInstance } from "@dsb-norge/vue-keycloak-js/dist/types";
-import { keycloakUrl, keycloakClientId, nodeEnv } from "./core/EnvConstants";
+import { keycloakUrl, keycloakClientId, nodeEnv } from "./core/CoreConstants";
 
 // Import Bootstrap an BootstrapVue CSS files (order is important)
 import "bootstrap/dist/css/bootstrap.css";
@@ -13,7 +13,7 @@ import "bootstrap-vue/dist/bootstrap-vue.css";
 
 const app = createApp(App);
 
-console.log('nodeEnv', nodeEnv);
+console.log("nodeEnv", nodeEnv);
 
 if (nodeEnv && nodeEnv == "openshift-dev") {
   // disable the login authentication for the deployment in the openshift dev namespace
