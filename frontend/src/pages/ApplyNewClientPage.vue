40f476c83a82140d001a98e246e9dbd54429d083
catherine meng <mcatherine1994@gmail.com>
2022-11-07 16:03:22 -0800

    feat/frontend: remove unused package, added modal component, refs: #55

--- frontend/src/pages/ApplyNewClientPage.vue
+++ frontend/src/pages/ApplyNewClientPage.vue
@@ -7,26 +7,35 @@
       :value="emailValue"
       @updateValue="updateEmailValue"
     />
+
     <b-button
       variant="primary"
       :style="'background-color:' + primary + ';margin-top: 24px'"
-      @click="sendEmail()"
+      @click="openModal()"
     >
       Submit
     </b-button>
+
+    <ConfirmModal
+      :value="modalShow"
+      okayText="Submit"
+      @onOkay="onModalOkay()"
+      @onCancel="onModalCancel()"
+    />
   </div>
 </template>
 
 <script setup lang="ts">
+// composition api
 import { ref } from "vue";
 import FormInput from "../common/FormInput.vue";
 import SubmitFailText from "../containers/SubmitFailText.vue";
 import SubmitSucessText from "../containers/SubmitSucessText.vue";
+import ConfirmModal from "../common/ConfirmModal.vue";
 import { sendConfirmationEmail } from "../services/forestClient.service";
 import type { FormFieldTemplateType } from "../core/AppType";
 import { primary } from "../utils/color";
 
-// composition api
 const success = ref(false);
 const error = ref(false);
 
@@ -34,16 +43,19 @@ const emailInputProps: FormFieldTemplateType = {
   label: "Email Address",
   id: "test-email",
 };
-
 const emailValue = ref("");
-
 const updateEmailValue = (id: string, newValue: string) => {
   if (success) success.value = false;
   if (error) error.value = false;
   emailValue.value = newValue;
 };
 
-const sendEmail = () => {
+const modalShow = ref(false);
+const openModal = () => {
+  modalShow.value = true;
+};
+const onModalOkay = () => {
+  modalShow.value = false;
   sendConfirmationEmail(emailValue.value, "Hello World!").then((response) => {
     if (response.status == 200) {
       if (error) error.value = false;
@@ -53,6 +65,9 @@ const sendEmail = () => {
       error.value = true;
     }
   });
+};
+const onModalCancel = () => {
+  modalShow.value = false;
 };
 </script>
 
