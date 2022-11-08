42041df2cf24359bae957c449a7c5d5d5f15c99a
maria.martinez@gov.bc.ca <maria.martinez@gov.bc.ca>
2022-11-08 15:53:56 -0800

    Removed unneeded classes

--- backend/src/main/java/ca/bc/gov/app/m/postgres/client/repository/ClientTypeCodeRepository.java
+++ backend/src/main/java/ca/bc/gov/app/m/postgres/client/repository/ClientTypeCodeRepository.java
@@ -3,8 +3,9 @@ package ca.bc.gov.app.m.postgres.client.repository;
 import org.springframework.stereotype.Repository;
 
 import ca.bc.gov.app.core.repository.CoreRepository;
-import ca.bc.gov.app.m.postgres.client.entity.ClientStatusCodeEntity;
+import ca.bc.gov.app.m.postgres.client.entity.ClientTypeCodeEntity;
 
 @Repository
-public interface ClientStatusCodeRepository extends CoreRepository<ClientStatusCodeEntity> {
+public interface ClientTypeCodeRepository extends CoreRepository<ClientTypeCodeEntity> {
+
 }
