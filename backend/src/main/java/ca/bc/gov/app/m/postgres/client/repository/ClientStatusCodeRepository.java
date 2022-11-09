38b55d8b43ed66bd7539f3b3541a101f78cddd33
maria.martinez@gov.bc.ca <maria.martinez@gov.bc.ca>
2022-11-09 12:01:03 -0800

    Revert "Removed unneeded classes"
    This reverts commit 42041df2cf24359bae957c449a7c5d5d5f15c99a.

--- backend/src/main/java/ca/bc/gov/app/m/postgres/client/repository/ClientStatusCodeRepository.java
+++ backend/src/main/java/ca/bc/gov/app/m/postgres/client/repository/ClientStatusCodeRepository.java
@@ -3,9 +3,8 @@ package ca.bc.gov.app.m.postgres.client.repository;
 import org.springframework.stereotype.Repository;
 
 import ca.bc.gov.app.core.repository.CoreRepository;
-import ca.bc.gov.app.m.postgres.client.entity.ClientTypeCodeEntity;
+import ca.bc.gov.app.m.postgres.client.entity.ClientStatusCodeEntity;
 
 @Repository
-public interface ClientTypeCodeRepository extends CoreRepository<ClientTypeCodeEntity> {
-
+public interface ClientStatusCodeRepository extends CoreRepository<ClientStatusCodeEntity> {
 }
