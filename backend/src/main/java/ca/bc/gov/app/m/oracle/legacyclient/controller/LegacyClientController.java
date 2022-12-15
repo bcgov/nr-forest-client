7f16335429ffbe171829c56fa0c61d7e0c9de199
Paulo Gomes da Cruz Junior <pcruz@daitan.com>
2022-12-15 15:46:37 -0800

    chore: code cleanup

--- backend/src/main/java/ca/bc/gov/app/m/oracle/legacyclient/controller/LegacyClientController.java
+++ backend/src/main/java/ca/bc/gov/app/m/oracle/legacyclient/controller/LegacyClientController.java
@@ -11,7 +11,6 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
