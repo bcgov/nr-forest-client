7f161764d9e6a5e7aaa17327f59326b663486d1c
Paulo Gomes da Cruz Junior <paulo.cruz@encora.com>
2022-12-29 14:26:09 -0800

    ci(docker): updating dockerfile
    - changed dockerfile to use jre instead of jdk
    - removed oracle part of the process
    - updated ci to build image before release

--- backend/startup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-java --source 17 InstallCert.java --quiet "${ORACLEDB_HOST}:${ORACLEDB_PORT}"
-keytool -exportcert -alias "${ORACLEDB_HOST}-1" -keystore jssecacerts -storepass changeit -file oracle.cer
-keytool -importcert -alias orakey -noprompt -cacerts -storepass changeit -file oracle.cer
-
-
-java -cp /app:/app/lib/* ca.bc.gov.app.BootApplication
