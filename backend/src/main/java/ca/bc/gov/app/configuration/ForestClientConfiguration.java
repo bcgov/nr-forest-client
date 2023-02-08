e70665b11359aeddcbf4461bef52839360d9abae
Paulo Gomes da Cruz Junior <paulo.cruz@encora.com>
2023-02-08 13:26:36 -0800

    chore: fixing cors issue
    - added cors parameters to yml
    - merged configuration files

---
+++ backend/src/main/java/ca/bc/gov/app/configuration/ForestClientConfiguration.java
@@ -0,0 +1,73 @@
+package ca.bc.gov.app.configuration;
+
+import java.time.Duration;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Component
+@ConfigurationProperties("ca.bc.gov.nrs")
+public class ForestClientConfiguration {
+
+  private ChesConfiguration ches;
+  private FrontEndConfiguration frontend;
+  private OpenMapsConfiguration openmaps;
+  private OrgBookConfiguration orgbook;
+
+  @Data
+  @Builder
+  @NoArgsConstructor
+  @AllArgsConstructor
+  public static class ChesConfiguration {
+    private String uri;
+    private String tokenUrl;
+    private String clientId;
+    private String clientSecret;
+    private String scope;
+  }
+
+  @Data
+  @Builder
+  @NoArgsConstructor
+  @AllArgsConstructor
+  public static class FrontEndConfiguration {
+    private String url;
+    private FrontEndCorsConfiguration cors;
+
+  }
+
+  @Data
+  @Builder
+  @NoArgsConstructor
+  @AllArgsConstructor
+  public static class FrontEndCorsConfiguration {
+    private List<String> headers;
+    private List<String> methods;
+    private Duration age;
+  }
+
+
+  @Data
+  @Builder
+  @NoArgsConstructor
+  @AllArgsConstructor
+  public static class OpenMapsConfiguration {
+    private String uri;
+  }
+
+  @Data
+  @Builder
+  @NoArgsConstructor
+  @AllArgsConstructor
+  public static class OrgBookConfiguration {
+    private String uri;
+  }
+}
--- backend/src/main/java/ca/bc/gov/app/configuration/GlobalServiceConfiguration.java
+++ backend/src/main/java/ca/bc/gov/app/configuration/GlobalServiceConfiguration.java
@@ -1,25 +1,67 @@
 package ca.bc.gov.app.configuration;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.web.cors.reactive.CorsUtils;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.server.WebFilter;
+import org.springframework.web.server.WebFilterChain;
+import reactor.core.publisher.Mono;
 
 @Configuration
+@Slf4j
 public class GlobalServiceConfiguration {
 
   @Bean
-  public WebClient orgBookApi(OrgBookConfiguration configuration) {
-    return WebClient.builder().baseUrl(configuration.getUri()).build();
+  public WebClient orgBookApi(ForestClientConfiguration configuration) {
+    return WebClient.builder().baseUrl(configuration.getOpenmaps().getUri()).build();
   }
 
   @Bean
-  public WebClient chesApi(ChesConfiguration configuration) {
-    return WebClient.builder().baseUrl(configuration.getUri()).build();
+  public WebClient chesApi(ForestClientConfiguration configuration) {
+    return WebClient.builder().baseUrl(configuration.getChes().getUri()).build();
   }
 
   @Bean
-  public WebClient openMapsApi(OpenMapsConfiguration configuration) {
-    return WebClient.builder().baseUrl(configuration.getUri()).build();
+  public WebClient openMapsApi(ForestClientConfiguration configuration) {
+    return WebClient.builder().baseUrl(configuration.getOpenmaps().getUri()).build();
+  }
+
+  @Bean
+  public WebFilter corsFilter(ForestClientConfiguration configuration) {
+    return (ServerWebExchange ctx, WebFilterChain chain) -> {
+
+      ServerHttpRequest request = ctx.getRequest();
+
+      if (CorsUtils.isCorsRequest(request)) {
+        ServerHttpResponse response = ctx.getResponse();
+        HttpHeaders headers = response.getHeaders();
+        headers.add("Access-Control-Allow-Origin", configuration.getFrontend().getUrl());
+        headers.add("Access-Control-Max-Age",
+            String.valueOf(configuration.getFrontend().getCors().getAge().getSeconds())
+        );
+        headers.add("Access-Control-Allow-Methods",
+            String.join(",", configuration.getFrontend().getCors().getMethods())
+        );
+        headers.add("Access-Control-Allow-Headers",
+            String.join(",", configuration.getFrontend().getCors().getHeaders())
+        );
+
+        if (request.getMethod().equals(HttpMethod.OPTIONS)) {
+          response.setStatusCode(HttpStatus.OK);
+          return Mono.empty();
+        }
+      }
+      return chain.filter(ctx);
+
+    };
   }
 
 }
