ea8dc7f550ca15e4d1e4262bd7786c94805b5e82
Maria Martinez <maria.martinez@gov.bc.ca>
2023-02-14 11:54:58 -0800

    feat(fe):
    - Added Countries from backend
    - Cleaned code

--- frontend/src/pages/applynewclient/formvalidationrules/RequiredFields.ts
+++ frontend/src/pages/applynewclient/formvalidationrules/RequiredFields.ts
@@ -2,19 +2,8 @@ import type { FormValidationRequiredFieldType } from "../../../core/FormType";
 
 /* --------------- Required Fields --------------------- */
 // todo: so far it supports at most 2 level in-depth array, if the depth increase, need to redesign the structure
-export const commonRequiredFields: Array<FormValidationRequiredFieldType> = [
-  { path: "businessType.clientType" },
-  { path: "location.address", subFieldModelName: "streetAddress" },
-  { path: "location.address", subFieldModelName: "country" },
-  { path: "location.address", subFieldModelName: "province" },
-  { path: "location.address", subFieldModelName: "city" },
-  { path: "location.address", subFieldModelName: "postalCode" },
-];
+export const commonRequiredFields: Array<FormValidationRequiredFieldType> = [];
 
-export const businessRequiredFields: Array<FormValidationRequiredFieldType> = [
-  { path: "information.businessName" },
-  { path: "information.registrationNumber" },
-];
+export const businessRequiredFields: Array<FormValidationRequiredFieldType> = [];
 
-export const individualRequiredFields: Array<FormValidationRequiredFieldType> =
-  [{ path: "information.firstName" }, { path: "information.birthdate" }];
+export const individualRequiredFields: Array<FormValidationRequiredFieldType> = [];
