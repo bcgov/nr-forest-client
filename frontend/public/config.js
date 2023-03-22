b9007fa1ec9c7b939231d72bf3837fca4eae9a38
Paulo Gomes da Cruz Junior <paulo.cruz@encora.com>
2023-03-22 15:41:03 -0700

    fix: fixing folder for extra config

--- frontend/public/config.js
+++
@@ -1,7 +0,0 @@
-// This file is provided as an option to support runtime loading of configuration parameters in OpenShift
-// Development builds and production builds using yarn build will still work using .env files
-
-// window.localStorage.setItem("VITE_KEYCLOAK_URL", "");
-// window.localStorage.setItem("VITE_KEYCLOAK_CLIENT_ID", "");
-// window.localStorage.setItem("VITE_NODE_ENV", "");
-// window.localStorage.setItem("VITE_BACKEND_URL", "");
