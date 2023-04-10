a91a3a94fe9057af237f3774450c02f5af2f8e31
Paulo Gomes da Cruz Junior <paulo.cruz@encora.com>
2023-04-10 09:44:22 -0700

    chore: testing removal of log4j

--- legacy/src/main/java/ca/bc/gov/app/LegacyApplication.java
+++ legacy/src/main/java/ca/bc/gov/app/LegacyApplication.java
@@ -3,6 +3,7 @@ package ca.bc.gov.app;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.servers.Server;
+import org.slf4j.bridge.SLF4JBridgeHandler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -21,6 +22,8 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class LegacyApplication {
 
   public static void main(String[] args) {
+    SLF4JBridgeHandler.removeHandlersForRootLogger();
+    SLF4JBridgeHandler.install();
     SpringApplication.run(LegacyApplication.class, args);
   }
 
