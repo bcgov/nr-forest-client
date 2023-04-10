bc31b7a81ed9e1ef052de194372a26cd1c094ae6
Paulo Gomes da Cruz Junior <paulo.cruz@encora.com>
2023-04-10 14:59:56 -0700

    feat: changing lib from poi to fastexcel

--- legacy/src/main/java/ca/bc/gov/app/util/SheetWriter.java
+++ legacy/src/main/java/ca/bc/gov/app/util/SheetWriter.java
@@ -8,37 +8,21 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.util.concurrent.atomic.AtomicInteger;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.poi.ss.usermodel.BorderStyle;
-import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.CellStyle;
-import org.apache.poi.ss.usermodel.Font;
-import org.apache.poi.ss.usermodel.FormulaEvaluator;
-import org.apache.poi.ss.usermodel.HorizontalAlignment;
-import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.ss.util.CellRangeAddress;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.dhatim.fastexcel.Color;
+import org.dhatim.fastexcel.Workbook;
+import org.dhatim.fastexcel.Worksheet;
 
 @Slf4j
 public class SheetWriter {
 
+  public static final String BLACK_COLOR = "black";
   private final BufferedOutputStream fileOut;
   // Create a new workbook
-  private final XSSFWorkbook workbook;
+  private final Workbook workbook;
   //Create the main sheet
-  private final Sheet sheet;
+  private final Worksheet sheet;
   //This sets the initial line for the entries
   private final AtomicInteger rowCounter = new AtomicInteger(18);
-  //This evaluator is to execute all the formulas/functions
-  private final FormulaEvaluator evaluator;
-  //Setting all the available styles
-  private final CellStyle rightAlignment;
-  private final CellStyle rightPercentAlignment;
-  private final CellStyle leftAlignment;
-  private final CellStyle leftTitles;
-  private final CellStyle rightTitles;
-  private final CellStyle centerTitles;
-  private final Font boldFont;
 
   public SheetWriter(File sheetFile) {
 
@@ -50,86 +34,30 @@ public class SheetWriter {
       );
     }
 
-    this.workbook = new XSSFWorkbook();
-    this.sheet = workbook.createSheet("C an A");
+    this.workbook = new Workbook(this.fileOut, "SampleName", "1.0");
+    this.sheet = workbook.newWorksheet("C an A");
 
-    this.rightAlignment = workbook.createCellStyle();
-    this.rightPercentAlignment = workbook.createCellStyle();
-    this.leftAlignment = workbook.createCellStyle();
-    this.leftTitles = workbook.createCellStyle();
-    this.rightTitles = workbook.createCellStyle();
-    this.centerTitles = workbook.createCellStyle();
-    this.boldFont = workbook.createFont();
-    this.evaluator = workbook.getCreationHelper().createFormulaEvaluator();
 
-    initializeStyles();
-    initializeDataHeader();
   }
 
   private void initializeDataHeader() {
-    valueRow(17, centerTitles,
-        "Client Number",
-        "Registration Number",
-        "Found?",
-        "Active in OrgBook?",
-        "Same name?",
-        "Name in Client",
-        "Name in OB"
-    );
-    CellRangeAddress range = CellRangeAddress.valueOf("B18:G18");
-    sheet.setAutoFilter(range);
-  }
-
-  private void initializeStyles() {
-
-    boldFont.setBold(true);
-    rightAlignment.setAlignment(HorizontalAlignment.RIGHT);
-    rightAlignment.setBorderBottom(BorderStyle.THIN);
-    rightAlignment.setBorderTop(BorderStyle.THIN);
-    rightAlignment.setBorderLeft(BorderStyle.THIN);
-    rightAlignment.setBorderRight(BorderStyle.THIN);
-
-    rightPercentAlignment.setAlignment(HorizontalAlignment.RIGHT);
-    rightPercentAlignment.setBorderBottom(BorderStyle.THIN);
-    rightPercentAlignment.setBorderTop(BorderStyle.THIN);
-    rightPercentAlignment.setBorderLeft(BorderStyle.THIN);
-    rightPercentAlignment.setBorderRight(BorderStyle.THIN);
-    rightPercentAlignment.setDataFormat(workbook.createDataFormat().getFormat("0.00%"));
-
-
-    leftAlignment.setAlignment(HorizontalAlignment.LEFT);
-    leftAlignment.setBorderBottom(BorderStyle.THIN);
-    leftAlignment.setBorderTop(BorderStyle.THIN);
-    leftAlignment.setBorderLeft(BorderStyle.THIN);
-    leftAlignment.setBorderRight(BorderStyle.THIN);
-
-    centerTitles.setAlignment(HorizontalAlignment.CENTER);
-    centerTitles.setFont(boldFont);
-    centerTitles.setBorderBottom(BorderStyle.THIN);
-    centerTitles.setBorderTop(BorderStyle.THIN);
-    centerTitles.setBorderLeft(BorderStyle.THIN);
-    centerTitles.setBorderRight(BorderStyle.THIN);
-
-    leftTitles.setAlignment(HorizontalAlignment.LEFT);
-    leftTitles.setFont(boldFont);
-    leftTitles.setBorderBottom(BorderStyle.THIN);
-    leftTitles.setBorderTop(BorderStyle.THIN);
-    leftTitles.setBorderLeft(BorderStyle.THIN);
-    leftTitles.setBorderRight(BorderStyle.THIN);
-
-    rightTitles.setAlignment(HorizontalAlignment.RIGHT);
-    rightTitles.setFont(boldFont);
-    rightTitles.setBorderBottom(BorderStyle.THIN);
-    rightTitles.setBorderTop(BorderStyle.THIN);
-    rightTitles.setBorderLeft(BorderStyle.THIN);
-    rightTitles.setBorderRight(BorderStyle.THIN);
+    sheet
+        .range(17, 1, rowCounter.get() - 1, 7)
+        .createTable(
+            "Client Number",
+            "Registration Number",
+            "Found?",
+            "Active in OrgBook?",
+            "Same name?",
+            "Name in Client",
+            "Name in OB"
+        );
   }
 
   public void write(ClientPublicViewDto client) {
 
     valueRow(
         rowCounter.getAndIncrement(),
-        leftAlignment,
         client.clientNumber(),
         client.incorporationNumber(),
         client.found(),
@@ -138,23 +66,20 @@ public class SheetWriter {
         client.clientName(),
         client.orgBookName()
     );
-    try {
-      workbook.write(fileOut);
-    } catch (Exception e) {
-      e.printStackTrace();
-    }
   }
 
   public void complete() {
     generateCountData();
+    initializeDataHeader();
+    sheet.freezePane(8,18);
     close();
   }
 
   private void close() {
     try {
-      workbook.write(fileOut);
+      sheet.finish();
+      workbook.finish();
       fileOut.close();
-      workbook.close();
       log.debug("Saving and closing file");
     } catch (Exception e) {
       log.error("Error while closing spreadsheet", e);
@@ -168,10 +93,23 @@ public class SheetWriter {
     totalOrgBookActiveInfo();
     totalPartialInfo();
 
-    evaluator.evaluateAll();
-    for (int columnId = 0; columnId < 8; columnId++) {
-      sheet.autoSizeColumn(columnId);
-    }
+    sheet
+        .range(1, 1, 1, 4)
+        .style()
+        .bold()
+        .borderColor(BLACK_COLOR)
+        .borderStyle("thin")
+        .set();
+
+
+    sheet
+        .range(18, 1, rowCounter.get() - 1, 7)
+        .style()
+        .shadeAlternateRows(Color.GRAY2)
+        .borderColor(BLACK_COLOR)
+        .borderStyle("thin")
+        .set();
+
   }
 
   private void totalFoundInfo() {
@@ -191,6 +129,7 @@ public class SheetWriter {
         getCountIf("D", "NF"),
         getPercentage(7, 2)
     );
+
   }
 
   private void totalOrgBookActiveInfo() {
@@ -223,24 +162,32 @@ public class SheetWriter {
   }
 
   private void totalClientInfo() {
-    Row totalRow = sheet.createRow(1);
-    setMergedValue(totalRow, 1, "Total Clients", leftTitles);
-    setCellValue(totalRow, 4, String.valueOf(rowCounter.get() - 18), rightTitles);
+    setMergedValue(1, 1, "Total Clients");
+    setCellValue(1, 4, String.valueOf(rowCounter.get() - 18));
+    right(1, 4);
+    setBold(1, 1);
+    setBold(1, 4);
+    setBorder(1, 1);
+    setBorder(1, 4);
   }
 
   private void headerRow(int rowNumber) {
-    Row headerRow = sheet.createRow(rowNumber);
-    setCellValue(headerRow, 4, "# of Records", centerTitles);
-    setCellValue(headerRow, 5, "Percentage", centerTitles);
+    setCellValue(rowNumber, 4, "# of Records");
+    setBold(rowNumber, 4);
+    center(rowNumber, 4);
+    setBorder(rowNumber, 4);
+
+    setCellValue(rowNumber, 5, "Percentage");
+    setBold(rowNumber, 5);
+    center(rowNumber, 5);
+    setBorder(rowNumber, 5);
   }
 
-  private void valueRow(int rowNumber, CellStyle style, String... contents) {
-    Row valueRow = sheet.createRow(rowNumber);
-
+  private void valueRow(int rowNumber, String... contents) {
     for (int columnId = 1; columnId <= contents.length; columnId++) {
-      Cell cell = valueRow.createCell(columnId);
-      cell.setCellValue(contents[columnId - 1]);
-      cell.setCellStyle(style);
+      this.sheet.value(rowNumber, columnId, contents[columnId - 1]);
+      setBorder(rowNumber, columnId);
+      left(rowNumber, columnId);
     }
   }
 
@@ -250,45 +197,45 @@ public class SheetWriter {
       String contentRecords,
       String contentPercentage
   ) {
-    Row valueRow = sheet.createRow(rowNumber);
-    setMergedValue(valueRow, 1, contentTitle, leftAlignment);
-    setCellFormula(valueRow, 4, contentRecords, rightAlignment);
-    setCellFormula(valueRow, 5, contentPercentage, rightPercentAlignment);
+    setMergedValue(rowNumber, 1, contentTitle);
+
+    setCellFormula(rowNumber, 4, contentRecords);
+
+    setCellFormula(rowNumber, 5, contentPercentage);
+
+    sheet.style(rowNumber, 5).format("0.00%").set();
+
   }
 
-  private void setCellValue(Row row, int cellId, String cellValue, CellStyle style) {
-    setCell(row, cellId, cellValue, style, false);
+  private void setCellValue(int rowId, int cellId, String cellValue) {
+    setCell(rowId, cellId, cellValue, false);
   }
 
-  private void setCellFormula(Row row, int cellId, String cellValue, CellStyle style) {
-    setCell(row, cellId, cellValue, style, true);
+  private void setCellFormula(int rowId, int cellId, String cellValue) {
+    setCell(rowId, cellId, cellValue, true);
+    right(rowId, cellId);
+    setBorder(rowId, cellId);
   }
 
   private void setCell(
-      Row row,
+      int rowId,
       int cellId,
       String cellValue,
-      CellStyle style,
       boolean formula
   ) {
-    Cell cell = row.createCell(cellId);
     if (formula) {
-      cell.setCellFormula(cellValue);
+      sheet.formula(rowId, cellId, cellValue);
     } else {
-      cell.setCellValue(cellValue);
+      sheet.value(rowId, cellId, cellValue);
     }
-    cell.setCellStyle(style);
   }
 
-  private void setMergedValue(Row row, int cellId, String cellValue, CellStyle style) {
-    setCellValue(row, cellId, cellValue, style);
-    CellRangeAddress mergedRegion = new CellRangeAddress(
-        row.getRowNum(),
-        row.getRowNum(),
-        cellId,
-        cellId + 2
-    );
-    sheet.addMergedRegion(mergedRegion);
+  private void setMergedValue(int rowId, int cellId, String cellValue) {
+    setCellValue(rowId, cellId, cellValue);
+    sheet.range(rowId, cellId, rowId, cellId + 2).merge();
+    left(rowId, 1);
+    setBold(rowId, 1);
+    setBorder(rowId, 1, 3);
   }
 
   private String getPercentage(int columnValue, int columnTotal) {
@@ -303,4 +250,54 @@ public class SheetWriter {
     return String.format("COUNTIFS(%s19:%s%d,\"%s\",D19:D%d,\"%s\")", column, column,
         rowCounter.get(), value, rowCounter.get(), reference);
   }
+
+  private void setBorder(int rowId, int columnId) {
+    sheet
+        .style(rowId, columnId)
+        .borderColor(BLACK_COLOR)
+        .borderStyle("thin")
+        .set();
+  }
+
+  private void setBorder(int rowId, int columnId, int lastColumn) {
+    if (lastColumn == 0) {
+      setBorder(rowId, columnId);
+    } else {
+      sheet
+          .range(rowId, columnId, rowId, lastColumn)
+          .style()
+          .borderColor(BLACK_COLOR)
+          .borderStyle("thin")
+          .set();
+    }
+  }
+
+  private void setBold(int rowId, int columnId) {
+    sheet
+        .style(rowId, columnId)
+        .bold()
+        .set();
+  }
+
+  private void center(int rowId, int columnId) {
+    sheet
+        .style(rowId, columnId)
+        .horizontalAlignment("center")
+        .set();
+  }
+
+  private void left(int rowId, int columnId) {
+    sheet
+        .style(rowId, columnId)
+        .horizontalAlignment("left")
+        .set();
+  }
+
+  private void right(int rowId, int columnId) {
+    sheet
+        .style(rowId, columnId)
+        .horizontalAlignment("right")
+        .set();
+  }
+
 }
