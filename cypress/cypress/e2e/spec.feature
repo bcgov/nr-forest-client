4c3a8147d493f438d9642091026819f2bbfc9960
Paulo Gomes da Cruz Junior <paulo.cruz@encora.com>
2023-05-05 11:45:25 -0700

    feat(FSADT1-713): adding base cypress code

---
+++ cypress/cypress/e2e/spec.feature
@@ -0,0 +1,4 @@
+Feature: Form Screen
+  Scenario: Screen loads
+    Given I am on the form page
+    Then I can see the title
---
+++ cypress/cypress/e2e/spec.ts
@@ -0,0 +1,9 @@
+import { Then, Given } from "@badeball/cypress-cucumber-preprocessor";
+
+Given("I am on the form page", () => {
+  cy.visit('/');
+});
+
+Then("I can see the title", () => {
+  cy.contains('Client Management System')
+});
