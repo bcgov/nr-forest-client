cc187802870f732703a064374f002701265831dc
Maria Martinez <maria.martinez@gov.bc.ca>
2023-06-05 14:42:34 -0700

    chore: updating tests

--- backend/src/test/java/ca/bc/gov/app/util/ClientValidationUtilsTest.java
+++ backend/src/test/java/ca/bc/gov/app/util/ClientValidationUtilsTest.java
@@ -1,16 +1,14 @@
 package ca.bc.gov.app.util;
 
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
-
-import ca.bc.gov.app.dto.client.ClientTypeEnum;
-import ca.bc.gov.app.dto.client.LegalTypeEnum;
-import java.util.stream.Stream;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Stream;
 import org.apache.commons.lang3.StringUtils;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -18,6 +16,9 @@ import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.validation.Errors;
 import org.springframework.validation.FieldError;
 import org.springframework.validation.MapBindingResult;
+import org.springframework.validation.ObjectError;
+import ca.bc.gov.app.dto.client.ClientTypeEnum;
+import ca.bc.gov.app.dto.client.LegalTypeEnum;
 
 @DisplayName("Unit Test | Client Validation")
 class ClientValidationUtilsTest {
@@ -33,6 +34,48 @@ class ClientValidationUtilsTest {
     } else {
       assertNull(ClientValidationUtils.getClientType(legal));
     }
+  }
+  
+  @ParameterizedTest(name = "For phone number {0} I will have {1} errors with message {2}")
+  @MethodSource("validatePhoneNumberContents")
+  @DisplayName("validating phone numbers")
+  void shouldValidatePhoneNumbers(String phoneNumber, int errorCount, String errorMessage) {
+    String field = "phoneNumber";
+    Map<String, Object> target = new HashMap<>();
+    Errors errors = new MapBindingResult(target, "");
+
+    ClientValidationUtils.validatePhoneNumber(phoneNumber, field, errors);
+    
+    System.out.println("Expected Error Count: " + errorCount);
+    System.out.println("Actual Error Count: " + errors.getErrorCount());
+
+    assertEquals(errorCount, errors.getErrorCount());
+    if (errorCount > 0) {
+      System.out.println("Errors:");
+      for (ObjectError error : errors.getAllErrors()) {
+          System.out.println(error.getDefaultMessage());
+      }
+      
+      assertThat(
+          errors
+              .getAllErrors()
+              .stream()
+              .map(x -> (FieldError) x)
+              .map(DefaultMessageSourceResolvable::getCodes)
+              .flatMap(Stream::of)
+      )
+      .isNotNull()
+      .hasSize(3)
+      .contains(errorMessage);
+    }
+  }
+  
+  private static Stream<Arguments> validatePhoneNumberContents() {
+    return Stream.of(Arguments.of("1234567890", 0, StringUtils.EMPTY),
+                     Arguments.of(StringUtils.EMPTY, 1, "The phone number must be a 10-digit number"),
+                     Arguments.of("abc", 1, "The phone number must be a 10-digit number"),
+                     Arguments.of("123", 1, "The phone number must be 10 digits")
+    );
   }
 
   @ParameterizedTest(name = "For email {0} I will have {1} errors with message {2}")
@@ -55,9 +98,9 @@ class ClientValidationUtilsTest {
               .map(DefaultMessageSourceResolvable::getCodes)
               .flatMap(Stream::of)
       )
-          .isNotNull()
-          .hasSize(3)
-          .contains(errorMessage);
+      .isNotNull()
+      .hasSize(3)
+      .contains(errorMessage);   
     }
   }
 
@@ -70,7 +113,6 @@ class ClientValidationUtilsTest {
             Arguments.of(StringUtils.EMPTY, 1, "You must enter an email address")
         );
   }
-
 
   private static Stream<Arguments> getClientType() {
     return
@@ -95,18 +137,5 @@ class ClientValidationUtilsTest {
             Arguments.of(null, null)
         );
   }
-
-  @Test
-  void testValidatePhoneNumber() {
-    String phoneNumber = "1234567890";
-    String field = "phoneNumber";
-    Map<String, Object> target = new HashMap<>();
-    Errors errors = new MapBindingResult(target, "");
-
-    ClientValidationUtils.validatePhoneNumber(phoneNumber, field, errors);
-
-    assertEquals(0, errors.getErrorCount());
-  }
-
 
 }
