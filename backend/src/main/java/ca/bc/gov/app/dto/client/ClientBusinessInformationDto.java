ca7a7def7beb7353e0ea622c1a2731995200af97
Paulo Gomes da Cruz Junior <paulo.cruz@encora.com>
2023-06-14 08:19:50 -0700

    feat(BE:FSADT1-794): updating content (#494)
    * no message
    
    * no message
    
    * FSADT1794
    
    * feat:(BE:FSADT1-745): changing mail template contents
    
    * chore: removing unused files
    
    * chore: updating templates
    
    * chore: updating templates
    
    * fix(be):
    - Fixed code smells
    
    * feat(be):
    - Updated templates as per requested by Andrew (FSADT1-794)
    
    ---------
    
    Co-authored-by: Maria Martinez <maria.martinez@gov.bc.ca>
    Co-authored-by: Maria Martinez <77364706+mamartinezmejia@users.noreply.github.com>

--- backend/src/main/java/ca/bc/gov/app/dto/client/ClientBusinessInformationDto.java
+++ backend/src/main/java/ca/bc/gov/app/dto/client/ClientBusinessInformationDto.java
@@ -3,16 +3,26 @@ package ca.bc.gov.app.dto.client;
 import java.util.Map;
 import org.apache.commons.lang3.StringUtils;
 
-public record ClientBusinessInformationDto(String incorporationNumber, String businessName,
-    String businessType, String clientType, String goodStanding, String legalType) {
+public record ClientBusinessInformationDto(
+    String incorporationNumber, 
+    String businessName,
+    String businessType, 
+    String clientType, 
+    String goodStanding, 
+    String legalType) {
+  /**
+   * Returns a map containing the description of the client's business information.
+   *
+   * @return a map with keys representing the description fields and corresponding values
+   */
   public Map<String, Object> description() {
     return Map.of(
         "incorporation", StringUtils.isBlank(incorporationNumber) ? "" : incorporationNumber, 
         "name", StringUtils.isBlank(businessName) ? "" : businessName, 
         "businessType", StringUtils.isBlank(businessType) ? "" : businessType, 
         "clientType", StringUtils.isBlank(clientType) ? "" : clientType,
-        "legalType", StringUtils.isBlank(legalType) ? "" : legalType, 
-        "goodStanding", StringUtils.isBlank(goodStanding) ? "" : goodStanding
+        "goodStanding", StringUtils.isBlank(goodStanding) ? "" : goodStanding,
+        "legalType", StringUtils.isBlank(legalType) ? "" : legalType
         );
   }
 }
