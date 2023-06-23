5d6af9dabc1d661603e44aa02cc29760a269775c
Paulo Gomes da Cruz Junior <paulo.cruz@gov.bc.ca>
2023-06-23 10:40:00 -0700

    chore: adding test content to the home page

--- frontend/src/pages/HomePage.vue
+++ frontend/src/pages/HomePage.vue
@@ -1,5 +1,11 @@
 <template>
   <div style="margin: 24px">Welcome to forest client!</div>
+  <div>
+    <router-link :to="{ path: '/new-client' }">New Client</router-link><br />
+    <router-link :to="{ path: '/form-submission' }"
+      >Form Submission</router-link
+    >
+  </div>
 </template>
 
 <script setup lang="ts"></script>
