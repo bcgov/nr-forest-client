639ad01ca1bbd49ca364f3e3295ca49f6192c398
Paulo Gomes da Cruz Junior <paulo.cruz@gov.bc.ca>
2023-07-04 15:33:28 -0700

    chore: cleaning imports

--- backend/src/main/java/ca/bc/gov/app/controller/client/ClientController.java
+++ backend/src/main/java/ca/bc/gov/app/controller/client/ClientController.java
@@ -1,27 +1,23 @@
 package ca.bc.gov.app.controller.client;
 
-import ca.bc.gov.app.ApplicationConstant;
 import ca.bc.gov.app.dto.bcregistry.ClientDetailsDto;
 import ca.bc.gov.app.dto.client.ClientLookUpDto;
 import ca.bc.gov.app.dto.client.ClientNameCodeDto;
 import ca.bc.gov.app.dto.client.SendMailRequestDto;
 import ca.bc.gov.app.exception.NoClientDataFound;
 import ca.bc.gov.app.service.client.ClientService;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.media.ArraySchema;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.ExampleObject;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import java.time.LocalDate;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.http.server.reactive.ServerHttpResponse;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
@@ -98,7 +94,7 @@ public class ClientController {
 
   @GetMapping("/mail")
   @ResponseStatus(HttpStatus.ACCEPTED)
-    public Mono<Void> sendEmail(@RequestBody SendMailRequestDto sendMailRequestDto){
-        return clientService.sendEmail(sendMailRequestDto);
+  public Mono<Void> sendEmail(@RequestBody SendMailRequestDto sendMailRequestDto) {
+    return clientService.sendEmail(sendMailRequestDto);
   }
 }
