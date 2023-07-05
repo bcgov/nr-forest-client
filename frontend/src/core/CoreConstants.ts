8b4d920c74a35b80953ef85543a3628182c6c58e
Paulo Gomes da Cruz Junior <paulo.cruz@gov.bc.ca>
2023-07-05 11:42:04 -0700

    ci: adding vite node env as part of deployment

--- frontend/src/core/CoreConstants.ts
+++ frontend/src/core/CoreConstants.ts
@@ -1,20 +1,19 @@
 // env vars
 export const backendUrl =
-  window.localStorage.getItem("VITE_BACKEND_URL") ||
-  import.meta.env.VITE_BACKEND_URL;
+  window.localStorage.getItem('VITE_BACKEND_URL') ||
+  import.meta.env.VITE_BACKEND_URL
 export const frontendUrl =
-  window.localStorage.getItem("VITE_FRONTEND_URL") ||
-  import.meta.env.VITE_FRONTEND_URL;
+  window.localStorage.getItem('VITE_FRONTEND_URL') ||
+  import.meta.env.VITE_FRONTEND_URL
 export const keycloakUrl =
-  window.localStorage.getItem("VITE_KEYCLOAK_URL") ||
-  import.meta.env.VITE_KEYCLOAK_URL;
+  window.localStorage.getItem('VITE_KEYCLOAK_URL') ||
+  import.meta.env.VITE_KEYCLOAK_URL
 export const keycloakClientId =
-  window.localStorage.getItem("VITE_KEYCLOAK_CLIENT_ID") ||
-  import.meta.env.VITE_KEYCLOAK_CLIENT_ID;
+  window.localStorage.getItem('VITE_KEYCLOAK_CLIENT_ID') ||
+  import.meta.env.VITE_KEYCLOAK_CLIENT_ID
 export const nodeEnv =
-  window.localStorage.getItem("VITE_NODE_ENV") ||
-  import.meta.env.VITE_NODE_ENV;
+  window.localStorage.getItem('VITE_NODE_ENV') || import.meta.env.VITE_NODE_ENV
 
 // constant
-export const maxFileSizePerFile = 1000000 * 20; //20 mb
-export const maxTotalFileSize = 1000000 * 20 * 5; //100 mb
+export const maxFileSizePerFile = 1000000 * 20 // 20 mb
+export const maxTotalFileSize = 1000000 * 20 * 5 // 100 mb
