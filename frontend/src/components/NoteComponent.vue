b5d568270cb2fab1ce266a1bb1287169b341cbef
Paulo Gomes da Cruz Junior <paulo.cruz@gov.bc.ca>
2023-07-06 14:19:04 -0700

    chore: refactoring NoteComponent

--- frontend/src/components/NoteComponent.vue
+++ frontend/src/components/NoteComponent.vue
@@ -3,18 +3,7 @@
 </template>
 
 <script setup lang="ts">
-const props = defineProps({
-  note: { 
-    type: String, 
-    required: true 
-  },
-});
-</script>
-<script lang="ts">
-import { defineComponent } from "vue";
-export default defineComponent({
-  name: "NoteComponent",
-});
+const props = defineProps<{ note: String }>()
 </script>
 
 <style scoped>
