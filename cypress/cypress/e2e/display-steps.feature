0fbaf3df06d2e1d771359d9e475b21d9d815cd4c
Fernando Terra <79578735+fterra-encora@users.noreply.github.com>
2023-07-10 16:06:12 -0300

    feat(FE:FSADT1-814): Vertical wizard (breadcrumb) (#515)
    * chore: display equal width for wizard steps
    
    And display ellipsis when text doesn't fit.
    
    * feat: display wizard steps vertically
    
    * test: display steps either vertically or horizontally
    
    * test: add more examples
    
    * style: format feature file
    
    * style: remove unused variable
    
    * chore: replace px units with rem

---
+++ cypress/cypress/e2e/display-steps.feature
@@ -0,0 +1,25 @@
+Feature: Steps are displayed correctly
+
+  Test the page layout regarding the wizard steps according to different screen widths.
+
+  # screen width above or equal to 672
+  Scenario Outline: Steps are displayed horizontally
+    Given I am on the form page
+    And the width of screen is <width> pixels
+    Then steps are displayed horizontally
+
+    Examples:
+      | width |
+      | 672   |
+      | 1200  |
+
+  # screen width below 672
+  Scenario Outline: Steps are displayed vertically
+    Given I am on the form page
+    And the width of screen is <width> pixels
+    Then steps are displayed vertically
+
+    Examples:
+      | width |
+      | 671   |
+      | 400   |
--- frontend/src/components/wizard/WizardProgressIndicatorComponent.vue
+++ frontend/src/components/wizard/WizardProgressIndicatorComponent.vue
@@ -91,27 +91,55 @@ const canShowLink = (step: ProgressData) => {
 </script>
 
 <style scoped>
+@media screen and (max-width: 671px) {
+  .wizard-wrap-indicator {
+    flex-direction: column;
+  }
+  .wizard-wrap-item {
+    border-left: solid 0.125rem;
+    flex: none;
+  }
+}
+
+@media screen and (min-width: 672px) {
+  .wizard-wrap-indicator {
+    flex-direction: row;
+  }
+  .wizard-wrap-item {
+    border-top: solid 0.125rem;
+    flex: 1;
+  }
+}
+
 .wizard-wrap-indicator {
   display: flex;
   justify-content: space-between;
-  margin-bottom: 48px;
+  margin-bottom: 3rem;
 }
 .wizard-wrap-item {
-  flex-grow: 1;
   display: flex;
   justify-content: space-between;
-  border-top: solid 2px;
+  overflow: hidden;
+  height: 3.625rem;
 }
 
 .wizard-wrap-item-icon {
-  margin: 4px 10px 0px 0px;
+  margin: 0.25rem 0.625rem 0 0;
+  flex-shrink: 0;
 }
 
 .wizard-wrap-item-text {
   flex-grow: 1;
+  overflow: hidden;
 }
 .wizard-wrap-item-text p {
-  font-size: 14px;
+  font-size: 0.875rem;
+}
+.wizard-wrap-item-text a, .wizard-wrap-item-text span {
+  overflow: hidden;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+  display: block;
 }
 .wizard-wrap-item-text p a {
   text-decoration: none;
@@ -122,7 +150,7 @@ const canShowLink = (step: ProgressData) => {
   cursor: pointer;
 }
 .wizard-wrap-item-text span {
-  font-size: 12px;
+  font-size: 0.75rem;
 }
 
 .wizard-wrap-item-current {
