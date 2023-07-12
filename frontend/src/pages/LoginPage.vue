8f81b5384a4cc06c0d730c7735ff661cab30a7c4
Paulo Gomes da Cruz Junior <paulo.cruz@gov.bc.ca>
2023-07-12 14:15:27 -0700

    feat(FE:FSADT1-816): fam integration

---
+++ frontend/src/pages/LoginPage.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>FAM Template</div>
+
+  <bx-btn
+    kind="tertiary"
+    iconLayout=""
+    class="bx--btn"
+    @click.prevent="$session?.logIn('bceid')"
+    size="field"
+  >
+    <span>Login with BCeID</span>
+    <Logout16 slot="icon" />
+  </bx-btn>
+
+  <bx-btn
+    kind="primary"
+    iconLayout=""
+    class="bx--btn"
+    @click.prevent="$session?.logIn('idir')"
+    size="field"
+  >
+    <span>LogIn with IDIR</span>
+    <Logout16 slot="icon" />
+  </bx-btn>
+</template>
+
+<script setup lang="ts">
+import { useRouter } from 'vue-router'
+import Logout16 from '@carbon/icons-vue/es/logout/16'
+import AmplifyUserSession from '@/helpers/AmplifyUserSession'
+
+// extract the querystring parameters from the URL
+const router = useRouter()
+const { query } = router.currentRoute.value
+
+// check if a querystring parameter called ref exists and if it has a value of external
+if (query.ref && query.ref === 'external') {
+  AmplifyUserSession.logIn('bceid')
+}
+</script>
