a1c2c25fbb2afeaa5b34d41a7028f68d6259c2bc
Paulo Gomes da Cruz Junior <paulo.cruz@gov.bc.ca>
2023-07-12 14:17:37 -0700

    chore: adding missing files

--- frontend/src/CoreConstants.ts
+++ frontend/src/CoreConstants.ts
@@ -15,3 +15,20 @@ export const cognitoEnv: string = checkEnv('VITE_AWS_ENV')
 // constant
 export const maxFileSizePerFile = 1000000 * 20 // 20 mb
 export const maxTotalFileSize = 1000000 * 20 * 5 // 100 mb
+
+export const amplifyConfig = {
+  aws_cognito_region: checkEnv('VITE_AWS_COGNITO_REGION'),
+  aws_user_pools_id: checkEnv('VITE_AWS_USER_POOLS_ID'),
+  aws_user_pools_web_client_id: checkEnv('VITE_AWS_USER_POOLS_WEB_CLIENT_ID'),
+  aws_mandatory_sign_in: 'enable',
+  oauth: {
+    domain: `${checkEnv('VITE_AWS_COGNITO_DOMAIN')}.auth.${checkEnv(
+      'VITE_AWS_COGNITO_REGION'
+    )}.amazoncognito.com`,
+    scope: ['openid'],
+    redirectSignIn: `${checkEnv('VITE_FRONTEND_URL')}/dashboard`,
+    redirectSignOut: checkEnv('VITE_AWS_LOGOUT'),
+    responseType: 'code'
+  },
+  federationTarget: 'COGNITO_USER_POOLS'
+}
