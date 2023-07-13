e378b797e41d3b74975fd4422e9d1f60f2e50b81
Fernando Terra <79578735+fterra-encora@users.noreply.github.com>
2023-07-13 13:18:02 -0300

    feat(FE:FSADT1-826): Landing page (#522)
    * feat: add landig page base code
    
    * feat: display landing page at the root location
    
    * chore: remove unused variables
    
    * fix: add the prefix on grid classes names
    
    * chore: add the proper microcopies
    
    * chore: adjust layout columns
    
    * chore: adjust font family
    
    * chore: remove the BCeID button
    
    * fix: fixing test issues
    
    * fix: fixing workflow
    
    * ci: fixing ci
    
    * ci: debbuging code coverage
    
    * ci: testing new node version
    
    * chore: enabling debug
    
    * ci: preventing error when folder not exists
    
    ---------
    
    Co-authored-by: Paulo Gomes da Cruz Junior <paulo.cruz@gov.bc.ca>

---
+++ frontend/src/pages/LandingPage.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import Seeding from '../assets/images/seeding.png'
+import logo from '../assets/images/bc-gov-logo.png'
+import login16 from '@carbon/icons-vue/es/login/16'
+</script>
+
+<template>
+  <div class="full-width">
+    <div class="row bx--row landing-grid">
+      <div class="bx--col-sm-8 bx--col-md-10 bx--col-lg-10">
+        <img :src="logo" alt="BCGov Logo" width="160" class="logo" />
+        <h1 id="landing-title" class="landing-title">Welcome to CLIENT</h1>
+        <h2 id="landing-subtitle" class="landing-subtitle">
+          The Ministry of Forests' client management system
+        </h2>
+
+        <p id="landing-desc" class="landing-desc">Check and manage client submissions</p>
+
+        <bx-btn kind="primary" iconLayout="" class="landing-button">
+          <span>Login with IDIR</span>
+          <login16 slot="icon" />
+        </bx-btn>
+      </div>
+      <div class="bx--col-sm-8 bx--col-md-6 bx--col-lg-6 landing-img-column">
+        <img
+          :src="Seeding"
+          alt="Small green seedling on the dirt and watered"
+          class="landing-img"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+@import '@bcgov-nr/nr-fsa-theme/style-sheets/landing-page-components-overrides.scss';
+</style>
