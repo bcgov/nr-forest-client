5d2a955a55d5e20a576256a62a2df14079bc0f49
Maria Martinez <maria.martinez@gov.bc.ca>
2023-07-17 09:56:11 -0700

    Removed repeated classes

--- frontend/src/components/MainHeaderComponent.vue
+++ frontend/src/components/MainHeaderComponent.vue
@@ -31,43 +31,7 @@ import Logout16 from '@carbon/icons-vue/es/logout/16'
 </script>
 
 <style scoped>
-/*header {
-  background-color: #0073E6;
-  padding: 0 30px 0 30px;
-  color: #fff;
-  display: flex;
-  height: 65px;
-  top: 0px;
-  position: static;
-  width: 100%;
-}
 
-header h1 {
-  font-family: ‘BCSans’, ‘Noto Sans’, Verdana, Arial, sans-serif;
-  margin: 5px 5px 0 18px;
-  visibility: hidden;
-
-  font-size: 15px;
-  line-height: 18px;
-  font-weight: 700;
-  letter-spacing: 0.16px;
-}*/
-
-
-.bx--header {
-  background-color: #0073E6 !important;
-  display: relative;
-  height: 3.5rem;
-}
-
-.bx--header-site-name {
-  font-family: ‘BCSans’, ‘Noto Sans’, Verdana, Arial, sans-serif;
-  font-size: 0.875rem;
-  font-weight: 700;
-  color: #F3F3F5;
-  padding-left: 2rem;
-  display: inline-block;
-}
 
 /*
   These are sample media queries only. Media queries are quite subjective
