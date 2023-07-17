67b08b6b26be90f2362e6c5ac848d0359ecd5d0d
Paulo Gomes da Cruz Junior <paulo.cruz@gov.bc.ca>
2023-07-17 13:11:11 -0700

    fix: fixing test

--- frontend/tests/unittests/components/MainHeaderComponent.spec.ts
+++ frontend/tests/unittests/components/MainHeaderComponent.spec.ts
@@ -1,22 +1,28 @@
-import { describe, it, expect, vi } from 'vitest'
+import { describe, it, expect, vi, afterEach } from 'vitest'
 import { mount } from '@vue/test-utils'
 
 import MainHeaderComponent from '@/components/MainHeaderComponent.vue'
 
-describe('MainHeaderComponent.vue', () => {
-  it('renders the component when authenticated', async () => {
-    const mockSession = {
-      user: {},
-      logOut: vi.fn()
-    }
+import AmplifyUserSession from '@/helpers/AmplifyUserSession'
 
-    const wrapper = mount(MainHeaderComponent, {
-      global: {
-        mocks: {
-          $session: mockSession
-        }
-      }
+describe('MainHeaderComponent.vue', () => {
+  afterEach(() => {
+    vi.restoreAllMocks()
+  })
+  it('renders the component when authenticated', async () => {
+    vi.spyOn(AmplifyUserSession, 'isLoggedIn').mockImplementation(() =>
+      Promise.resolve(true)
+    )
+
+    const wrapper = mount(MainHeaderComponent)
+
+    await new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(true)
+      }, 10) // Small delay to allow computedAsync to catch up, nextTick doesn't work
     })
+
+    console.log(wrapper.html())
 
     expect(wrapper.find('header').exists()).toBe(true)
     expect(wrapper.find('bx-btn').exists()).toBe(true)
@@ -24,13 +30,11 @@ describe('MainHeaderComponent.vue', () => {
   })
 
   it('renders the component when unauthenticated', async () => {
-    const wrapper = mount(MainHeaderComponent, {
-      global: {
-        mocks: {
-          $session: null
-        }
-      }
-    })
+    vi.spyOn(AmplifyUserSession, 'isLoggedIn').mockImplementation(() =>
+      Promise.resolve(false)
+    )
+
+    const wrapper = mount(MainHeaderComponent)
 
     expect(wrapper.find('header').exists()).toBe(true)
     expect(wrapper.find('bx-btn').exists()).toBe(false)
