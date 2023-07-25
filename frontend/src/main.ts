65f788a0197cdfffd6b38cf9ee58b0ec697d1cf8
Paulo Gomes da Cruz Junior <paulo.cruz@gov.bc.ca>
2023-07-25 07:39:59 -0700

    feat: adding feature flags as global data on vue

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -3,13 +3,13 @@ import { Amplify } from 'aws-amplify'
 
 import App from '@/App.vue'
 import { router } from '@/routes'
-import { amplifyConfig } from '@/CoreConstants'
+import { amplifyConfig, featureFlags } from '@/CoreConstants'
 import { masking } from '@/helpers/CustomDirectives'
 import type { AmplifyCustomProperties } from '@/dto/CommonTypesDto'
 import AmplifyUserSession from '@/helpers/AmplifyUserSession'
 
 // Importing Styles
-import '@/styles';
+import '@/styles'
 
 const app = createApp(App)
 
@@ -17,11 +17,13 @@ app.use(router)
 app.directive('mask', masking('.bx--text-input__field-wrapper input'))
 Amplify.configure(amplifyConfig)
 app.config.globalProperties.$session = AmplifyUserSession
+app.config.globalProperties.$features = featureFlags
 app.mount('#app')
 
 declare module '@vue/runtime-core' {
   // eslint-disable-next-line no-unused-vars
   interface ComponentCustomProperties {
     $session: AmplifyCustomProperties
+    $features: Record<string, any>
   }
 }
