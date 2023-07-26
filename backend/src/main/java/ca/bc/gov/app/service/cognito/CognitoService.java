b21abe64358e2a65ab7a7e63998478fa5eca4474
Paulo Gomes da Cruz Junior <paulo.cruz@gov.bc.ca>
2023-07-26 12:29:26 -0700

    feat(BE:FSADT1-816): adding cognito login on backend
    - adding login capability on BE
    - adding token extraction on BE
    - adding logout capability on BE

---
+++ backend/src/main/java/ca/bc/gov/app/service/cognito/CognitoService.java
@@ -0,0 +1,59 @@
+package ca.bc.gov.app.service.cognito;
+
+import ca.bc.gov.app.configuration.ForestClientConfiguration;
+import ca.bc.gov.app.dto.cognito.AuthResponse;
+import ca.bc.gov.app.util.PKCEUtil;
+import java.security.NoSuchAlgorithmException;
+import java.util.Optional;
+import java.util.UUID;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.env.Environment;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class CognitoService {
+
+  private final ForestClientConfiguration configuration;
+  private final Environment environment;
+  private final WebClient webClient = WebClient
+      .builder()
+      .build();
+  private final String codeVerify = PKCEUtil.generateCodeVerifier();
+
+  public Mono<AuthResponse> exchangeAuthorizationCodeForTokens(String code) {
+
+    MultiValueMap<String, String> requestBody = new LinkedMultiValueMap<>();
+    requestBody.add("grant_type", "authorization_code");
+    requestBody.add("code", code);
+    requestBody.add("client_id", configuration.getCognito().getClientId());
+    requestBody.add("redirect_uri", configuration.getCognito().getRedirectUri());
+    requestBody.add("code_verifier", codeVerify);
+
+    return webClient
+        .post()
+        .uri(configuration.getCognito().getUrl() + "/oauth2/token")
+        .header("Content-Type", MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+        .header("Accept", "application/json")
+        .cookie("XSRF-TOKEN", UUID.randomUUID().toString())
+        .bodyValue(requestBody)
+        .exchangeToMono(clientResponse -> clientResponse.bodyToMono(AuthResponse.class));
+  }
+
+  public Optional<String> getCodeChallenge() {
+    try {
+      return Optional.of(PKCEUtil.generateCodeChallenge(codeVerify));
+    } catch (NoSuchAlgorithmException e) {
+      log.error("Cannot generate code challenge", e);
+    }
+    return Optional.empty();
+  }
+
+}
