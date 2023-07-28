fd993882cf09b6798ddafb2f771e3b21fc57264a
Maria Martinez <maria.martinez@gov.bc.ca>
2023-07-28 10:35:11 -0700

    Completed header changes: - Hide/show elements based on the logged in person/entity

--- frontend/src/components/MainHeaderComponent.vue
+++ frontend/src/components/MainHeaderComponent.vue
@@ -3,29 +3,42 @@
 <template>
   <header>
     <div class="bx--header">
-      <a href="https://gov.bc.ca">
-        <img
-          src="/img/bc_for_logo.png"
-          alt="Go to the Government of British Columbia website"
-          width="150"
-        />
-      </a>
-      <label class="bx--header-site-name">Ministry of Forests</label>
-      <label class="bx--header-env-and-rel" 
-             v-if="env !== 'Prod'">
-        Env. {{ env }} - Rel. {{appVersion}}
-      </label>
-      <bx-btn
-        v-if="$keycloak"
-        kind="tertiary"
-        iconLayout=""
-        class="bx--btn bx--btn-header"
-        @click.prevent="$keycloak.logoutFn"
-        size="field"
-      >
-        <span>Logout</span>
-        <Logout16 slot="icon" />
-      </bx-btn>
+      <div v-if="$keycloak?.tokenParsed?.identity_provider !== 'idir'">
+        <a href="https://gov.bc.ca">
+          <img
+            src="/img/bc_for_logo.png"
+            alt="Go to the Government of British Columbia website"
+            width="150"
+          />
+        </a>
+        <div class="bx--header-external-texts">
+          <label class="bx--header-site-name">
+            Ministry of Forests
+          </label>
+          <label class="bx--header-env-and-rel" 
+                v-if="env !== 'Prod'">
+            Env. {{ env }} - Rel. {{appVersion}}
+          </label>
+        </div>
+        <bx-btn
+          v-if="$keycloak"
+          kind="tertiary"
+          iconLayout=""
+          class="bx--btn bx--btn-header"
+          @click.prevent="$keycloak.logoutFn"
+          size="field"
+        >
+          <span>Logout</span>
+          <Logout16 slot="icon" />
+        </bx-btn>
+      </div>
+      <div v-if="$keycloak?.tokenParsed?.identity_provider === 'idir'">
+        <label class="bx--header-site-name">Client Management System</label>
+        <label class="bx--header-env-and-rel" 
+              v-if="env !== 'Prod'">
+          Env. {{ env }} - Rel. {{appVersion}}
+        </label>
+      </div>
     </div>
   </header>
 </template>
@@ -42,6 +55,12 @@ env.value = env.value.charAt(0).toUpperCase() + env.value.slice(1);
 </script>
 
 <style scoped>
+.bx--header-external-texts {
+  position: absolute;
+  top: 40%;
+  left: 9rem;
+}
+
 .bx--header-env-and-rel {
   padding-left: 2rem; 
   color: white;
