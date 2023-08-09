84857b4d13150ccce74f0bc6ddb37b0c5c58b9af
Maria Martinez <maria.martinez@gov.bc.ca>
2023-08-09 15:43:07 -0700

    Made changes requested

--- backend/src/main/java/ca/bc/gov/app/controller/ches/ChesController.java
+++ backend/src/main/java/ca/bc/gov/app/controller/ches/ChesController.java
@@ -1,13 +1,25 @@
 package ca.bc.gov.app.controller.ches;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 import ca.bc.gov.app.dto.client.EmailRequestDto;
 import ca.bc.gov.app.service.client.ClientService;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
 import reactor.core.publisher.Mono;
 
+@RestController
+@Tag(
+    name = "CHES",
+    description = "The CHES endpoint is responsible for handling the sending of emails"
+)
+@RequestMapping(value = "/api/ches", produces = MediaType.APPLICATION_JSON_VALUE)
+@RequiredArgsConstructor
 public class ChesController {
   
   private ClientService clientService;
--- backend/src/main/java/ca/bc/gov/app/dto/ValidationError.java
+++ backend/src/main/java/ca/bc/gov/app/dto/ValidationError.java
@@ -1,20 +1,6 @@
 package ca.bc.gov.app.dto;
 
 import java.io.Serializable;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.With;
 
-@Data
-@With
-@NoArgsConstructor
-@AllArgsConstructor
-public class ValidationError implements Serializable {
-
-  private static final long serialVersionUID = -3525306340782994707L;
-  
-  private String fieldId;
-  private String errorMsg;
-  
+public record ValidationError(String fieldId, String errorMsg) implements Serializable {
 }
