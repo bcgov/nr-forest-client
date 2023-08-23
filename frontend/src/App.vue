02b9a35805b1219bf3d17d91ab3d9b13d34c76f2
Paulo Gomes da Cruz Junior <paulo.cruz@gov.bc.ca>
2023-08-23 10:54:30 -0700

    fix: fixing modal

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,62 +1,8 @@
-<template>
-  <div class="headers" v-if="$session?.isLoggedIn()">
-    <main-header-component v-if="!$route.meta.hideHeader"></main-header-component>
-  </div>
-  <div :class="{[`${$route.meta.format}`]:true}">
-    <div class="content">
-    <router-view></router-view>
-    </div>
-  </div>
-
-  <div class="modals">
-    
-  <bx-modal
-    id="modal-example"
-    size="sm"
-    :open="modalContent.active"
-    @bx-modal-closed="closeModal"
-  >
-    <bx-modal-header>
-      <bx-modal-close-button></bx-modal-close-button>
-      <bx-modal-label>Delete additional {{ modalContent.kind }}</bx-modal-label>
-      <bx-modal-heading
-        >Are you sure you want to delete "{{ modalContent.message }}" additional
-        {{ modalContent.kind }}</bx-modal-heading
-      >
-    </bx-modal-header>
-    <bx-modal-body><p></p></bx-modal-body>
-
-    <bx-modal-footer>
-      <bx-btn kind="secondary" 
-              data-modal-close
-              class="bx--modal-btn bx--modal-btn1">
-        Cancel
-      </bx-btn>
-      <span class="bx--modal-btn-divider"></span>
-      <bx-btn kind="danger" 
-              @click.prevent="deleteContentModal"
-              class="bx--modal-btn bx--modal-btn2">
-        Delete additional {{ modalContent.kind }}
-      </bx-btn>
-    </bx-modal-footer>
-  </bx-modal>
-</div>
-
-  <bx-toast-notification
-      v-if="toastContent.active"
-      class="wizard-head-toast"
-      timeout="8000"
-      kind="success"
-      :title="toastContent.kind"
-      :subtitle="toastContent.message"
-    >
-  </bx-toast-notification>
-</template>
-
 <script setup lang="ts">
 import { ref, computed, useSlots, reactive, provide } from 'vue'
 import { useEventBus } from '@vueuse/core'
 import type { ModalNotification } from '@/dto/CommonTypesDto'
+import Delete16 from '@carbon/icons-vue/es/trash-can/16'
 
 const modalBus = useEventBus<ModalNotification>('modal-notification')
 const toastBus = useEventBus<ModalNotification>('toast-notification')
@@ -99,3 +45,61 @@ const deleteContentModal = () => {
 
 modalBus.on(openModal)
 </script>
+
+<template>
+  <div class="headers" v-if="$session?.isLoggedIn()">
+    <main-header-component v-if="!$route.meta.hideHeader"></main-header-component>
+  </div>
+  <div :class="{[`${$route.meta.format}`]:true}">
+    <div class="content">
+    <router-view></router-view>
+    </div>
+  </div>
+
+  <div class="modals">
+    
+  <bx-modal
+    id="modal-example"
+    size="sm"
+    :open="modalContent.active"
+    @bx-modal-closed="closeModal"
+  >
+    <bx-modal-header>
+      <bx-modal-close-button></bx-modal-close-button>
+      <bx-modal-heading
+        >Are you sure you want to delete "{{ modalContent.message }}" additional
+        {{ modalContent.kind }}?</bx-modal-heading
+      >
+    </bx-modal-header>
+    <bx-modal-body><p></p></bx-modal-body>
+
+    <bx-modal-footer>
+      <div class="modal-footer">
+        <bx-btn kind="secondary" 
+              data-modal-close
+              class="bx--modal-btn bx--modal-btn1">
+        Cancel
+      </bx-btn>
+      
+      <bx-btn kind="danger" 
+              @click.prevent="deleteContentModal"
+              class="bx--modal-btn bx--modal-btn2">
+        Delete
+        <Delete16 slot="icon" />
+      </bx-btn>
+      </div>
+    </bx-modal-footer>
+  </bx-modal>
+</div>
+
+  <bx-toast-notification
+      v-if="toastContent.active"
+      class="wizard-head-toast"
+      timeout="8000"
+      kind="success"
+      :title="toastContent.kind"
+      :subtitle="toastContent.message"
+    >
+  </bx-toast-notification>
+</template>
+
