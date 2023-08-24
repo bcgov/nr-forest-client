d564a0113b733b2141ff8ac2463795a43cca8339
Paulo Gomes da Cruz Junior <paulo.cruz@gov.bc.ca>
2023-08-24 06:00:13 -0700

    chore: fixing eslint config for vscode

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,15 +1,32 @@
 module.exports = {
+  root: true,
   env: {
     browser: true,
     es2021: true,
     jest: true,
-    node: true
+    node: true,
+    'vue/setup-compiler-macros': true
+  },
+  'import/resolver': {
+    node: {
+      extensions: ['.js', '.jsx', '.ts', '.tsx', '.vue'],
+      moduleDirectory: ['node_modules', 'src/']
+    },
+    typescript: {
+      alwaysTryTypes: true
+    }
   },
   extends: [
     'plugin:import/errors',
     'plugin:import/warnings',
     'plugin:import/typescript',
-    'standard'
+    'standard',
+    'prettier',
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/typescript/recommended',
+    '@vue/eslint-config-typescript/recommended',
+    '@vue/eslint-config-prettier'
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
@@ -18,6 +35,12 @@ module.exports = {
   },
   plugins: ['@typescript-eslint'],
   rules: {},
+  overrides: [
+    {
+      files: ['cypress/integration/**.spec.{js,ts,jsx,tsx}'],
+      extends: ['plugin:cypress/recommended']
+    }
+  ],
   globals: {
     config: 'readable',
     cy: 'readonly',
