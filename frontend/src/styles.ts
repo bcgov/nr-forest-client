408166ab5071aff6bf2f13a61568597b636f3f23
Paulo Gomes da Cruz Junior <paulo.cruz@gov.bc.ca>
2023-08-31 05:43:18 -0700

    feat: updating styles

--- frontend/src/styles.ts
+++ frontend/src/styles.ts
@@ -1,8 +1,4 @@
 // Importing BC typography
 import '@bcgov/bc-sans/css/BCSans.css'
 
-// Import Carbon WebComponents
-import '@carbon/web-components/es'
-import 'carbon-components/css/carbon-components.min.css'
-
 import '@/assets/styles/global.scss'
