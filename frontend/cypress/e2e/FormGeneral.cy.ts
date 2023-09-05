5927497c5ecfce87d5b2104f6e95416d2eeba5d8
Paulo Gomes da Cruz Junior <paulo.cruz@gov.bc.ca>
2023-09-05 07:05:24 -0700

    test: fixing e2e tests

--- frontend/cypress/e2e/FormGeneral.cy.ts
+++ frontend/cypress/e2e/FormGeneral.cy.ts
@@ -2,22 +2,19 @@
 describe('General Form', () => {
   beforeEach(() => {
     cy.intercept('http://localhost:8080/api/clients/name/*', {
-      fixture: 'business.json'
+      fixture: 'business.json',
     }).as('searchCompany')
 
     cy.intercept('GET', '/api/clients/XX9016140', {
-      fixture: 'example.json'
+      fixture: 'example.json',
     }).as('selectCompany')
   })
 
   it('should render the component', () => {
     cy.visit('/')
-    cy.get('#landing-title').should('contain', 'Welcome to CLIENT')
+    cy.get('#landing-title').should('contain', 'Client Management System')
 
-    cy.get('#landing-subtitle').should(
-      'contain',
-      "The Ministry of Forests' client management system"
-    )
+    cy.get('#landing-subtitle').should('contain', 'Create and manage client accounts')
   })
 
   it('should render the form', () => {
@@ -26,19 +23,19 @@ describe('General Form', () => {
 
     cy.login('uattest@forest.client', 'Uat Test', 'bcsc')
 
-    cy.get('bx-form-item > #business').should('not.exist')
+    cy.get('#business').should('not.exist')
 
-    cy.get('.display-block-wrapper-info').should('not.be.visible')
+    cy.get('cds-inline-notification').should('not.exist')
 
     cy.get(
-      '[label-text="I have a BC registered business (corporation, sole proprietorship, society, etc.)"]'
+      '[label-text="I have a BC registered business (corporation, sole proprietorship, society, etc.)"]',
     )
       .should('be.visible')
       .click()
 
-    cy.get('.display-block-wrapper-info').should('be.visible')
+    cy.get('cds-inline-notification').should('be.visible')
 
-    cy.get('bx-form-item > #business')
+    cy.get('#business')
       .should('be.visible')
       .shadow()
       .find('input')
@@ -46,8 +43,7 @@ describe('General Form', () => {
       .type('Valid Company')
     cy.wait('@searchCompany')
 
-    cy.get('div.autocomplete-items-cell[data-id="XX9016140"]')
-      .click()
+    cy.get('cds-combo-box-item[data-id="XX9016140"]').click()
 
     cy.wait('@selectCompany')
 
