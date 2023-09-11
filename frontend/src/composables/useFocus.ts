a5154815cc612da13a3e22c0fa19d731c7b82f11
Fernando Terra <fernando.terra@encora.com>
2023-09-11 15:28:48 -0300

    feat: add callback argument to useFocus

--- frontend/src/composables/useFocus.ts
+++ frontend/src/composables/useFocus.ts
@@ -8,8 +8,16 @@ type OptionalElement = Element | HTMLElement | undefined
  * @returns two functions to set focus and scroll to a component
  */
 export const useFocus = (): {
-  setFocusedComponent: (componentName: string, time?: number) => Ref<OptionalElement>
-  setScrollPoint: (componentName: string, time?: number) => Ref<OptionalElement>
+  setFocusedComponent: (
+    componentName: string,
+    time?: number,
+    callback?: (refComponent: Ref<OptionalElement>) => void,
+  ) => Ref<OptionalElement>
+  setScrollPoint: (
+    componentName: string,
+    time?: number,
+    callback?: (refComponent: Ref<OptionalElement>) => void,
+  ) => Ref<OptionalElement>
 } => {
   // setActionOn is a function that execute the action on an a component
   const setActionOn = (
@@ -34,27 +42,61 @@ export const useFocus = (): {
     key: string,
     action: (target: Element | HTMLElement) => unknown,
     time: number = 100,
+    callback?: (refComponent: Ref<OptionalElement>) => void,
   ): Ref<OptionalElement> => {
     const refComponent = ref<OptionalElement>(undefined)
-    setTimeout(() => (refComponent.value = setActionOn(componentName, key, action)), time)
+    setTimeout(() => {
+      refComponent.value = setActionOn(componentName, key, action)
+      if (callback) {
+        const eventName = 'onscrollend' in window ? 'scrollend' : 'scroll'
+
+        const scrollEndListener = () => {
+          callback(refComponent)
+          document.removeEventListener(eventName, polyfilledListener)
+          console.log({ polyfilledListener })
+        }
+
+        let scrollEndTimer: NodeJS.Timeout
+
+        const polyfilledListener =
+          'onscrollend' in window
+            ? scrollEndListener
+            : () => {
+                clearTimeout(scrollEndTimer)
+                scrollEndTimer = setTimeout(scrollEndListener, 100)
+              }
+
+        document.addEventListener(eventName, polyfilledListener)
+      }
+    }, time)
     return refComponent
   }
 
   // Set the focus on a component with the data-focus attribute
-  const setFocusedComponent = (componentName: string, time: number = 100): Ref<OptionalElement> =>
+  const setFocusedComponent = (
+    componentName: string,
+    time: number = 100,
+    callback?: (refComponent: Ref<OptionalElement>) => void,
+  ): Ref<OptionalElement> =>
     execute(
       componentName,
       'data-focus',
       (element) => (element instanceof HTMLElement ? element.focus() : undefined),
       time,
+      callback,
     )
   // Scroll into view a component with the data-scroll attribute
-  const setScrollPoint = (componentName: string, time: number = 100): Ref<Element | undefined> =>
+  const setScrollPoint = (
+    componentName: string,
+    time: number = 100,
+    callback?: (refComponent: Ref<OptionalElement>) => void,
+  ): Ref<Element | undefined> =>
     execute(
       componentName,
       'data-scroll',
       (element) => element.scrollIntoView({ behavior: 'smooth', block: 'start' }),
       time,
+      callback,
     )
 
   return { setFocusedComponent, setScrollPoint }
