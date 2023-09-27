1bb375a91b77c1277eb39437a94b238d3e99c801
Maria Martinez <maria.martinez@gov.bc.ca>
2023-09-27 16:22:04 -0700

    Made code reviews

--- frontend/tests/unittests/components/MainHeaderComponent.spec.ts
+++ frontend/tests/unittests/components/MainHeaderComponent.spec.ts
@@ -1,45 +1,45 @@
-import { describe, it, expect, vi } from 'vitest'
-import { mount } from '@vue/test-utils'
+import { describe, it, expect, vi } from "vitest";
+import { mount } from "@vue/test-utils";
 
-import MainHeaderComponent from '@/components/MainHeaderComponent.vue'
+import MainHeaderComponent from "@/components/MainHeaderComponent.vue";
 
-describe('MainHeaderComponent.vue', () => {
-  it('renders the component when authenticated', async () => {
+describe("MainHeaderComponent.vue", () => {
+  it("renders the component when authenticated", async () => {
     const session = {
-      session: { user: { provider: 'bcsc' } },
+      session: { user: { provider: "bcsc" } },
       isLoggedIn: () => true,
-      logOut: vi.fn()
-    }
+      logOut: vi.fn(),
+    };
 
     const wrapper = mount(MainHeaderComponent, {
       global: {
         mocks: {
-          $session: session
-        }
-      }
-    })
+          $session: session,
+        },
+      },
+    });
 
-    expect(wrapper.html()).not.toBe('')
-    expect(wrapper.find('cds-button').exists()).toBe(true)
-    expect(wrapper.html()).toContain('Ministry of Forests')
-  })
+    expect(wrapper.html()).not.toBe("");
+    expect(wrapper.find("cds-button").exists()).toBe(true);
+    expect(wrapper.html()).toContain("Ministry of Forests");
+  });
 
-  it('renders the component when unauthenticated', async () => {
+  it("renders the component when unauthenticated", async () => {
     const session = {
       isLoggedIn: () => false,
-      logOut: vi.fn()
-    }
+      logOut: vi.fn(),
+    };
 
     const wrapper = mount(MainHeaderComponent, {
       global: {
         mocks: {
-          $session: session
-        }
-      }
-    })
+          $session: session,
+        },
+      },
+    });
 
-    expect(wrapper.html()).not.toBe('')
-    expect(wrapper.find('bx-btn').exists()).toBe(false)
-    expect(wrapper.html()).toContain('Ministry of Forests')
-  })
-})
+    expect(wrapper.html()).not.toBe("");
+    expect(wrapper.find("bx-btn").exists()).toBe(false);
+    expect(wrapper.html()).toContain("Ministry of Forests");
+  });
+});
