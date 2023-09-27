cf5d692b268c0f354c6af65955a552e5db83e912
Maria Martinez <maria.martinez@gov.bc.ca>
2023-09-27 12:48:54 -0700

    Made code reviews

--- frontend/src/components/grouping/AddressGroupComponent.vue
+++ frontend/src/components/grouping/AddressGroupComponent.vue
@@ -1,96 +1,95 @@
 <script setup lang="ts">
-import { reactive, watch, computed, ref, onMounted } from 'vue'
+import { reactive, watch, computed, ref, onMounted } from "vue";
 // Carbon
-import '@carbon/web-components/es/components/button/index';
+import "@carbon/web-components/es/components/button/index";
 // Composables
-import { useEventBus } from '@vueuse/core'
-import { useFetchTo } from '@/composables/useFetch'
-import { useFocus } from '@/composables/useFocus'
+import { useEventBus } from "@vueuse/core";
+import { useFetchTo } from "@/composables/useFetch";
+import { useFocus } from "@/composables/useFocus";
 // Types
-import type { CodeNameType, BusinessSearchResult } from '@/dto/CommonTypesDto'
-import type { Address } from '@/dto/ApplyClientNumberDto'
+import type { CodeNameType, BusinessSearchResult } from "@/dto/CommonTypesDto";
+import type { Address } from "@/dto/ApplyClientNumberDto";
 // Validators
-import { getValidations } from '@/helpers/validators/GlobalValidators'
-import { submissionValidation } from '@/helpers/validators/SubmissionValidators'
+import { getValidations } from "@/helpers/validators/GlobalValidators";
+import { submissionValidation } from "@/helpers/validators/SubmissionValidators";
 // @ts-ignore
-import Delete16 from '@carbon/icons-vue/es/trash-can/16'
+import Delete16 from "@carbon/icons-vue/es/trash-can/16";
 
 //Define the input properties for this component
 const props = defineProps<{
-  id: number
-  modelValue: Address
-  countryList: Array<CodeNameType>
-  validations: Array<Function>
-  revalidate?: boolean
-}>()
+  id: number;
+  modelValue: Address;
+  countryList: Array<CodeNameType>;
+  validations: Array<Function>;
+  revalidate?: boolean;
+}>();
 
 //Events we emit during component lifecycle
 const emit = defineEmits<{
-  (e: 'valid', value: boolean): void
-  (e: 'update:model-value', value: Address | undefined): void
-  (e: 'remove', value: number): void
-}>()
+  (e: "valid", value: boolean): void;
+  (e: "update:model-value", value: Address | undefined): void;
+  (e: "remove", value: number): void;
+}>();
 
-const generalErrorBus = useEventBus<string>('general-error-notification')
+const generalErrorBus = useEventBus<string>("general-error-notification");
 const { setFocusedComponent } = useFocus();
 
-const noValidation = (value: string) => ''
+const noValidation = (value: string) => "";
 
 //We set it as a separated ref due to props not being updatable
-const selectedValue = reactive<Address>(props.modelValue)
+const selectedValue = reactive<Address>(props.modelValue);
 const validateAddressData =
   props.validations.length === 0
     ? noValidation
-    : props.validations[0]('Address', props.id + '')
+    : props.validations[0]("Address", props.id + "");
 const validateAddressNameData =
   props.validations.length === 0
     ? noValidation
-    : props.validations[0]('Names', props.id + '')
-const addressError = ref<string | undefined>('')
-const nameError = ref<string | undefined>('')
-const showDetailsLoading = ref<boolean>(false)
+    : props.validations[0]("Names", props.id + "");
+const addressError = ref<string | undefined>("");
+const nameError = ref<string | undefined>("");
+const showDetailsLoading = ref<boolean>(false);
 
 const uniquenessValidation = () => {
   addressError.value = validateAddressData(
     `${selectedValue.streetAddress} ${selectedValue.country.value} ${selectedValue.province.value} ${selectedValue.city} ${selectedValue.city} ${selectedValue.postalCode}`
-  )
-  nameError.value = validateAddressNameData(selectedValue.locationName)
-  
-}
+  );
+  nameError.value = validateAddressNameData(selectedValue.locationName);
+};
 
 //Watch for changes on the input
-watch([selectedValue], () =>{ 
-  uniquenessValidation()
-  emit('update:model-value', selectedValue)
-})
+watch([selectedValue], () => {
+  uniquenessValidation();
+  emit("update:model-value", selectedValue);
+});
 
 watch(
   () => props.revalidate,
   () => uniquenessValidation(),
   { immediate: true }
-)
+);
 
 const updateStateProvince = (
   value: CodeNameType | undefined,
   property: string
 ) => {
-  if (value && (property === 'country' || property === 'province')) {
-    selectedValue[property] = { value: value.code, text: value.name }
+  if (value && (property === "country" || property === "province")) {
+    selectedValue[property] = { value: value.code, text: value.name };
   }
-}
+};
 
 //Province related data
 const provinceUrl = computed(
   () =>
     `/api/clients/activeCountryCodes/${selectedValue.country.value}?page=0&size=250`
-)
+);
 
 const resetProvinceOnChange = (receivedCountry: any) => {
   if (selectedValue.country.value !== receivedCountry && receivedCountry)
-    selectedValue.province = { value: '', text: '' }
-}
+    selectedValue.province = { value: "", text: "" };
+};
 
-const addressControl = ref(false)
+const addressControl = ref(false);
 
 //Validations
 const validation = reactive<Record<string, boolean>>({
@@ -99,133 +98,142 @@ const validation = reactive<Record<string, boolean>>({
   country: false,
   province: false,
   city: false,
-  postalCode: false
-})
+  postalCode: false,
+});
 
 const checkValid = () =>
   Object.values(validation).reduce(
     (accumulator: boolean, currentValue: boolean) =>
       accumulator && currentValue,
     true
-  )
+  );
 
-watch([validation], () => emit('valid', checkValid()))
-emit('valid', false)
+watch([validation], () => emit("valid", checkValid()));
+emit("valid", false);
 
 const postalCodeValidators = computed(() => {
   switch (selectedValue.country.value) {
-    case 'CA':
+    case "CA":
       return [
-      ...getValidations('location.addresses.*.postalCode(location.addresses.*.country.text === "CA")'),
-        submissionValidation(`location.addresses[${props.id}].postalCode`)
-      ]
-    case 'US':
+        ...getValidations(
+          'location.addresses.*.postalCode(location.addresses.*.country.text === "CA")'
+        ),
+        submissionValidation(`location.addresses[${props.id}].postalCode`),
+      ];
+    case "US":
       return [
-      ...getValidations('location.addresses.*.postalCode(location.addresses.*.country.text === "US")'),
-        submissionValidation(`location.addresses[${props.id}].postalCode`)
-      ]
+        ...getValidations(
+          'location.addresses.*.postalCode(location.addresses.*.country.text === "US")'
+        ),
+        submissionValidation(`location.addresses[${props.id}].postalCode`),
+      ];
     default:
       return [
-        ...getValidations('location.addresses.*.postalCode(location.addresses.*.country.text !== "CA" && location.addresses.*.country.text !== "US")'),
-        submissionValidation(`location.addresses[${props.id}].postalCode`)
-      ]
+        ...getValidations(
+          'location.addresses.*.postalCode(location.addresses.*.country.text !== "CA" && location.addresses.*.country.text !== "US")'
+        ),
+        submissionValidation(`location.addresses[${props.id}].postalCode`),
+      ];
   }
-})
+});
 
 const postalCodeMask = computed(() => {
   switch (selectedValue.country.value) {
-    case 'CA':
-      return 'A#A#A#'
-    case 'US':
-      return ['#####','#####-####']
+    case "CA":
+      return "A#A#A#";
+    case "US":
+      return ["#####", "#####-####"];
     default:
-      return '##########'
+      return "##########";
   }
-})
+});
 
-const postalCodeShowHint = ref(false)
+const postalCodeShowHint = ref(false);
 
 const postalCodePlaceholder = computed(() => {
-  if(postalCodeShowHint.value) {
+  if (postalCodeShowHint.value) {
     switch (selectedValue.country.value) {
-      case 'CA':
-        return 'Use A1A1A1 format'
-      case 'US':
-        return 'Use 00000 or 00000-0000 format'
+      case "CA":
+        return "Use A1A1A1 format";
+      case "US":
+        return "Use 00000 or 00000-0000 format";
       default:
-        return 'Use 00000 format'
+        return "Use 00000 format";
     }
-  }else{
-    return ''
+  } else {
+    return "";
   }
-})
+});
 
 const provinceNaming = computed(() => {
   switch (selectedValue.country.value) {
-    case 'CA':
-      return 'Province or territory'
-    case 'US':
-      return 'State'
+    case "CA":
+      return "Province or territory";
+    case "US":
+      return "State";
     default:
-      return 'Province/territory or state'
+      return "Province/territory or state";
   }
-})
+});
 
 const postalCodeNaming = computed(() =>
-  selectedValue.country.value === 'US' ? 'Zip code' : 'Postal code'
-)
+  selectedValue.country.value === "US" ? "Zip code" : "Postal code"
+);
 
 const autoCompleteUrl = computed(
   () =>
     `/api/clients/addresses?country=${selectedValue.country.value}&maxSuggestions=10&searchTerm=${selectedValue.streetAddress}`
-)
-const autoCompleteResult = ref<BusinessSearchResult|undefined>({} as BusinessSearchResult)
-const detailsData = ref<Address | null>(null)
+);
+const autoCompleteResult = ref<BusinessSearchResult | undefined>(
+  {} as BusinessSearchResult
+);
+const detailsData = ref<Address | null>(null);
 
 watch([autoCompleteResult], () => {
-  addressControl.value = false
+  addressControl.value = false;
   if (autoCompleteResult.value) {
-    showDetailsLoading.value = true
+    showDetailsLoading.value = true;
     const { error, loading: detailsLoading } = useFetchTo(
       `/api/clients/addresses/${encodeURIComponent(
         autoCompleteResult.value.code
       )}`,
       detailsData,
       {}
-    )
+    );
 
     watch([error], () => {
       // @ts-ignore
-      generalErrorBus.emit(error.response?.data.message)
-      postalCodeShowHint.value = true
-    })
+      generalErrorBus.emit(error.response?.data.message);
+      postalCodeShowHint.value = true;
+    });
 
     watch(
       [detailsLoading],
       () => (showDetailsLoading.value = detailsLoading.value)
-    )
+    );
   }
-})
+});
 
 watch([detailsData], () => {
   if (detailsData.value) {
-    selectedValue.streetAddress = detailsData.value.streetAddress.trim()
-    selectedValue.city = detailsData.value.city
-    selectedValue.province = detailsData.value.province
-    selectedValue.postalCode = detailsData.value.postalCode.replace(/\s/g, '')
-    if(!selectedValue.postalCode) postalCodeShowHint.value = true
-    else postalCodeShowHint.value = false
-    addressControl.value = true
+    console.log(JSON.stringify(detailsData.value));
+    selectedValue.streetAddress = detailsData.value.streetAddress.trim();
+    selectedValue.city = detailsData.value.city;
+    selectedValue.province = detailsData.value.province;
+    selectedValue.postalCode = detailsData.value.postalCode.replace(/\s/g, "");
+    if (!selectedValue.postalCode) postalCodeShowHint.value = true;
+    else postalCodeShowHint.value = false;
+    addressControl.value = true;
     //Why is this here? Because Address response differs from the street address value
     //addressControl is responsible for giving an empty list instead of the AC value
-    setTimeout(() => (addressControl.value = false), 200)
+    setTimeout(() => (addressControl.value = false), 200);
   }
-})
+});
 
-onMounted(() =>{
-  if(props.id == 0) setFocusedComponent(`addr_${props.id}`,800)
-  else setFocusedComponent(`name_${props.id}`,200)
-})
+onMounted(() => {
+  if (props.id == 0) setFocusedComponent(`addr_${props.id}`, 800);
+  else setFocusedComponent(`name_${props.id}`, 200);
+});
 </script>
 
 <template>
