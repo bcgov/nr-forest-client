2e8fce94d47c2830daab53a0d81ce4a2353ff283
Fernando Terra <fernando.terra@encora.com>
2023-10-04 20:09:39 -0300

    test: emit valid events properly

--- frontend/tests/components/components/grouping/ContactGroupComponent.cy.ts
+++ frontend/tests/components/components/grouping/ContactGroupComponent.cy.ts
@@ -1,6 +1,9 @@
 import ContactGroupComponent from '@/components/grouping/ContactGroupComponent.vue'
 import type { Contact } from '@/dto/ApplyClientNumberDto'
 
+// load app validations
+import '@/helpers/validators/BCeIDFormValidations'
+
 describe('<ContactGroupComponent />', () => {
   const dummyValidation = (): ((key: string, field: string) => (value: string) => string) => {
     return (key: string, fieldId: string) => (value: string) => {
@@ -207,5 +210,123 @@ describe('<ContactGroupComponent />', () => {
       .click()
 
     cy.get('#addressname_0').should('be.visible').and('have.value', '')
+  })
+
+  describe('when it has last emitted "valid" with false due to a single, not empty, invalid field', () => {
+    const genericTest = (
+      fieldSelector: string,
+      firstContent: string,
+      additionalContent: string,
+      expectedFinalValue = `${firstContent}${additionalContent}`,
+    ) => {
+      const calls: boolean[] = []
+      const onValid = (valid: boolean) => {
+        calls.push(valid)
+      }
+      cy.get('@contactFixture').then((contact: Contact) => {
+        cy.get('@rolesFixture').then((roles) => {
+          cy.get('@addressesFixture').then((addresses) => {
+            cy.mount(ContactGroupComponent, {
+              props: {
+                id: 1,
+                modelValue: {
+                  ...contact,
+                  locationNames: addresses.map((a) => ({ value: a.code, text: a.name })),
+                  email: 'a@b.com',
+                  phoneNumber: '(123) 123-1234',
+                  contactType: { value: roles[0].code, text: roles[0].name },
+                },
+                enabled: true,
+                roleList: roles,
+                addressList: addresses,
+                validations: [],
+                onValid,
+              },
+            })
+          })
+        })
+      })
+
+      cy.get(fieldSelector).shadow().find('input').clear() // emits false
+      cy.get(fieldSelector).blur() // (doesn't emit)
+      cy.get(fieldSelector).shadow().find('input').type(firstContent) // emits true before blurring
+      cy.get(fieldSelector).blur() // emits false
+      cy.get(fieldSelector).should('be.visible').and('have.value', firstContent)
+      cy.get(fieldSelector).shadow().find('input').type(additionalContent) // emits true (last2)
+      cy.get(fieldSelector).blur() // emits false (last1)
+      cy.get(fieldSelector).should('be.visible').and('have.value', expectedFinalValue)
+
+      // For some reason on Electron we need to wait a millisecond now.
+      // Otherwise this test either fails or can't be trusted on Electron.
+      cy.wait(1)
+
+      return calls
+    }
+    const checkValidFalseAgain = (calls: boolean[]) => {
+      cy.wrap(calls).then((value) => {
+        const last1 = value.pop()
+        const last2 = value.pop()
+        const last3 = value.pop()
+        expect(last3).to.equal(false)
+        expect(last2).to.equal(true)
+        expect(last1).to.equal(false)
+      })
+    }
+    const checkValidTrue = (calls: boolean[]) => {
+      cy.wrap(calls).then((value) => {
+        const last1 = value.pop()
+        const last2 = value.pop()
+        expect(last2).to.equal(false)
+        expect(last1).to.equal(true)
+      })
+    }
+    describe('First name', () => {
+      it('should emit "valid" with false again', () => {
+        const calls = genericTest('#firstName_1', 'A', '{backspace}', '')
+
+        checkValidFalseAgain(calls)
+      })
+      it('should emit "valid" with true', () => {
+        const calls = genericTest('#firstName_1', 'A', '{backspace}B', 'B')
+
+        checkValidTrue(calls)
+      })
+    })
+    describe('Last name', () => {
+      it('should emit "valid" with false again', () => {
+        const calls = genericTest('#lastName_1', 'A', '{backspace}', '')
+
+        checkValidFalseAgain(calls)
+      })
+      it('should emit "valid" with true', () => {
+        const calls = genericTest('#lastName_1', 'A', '{backspace}B', 'B')
+
+        checkValidTrue(calls)
+      })
+    })
+    describe('Email address', () => {
+      it('should emit "valid" with false again', () => {
+        const calls = genericTest('#email_1', 'a@', 'b')
+
+        checkValidFalseAgain(calls)
+      })
+      it('should emit "valid" with true', () => {
+        const calls = genericTest('#email_1', 'a@', 'b.com')
+
+        checkValidTrue(calls)
+      })
+    })
+    describe('Phone number', () => {
+      it('should emit "valid" with false again', () => {
+        const calls = genericTest('#phoneNumber_1', '(123) 123', '-1')
+
+        checkValidFalseAgain(calls)
+      })
+      it('should emit "valid" with true', () => {
+        const calls = genericTest('#phoneNumber_1', '(123) 123', '-1234')
+
+        checkValidTrue(calls)
+      })
+    })
   })
 })
