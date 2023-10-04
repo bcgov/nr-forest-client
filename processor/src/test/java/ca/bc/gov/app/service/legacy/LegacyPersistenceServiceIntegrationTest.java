d83a97d283287ee6e4de93f9801f32dddb0ccb99
Paulo Gomes da Cruz Junior <paulo.cruz@gov.bc.ca>
2023-10-04 13:01:50 -0700

    test: adding tests for legacy persistence

--- processor/src/test/java/ca/bc/gov/app/service/legacy/LegacyPersistenceServiceIntegrationTest.java
+++ processor/src/test/java/ca/bc/gov/app/service/legacy/LegacyPersistenceServiceIntegrationTest.java
@@ -39,17 +39,61 @@ class LegacyPersistenceServiceIntegrationTest extends AbstractTestContainer {
 
           assertThat(message.getHeaders().get(ApplicationConstant.CREATED_BY))
               .isNotNull()
-              .isInstanceOf(String.class)
-              .isEqualTo(ApplicationConstant.PROCESSOR_USER_NAME);
+              .isInstanceOf(String.class);
 
           assertThat(message.getHeaders().get(ApplicationConstant.UPDATED_BY))
               .isNotNull()
-              .isInstanceOf(String.class)
-              .isEqualTo(ApplicationConstant.PROCESSOR_USER_NAME);
+              .isInstanceOf(String.class);
 
         })
         .verifyComplete();
 
+  }
+
+  @Test
+  void shouldCreateForestClient(){
+    service
+        .createForestClient(
+            MessageBuilder
+            .withPayload(2)
+            .build()
+        )
+        .as(StepVerifier::create)
+        .assertNext(message -> {
+          assertThat(message)
+              .isNotNull()
+              .hasFieldOrPropertyWithValue("payload", 2);
+
+          assertThat(message.getHeaders().get(ApplicationConstant.SUBMISSION_ID))
+              .isNotNull()
+              .isInstanceOf(Integer.class)
+              .isEqualTo(2);
+
+          assertThat(message.getHeaders().get(ApplicationConstant.CREATED_BY))
+              .isNotNull()
+              .isInstanceOf(String.class);
+
+          assertThat(message.getHeaders().get(ApplicationConstant.UPDATED_BY))
+              .isNotNull()
+              .isInstanceOf(String.class);
+
+          assertThat(message.getHeaders().get(ApplicationConstant.FOREST_CLIENT_NAME))
+              .isNotNull()
+              .isInstanceOf(String.class)
+              .isEqualTo("Billy");
+
+          assertThat(message.getHeaders().get(ApplicationConstant.INCORPORATION_NUMBER))
+              .isNotNull()
+              .isInstanceOf(String.class)
+              .isEqualTo("123456789");
+
+          assertThat(message.getHeaders().get(ApplicationConstant.FOREST_CLIENT_NUMBER))
+              .isNotNull()
+              .isInstanceOf(String.class)
+              .isEqualTo("123456789");
+
+        })
+        .verifyComplete();
   }
 
 }
