a6207d2a1c50c29eabd3fa408fce4459c77690fe
Paulo Gomes da Cruz Junior <paulo.cruz@gov.bc.ca>
2023-10-17 15:13:14 -0700

    fix: fixing fuzzy query

--- processor/src/main/java/ca/bc/gov/app/repository/legacy/ForestClientRepository.java
+++ processor/src/main/java/ca/bc/gov/app/repository/legacy/ForestClientRepository.java
@@ -17,13 +17,18 @@ public interface ForestClientRepository extends ReactiveCrudRepository<ForestCli
       SELECT *
       FROM THE.FOREST_CLIENT
       WHERE
-      UTL_MATCH.JARO_WINKLER_SIMILARITY(UPPER(CLIENT_NAME),UPPER(:companyName)) >= 85""")
+      UTL_MATCH.JARO_WINKLER_SIMILARITY(UPPER(CLIENT_NAME),UPPER(:companyName)) >= 85
+      AND CLIENT_STATUS_CODE = 'ACT'
+      ORDER BY CLIENT_NUMBER""")
   Flux<ForestClientEntity> matchBy(String companyName);
 
   @Query("""
       SELECT *
       FROM THE.FOREST_CLIENT
-      WHERE (REGISTRY_COMPANY_TYPE_CODE || CORP_REGN_NMBR) = :incorporationNumber""")
+      WHERE
+      (REGISTRY_COMPANY_TYPE_CODE || CORP_REGN_NMBR) = :incorporationNumber
+      AND CLIENT_STATUS_CODE = 'ACT'
+      ORDER BY CLIENT_NUMBER""")
   Flux<ForestClientEntity> findByIncorporationNumber(String incorporationNumber);
 
 }
