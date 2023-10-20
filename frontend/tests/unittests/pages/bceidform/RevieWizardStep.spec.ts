32a418bff1614a54441cff7a0a29edef62ea5851
Maria Martinez <maria.martinez@gov.bc.ca>
2023-10-20 08:59:26 -0700

    Renamed file as it had a typo

--- frontend/tests/unittests/pages/bceidform/RevieWizardStep.spec.ts
+++ frontend/tests/unittests/pages/bceidform/RevieWizardStep.spec.ts
@@ -1,6 +1,6 @@
 // Import the necessary dependencies
 import { mount } from "@vue/test-utils";
-import { describe, it, expect } from "vitest";
+import { describe, it } from "vitest";
 import ReviewWizardStep from "@/pages/bceidform/ReviewWizardStep.vue";
 
 describe("ReviewWizardStep.vue", () => {
