398abe06670c800e192e1917ddb02f4c1a1b058b
Fernando Terra <79578735+fterra-encora@users.noreply.github.com>
2023-11-17 15:18:55 -0300

    [FSADT1-930] Some UI/UX adjustments that were missing in the first PR (#615)
    * fix: adjust width of panel My profile
    
    * fix: deactivate Options link
    
    * fix: prevent animation issue while closing panel
    
    * feat: add cds-side-nav-link states
    
    * feat: set row cursor
    
    * fix: divide table row width evenly among columns
    
    * fix: align submission type icon to the center
    
    * fix: set data-table borders and background colors
    
    * refactor: add missing css variables
    
    * fix: set table's header and pagination background
    
    * fix: set breadcrumb link state colors
    
    * feat: add backdrop to header panel
    
    * test: assert the backdrop is properly displayed
    
    * fix: set My profile panel background
    
    * feat: close My profile panel on backdrop click
    
    * fix: allow the left side nav to be covered by the backdrop
    
    * fix: remove trailing white-space in link
    
    * fix: show side nav drawer above the main content
    
    * Made code reviews
    
    ---------
    
    Co-authored-by: Paulo Gomes da Cruz Junior <paulo.cruz@gov.bc.ca>
    Co-authored-by: Maria Martinez <maria.martinez@gov.bc.ca>
    Co-authored-by: Maria Martinez <77364706+mamartinezmejia@users.noreply.github.com>

--- frontend/tests/unittests/components/MainHeaderComponent.spec.ts
+++ frontend/tests/unittests/components/MainHeaderComponent.spec.ts
@@ -1,14 +1,15 @@
-import { describe, it, expect, vi } from "vitest";
+import { describe, it, expect, vi, beforeEach, afterEach } from "vitest";
 import { mount } from "@vue/test-utils";
 
 import MainHeaderComponent from "@/components/MainHeaderComponent.vue";
+import { nextTick } from "vue";
 
 describe("MainHeaderComponent.vue", () => {
   const mockRoute = {
     path: "/",
-    meta:{
-      profile: false
-    }
+    meta: {
+      profile: false,
+    },
   };
   describe("Authenticated Scenario", () => {
     const session = {
@@ -21,9 +22,8 @@ describe("MainHeaderComponent.vue", () => {
     };
 
     it("renders the component correctly when authenticated", async () => {
-
       const wrapper = mount(MainHeaderComponent, {
-        global: {          
+        global: {
           mocks: {
             $session: session,
             $route: mockRoute,
@@ -44,41 +44,76 @@ describe("MainHeaderComponent.vue", () => {
         },
       };
 
-      it("closes the panel when the inner close button is clicked", async () => {
-        const wrapper = mount(MainHeaderComponent, {
-          global: {
-            mocks: {
-              $session: session,
-              $route: mockRoute,
+      describe("My profile panel", () => {
+        let wrapper: ReturnType<typeof mount<typeof MainHeaderComponent>>;
+        beforeEach(async () => {
+          wrapper = mount(MainHeaderComponent, {
+            global: {
+              mocks: {
+                $session: session,
+                $route: mockRoute,
+              },
             },
-          },
-          attachTo: document.body, // fixes the click event on the web component
+            attachTo: document.body, // fixes the click event on the web component
+          });
+
+          // The test fails without this. It's probably something related to the use of MutationObserver.
+          await nextTick();
+
+          expect(wrapper.html()).toBeTruthy();
+          expect(wrapper.html()).toContain("Client Management System");
+
+          const panelAction = wrapper.find("[data-testid='panel-action']");
+
+          // patch stuff missing in the web component in this test environment
+          Object.defineProperty(panelAction.element, "_buttonNode", {
+            value: {
+              classList: {
+                add: () => {},
+                remove: () => {},
+              },
+            },
+          });
+
+          expect(panelAction.exists()).toBe(true);
+          await panelAction.trigger("click");
+          const panel = wrapper.find("#my-profile-panel");
+          expect(panel.attributes().expanded).toEqual("true");
+        });
+        afterEach(() => {
+          wrapper.unmount();
+        });
+        it("closes the panel when the inner close button is clicked", async () => {
+          const button = wrapper.find(".close-panel-button");
+          expect(button.exists()).toBe(true);
+          await button.trigger("click");
+          const panel = wrapper.find("#my-profile-panel");
+          expect(panel.attributes().expanded).toBeUndefined();
         });
 
-        expect(wrapper.html()).toBeTruthy();
-        expect(wrapper.html()).toContain("Client Management System");
-
-        const panelAction = wrapper.find("[data-testid='panel-action']");
-
-        // patch stuff missing in the web component in this test environment
-        Object.defineProperty(panelAction.element, "_buttonNode", {
-          value: {
-            classList: {
-              add: () => {},
-              remove: () => {},
-            },
-          },
+        it("closes the panel when the backdrop is clicked", async () => {
+          const backdrop = wrapper.find("[data-testid='my-profile-backdrop']");
+          expect(backdrop.exists()).toBe(true);
+          await backdrop.trigger("click");
+          const panel = wrapper.find("#my-profile-panel");
+          expect(panel.attributes().expanded).toBeUndefined();
         });
 
-        expect(panelAction.exists()).toBe(true);
-        await panelAction.trigger("click");
-        const panel = wrapper.find("#apanel");
-        expect(panel.attributes().expanded).toEqual("true");
+        it("displays a backdrop only when the panel is open", async () => {
+          const backdrop = wrapper.find("[data-testid='my-profile-backdrop']");
 
-        const button = wrapper.find(".close-panel-button");
-        expect(button.exists()).toBe(true);
-        await button.trigger("click");
-        expect(panel.attributes().expanded).toBeUndefined();
+          // backdrop is active
+          expect(backdrop.classes()).toContain("overlay-active");
+
+          const button = wrapper.find(".close-panel-button");
+          expect(button.exists()).toBe(true);
+          await button.trigger("click");
+          const panel = wrapper.find("#my-profile-panel");
+          expect(panel.attributes().expanded).toBeUndefined();
+
+          // backdrop is not active
+          expect(backdrop.classes()).not.toContain("overlay-active");
+        });
       });
     });
   });
