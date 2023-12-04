300a942809203bdb693dbf32c62849e50ac1f0fc
Paulo Gomes da Cruz Junior <paulo.cruz@gov.bc.ca>
2023-12-04 15:16:16 -0800

    test: updating cypress commands

--- frontend/cypress/support/cypress.d.ts
+++ frontend/cypress/support/cypress.d.ts
@@ -1,14 +1,13 @@
 declare namespace Cypress {
   interface Chainable<Subject> {
-    login(email: string, name: string): Chainable<void>;
-      addCookie(name: string, value: string): Chainable<void>;
-      addToLocalStorage(key: string, value: any): Chainable<void>;
-      expireLocalStorage(key: string): Chainable<void>;
-      addToSessionStorage(key: string, value: any): Chainable<void>;
-      expireSessionStorage(key: string): Chainable<void>;
-      expireCookie(name: string): Chainable<void>;
-      login(email: string, name: string, provider: string): Chainable<void>;
-      logout(): Chainable<void>;
-      getMany(names: string[]): Chainable<any[]>;
+    addCookie(name: string, value: string): Chainable<void>;
+    addToLocalStorage(key: string, value: any): Chainable<void>;
+    expireLocalStorage(key: string): Chainable<void>;
+    addToSessionStorage(key: string, value: any): Chainable<void>;
+    expireSessionStorage(key: string): Chainable<void>;
+    expireCookie(name: string): Chainable<void>;
+    login(email: string, name: string, provider: string, extras: any): Chainable<void>;
+    logout(): Chainable<void>;
+    getMany(names: string[]): Chainable<any[]>;
   }
 }
