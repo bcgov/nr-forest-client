67f7b7f1703f9611c0f90ed4510123246153e5a4
Paulo Gomes da Cruz Junior <paulo.cruz@gov.bc.ca>
2023-12-18 11:31:26 -0800

    chore: removing swagger from legacy

--- legacy/src/main/java/ca/bc/gov/app/controller/ClientSearchController.java
+++ legacy/src/main/java/ca/bc/gov/app/controller/ClientSearchController.java
@@ -2,14 +2,6 @@ package ca.bc.gov.app.controller;
 
 import ca.bc.gov.app.dto.ForestClientDto;
 import ca.bc.gov.app.service.ClientSearchService;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.media.ArraySchema;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.ExampleObject;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
@@ -21,10 +13,6 @@ import reactor.core.publisher.Flux;
 
 @RestController
 @Slf4j
-@Tag(
-    name = "Search Client",
-    description = "Search clients based on some criterias"
-)
 @RequestMapping(value = "/api/search", produces = MediaType.APPLICATION_JSON_VALUE)
 @RequiredArgsConstructor
 public class ClientSearchController {
@@ -32,42 +20,8 @@ public class ClientSearchController {
   private final ClientSearchService service;
 
   @GetMapping("/incorporationOrName")
-  @Operation(
-      summary = "List forest client entries by incorporation number or name",
-      responses = {
-          @ApiResponse(
-              responseCode = "200",
-              description = "Returns a client based on it's number",
-              content = @Content(
-                  mediaType = MediaType.APPLICATION_JSON_VALUE,
-                  array = @ArraySchema(
-                      schema = @Schema(
-                          name = "ForestClient",
-                          implementation = ForestClientDto.class
-                      )
-                  )
-              )
-          ),
-          @ApiResponse(
-              responseCode = "412",
-              description = "Missing value for parameter incorporationNumber or companyName",
-              content = @Content(
-                  mediaType = MediaType.APPLICATION_JSON_VALUE,
-                  schema = @Schema(implementation = String.class),
-                  examples = {
-                      @ExampleObject(
-                          value =
-                              "Missing value for parameter incorporationNumber or companyName")
-                  }
-              )
-          )
-      }
-  )
   public Flux<ForestClientDto> findByIncorporationOrName(
-      @Parameter(description = "The incorporation ID to lookup", example = "BC0772006")
       @RequestParam(required = false) String incorporationNumber,
-
-      @Parameter(description = "The name to lookup", example = "Power Corp")
       @RequestParam(required = false) String companyName
   ) {
     return service
