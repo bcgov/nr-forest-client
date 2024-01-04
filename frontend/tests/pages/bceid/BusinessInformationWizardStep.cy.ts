137ae23a6a91724993052bf5eed909589cda44b1
Maria Martinez <maria.martinez@gov.bc.ca>
2024-01-04 10:18:46 -0800

    - Upgraded Cypress version - Removed unneeded code

--- frontend/tests/pages/bceid/BusinessInformationWizardStep.cy.ts
+++ frontend/tests/pages/bceid/BusinessInformationWizardStep.cy.ts
@@ -2,10 +2,7 @@ import BusinessInformationWizardStep from "@/pages/bceidform/BusinessInformation
 import type { FormDataDto } from "@/dto/ApplyClientNumberDto";
 
 describe('<BusinessInformationWizardStep />', () => {
-  beforeEach(() => {
-    cy.intercept('/api/clients/name/*').as('autoCompleteRequest');
-  });
-
+  
   it('renders the BusinessInformationWizardStep component and interacts with elements', () => {
     cy.mount(BusinessInformationWizardStep, {
       props: {
@@ -26,9 +23,7 @@ describe('<BusinessInformationWizardStep />', () => {
     });
 
     cy.get('#businessTyperbR').click();
-    
-    cy.get('#bcRegistryEmailId').click();
-
+ 
     cy.get('.link-button:visible').should('have.attr', 'aria-label', 'Contact BC Registry via Email');
     cy.get('.link-button:visible span').should('exist');
     cy.get('.link-button:visible span').invoke('text').should('not.be.empty');
