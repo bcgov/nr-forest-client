993567951bc29cd179070aab146b8e550d289c52
Maria Martinez <maria.martinez@gov.bc.ca>
2024-01-15 21:20:28 -0800

    Gave format

--- legacy/src/main/java/ca/bc/gov/app/dto/ForestClientDto.java
+++ legacy/src/main/java/ca/bc/gov/app/dto/ForestClientDto.java
@@ -7,32 +7,31 @@ import java.util.stream.Stream;
 import org.springframework.data.annotation.Transient;
 
 public record ForestClientDto(
-    String clientNumber,
-    String clientName,
-    String legalFirstName,
-    String legalMiddleName,
-    String clientStatusCode,
-    String clientTypeCode,
-    LocalDate birthdate,
-    String clientIdTypeCode,
-    String clientIdentification,
-    String registryCompanyTypeCode,
-    String corpRegnNmbr,
-    String clientComment,
-    String createdBy,
-    String updatedBy,
-    Long orgUnit
-) {
+	String clientNumber, 
+	String clientName, 
+	String legalFirstName, 
+	String legalMiddleName,
+	String clientStatusCode, 
+	String clientTypeCode, 
+	LocalDate birthdate, 
+	String clientIdTypeCode,
+	String clientIdentification, 
+	String registryCompanyTypeCode, 
+	String corpRegnNmbr, 
+	String clientComment,
+	String createdBy,
+	String updatedBy, 
+	Long orgUnit) {
 
-  @Transient
-  public String name(){
-    if(Objects.equals(this.clientTypeCode, "I")){
-      return Stream.of(this.legalFirstName, this.legalMiddleName, this.clientName)
-          .filter(Objects::nonNull)
-          .collect(Collectors.joining(" "));
-    }else{
-      return this.clientName;
-    }
-  }
+	@Transient
+	public String name() {
+		if (Objects.equals(this.clientTypeCode, "I")) {
+			return Stream.of(this.legalFirstName, this.legalMiddleName, this.clientName)
+					.filter(Objects::nonNull)
+					.collect(Collectors.joining(" "));
+		} else {
+			return this.clientName;
+		}
+	}
 
 }
