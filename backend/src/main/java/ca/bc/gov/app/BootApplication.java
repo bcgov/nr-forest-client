d3ff30ee235729ab98ac8b1770bbc36c1a4098a6
Paulo Gomes da Cruz Junior <paulo.cruz@gov.bc.ca>
2024-01-19 16:18:35 -0800

    feat(FSADT1-992): adding observability

--- backend/src/main/java/ca/bc/gov/app/BootApplication.java
+++ backend/src/main/java/ca/bc/gov/app/BootApplication.java
@@ -1,9 +1,11 @@
 package ca.bc.gov.app;
 
+import org.slf4j.bridge.SLF4JBridgeHandler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import reactor.core.publisher.Hooks;
 
 @SpringBootApplication
 @EnableScheduling
@@ -11,6 +13,9 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class BootApplication {
 
   public static void main(String[] args) {
+    SLF4JBridgeHandler.removeHandlersForRootLogger();
+    SLF4JBridgeHandler.install();
+    Hooks.enableAutomaticContextPropagation();
     SpringApplication.run(BootApplication.class, args);
   }
 
