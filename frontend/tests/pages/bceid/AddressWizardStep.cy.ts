d8072da1537dd6e40e227e816c4ba923c8dabbc7
Fernando Terra <fernando.terra@encora.com>
2024-01-22 19:01:44 -0300

    test: add scenario with other addresses in props

--- frontend/tests/pages/bceid/AddressWizardStep.cy.ts
+++ frontend/tests/pages/bceid/AddressWizardStep.cy.ts
@@ -1,13 +1,14 @@
 import AddressWizardStep from "@/pages/bceidform/AddressWizardStep.vue";
+import { emptyAddress } from "@/dto/ApplyClientNumberDto";
 import type { Address, FormDataDto } from "@/dto/ApplyClientNumberDto";
 import type { ModalNotification } from "@/dto/CommonTypesDto";
 import { useEventBus } from "@vueuse/core";
 
 describe("<AddressWizardStep />", () => {
   beforeEach(() => {
-    cy.fixture("contact.json").as("contactFixture");
-    cy.fixture("roles.json").as("rolesFixture");
-    cy.fixture("addresses.json").as("addressesFixture");
+    cy.intercept("GET", `/api/clients/addresses?country=CA&maxSuggestions=10&searchTerm=*`, {
+      fixture: "addressSearch.json",
+    });
   });
 
   it("renders the AddressWizardStep component", () => {
@@ -62,9 +63,6 @@ describe("<AddressWizardStep />", () => {
         .should("have.value", "")
         .type(streetAddress);
 
-      // blurs in case any address options cover the city field.
-      cy.get(`#addr_${addressId}`).blur();
-
       cy.get(`#city_${addressId}`)
         .should("be.visible")
         .shadow()
@@ -82,65 +80,80 @@ describe("<AddressWizardStep />", () => {
 
     let bus: ReturnType<typeof useEventBus<ModalNotification>>;
 
-    beforeEach(() => {
-      bus = useEventBus<ModalNotification>("modal-notification");
-      bus.on((payload) => {
-        payload.handler(); // automatically proceed with the deletion
-      });
-      cy.mount(AddressWizardStep, {
-        props: {
-          data: {
-            location: {
-              addresses: [
-                {
-                  locationName: "Mailing address",
-                  streetAddress: "123 Forest Street",
-                  country: { value: "CA", text: "Canada" },
-                  province: { value: "BC", text: "British Columbia" },
-                  city: "Victoria",
-                  postalCode: "A0A0A0",
-                } as Address,
-              ],
-              contacts: [],
+    [
+      { includeOtherAddressesInProps: true, predicate: "are provided in the props" },
+      { includeOtherAddressesInProps: false, predicate: "are added manually" },
+    ].forEach(({ includeOtherAddressesInProps, predicate }) =>
+      describe(`when other addresses ${predicate}`, () => {
+        beforeEach(() => {
+          bus = useEventBus<ModalNotification>("modal-notification");
+          bus.on((payload) => {
+            payload.handler(); // automatically proceed with the deletion
+          });
+          cy.mount(AddressWizardStep, {
+            props: {
+              data: {
+                location: {
+                  addresses: [
+                    {
+                      locationName: "Mailing address",
+                      streetAddress: "123 Forest Street",
+                      country: { value: "CA", text: "Canada" },
+                      province: { value: "BC", text: "British Columbia" },
+                      city: "Victoria",
+                      postalCode: "A0A0A0",
+                    } as Address,
+                    ...(includeOtherAddressesInProps ? otherAddressNames : []).map(
+                      (locationName) => ({
+                        ...emptyAddress(),
+                        locationName,
+                      }),
+                    ),
+                  ],
+                  contacts: [],
+                },
+              } as FormDataDto,
+              active: false,
             },
-          } as FormDataDto,
-          active: false,
-        },
-      })
-        .its("wrapper")
-        .as("vueWrapper");
+          })
+            .its("wrapper")
+            .as("vueWrapper");
 
-      otherAddressNames.forEach((name, index) => {
-        addAddress(index + 1, name);
-      });
-    });
+          if (!includeOtherAddressesInProps) {
+            otherAddressNames.forEach((name, index) => {
+              addAddress(index + 1, name);
+            });
+          }
+        });
 
-    afterEach(() => {
-      bus.reset();
-    });
+        afterEach(() => {
+          bus.reset();
+        });
 
-    it("removes the intended address from the DOM", () => {
-      cy.get("#deleteAddress_1").click();
-      cy.wait(150);
-      cy.get("#name_1").should("not.exist");
-      cy.get("#name_2").should("exist");
-    });
+        it("removes the intended address from the DOM", () => {
+          cy.get("#deleteAddress_1").click();
+          cy.wait(150);
+          cy.get("#name_1").should("not.exist");
+          cy.get("#name_2").should("exist");
+        });
 
-    it("can submit the form (regardless of the deleted address being invalid)", () => {
-      cy.get("#deleteAddress_1").click();
-      cy.wait(150);
+        it("can submit the form (regardless of the deleted address being invalid)", () => {
+          cy.get("#deleteAddress_1").click();
+          cy.wait(150);
 
-      fillAddress(2, {
-        streetAddress: "456 Lumber Street",
-        postalCode: "B0B0B0",
-      });
+          fillAddress(2, {
+            streetAddress: "456 Lumber Street",
+            postalCode: "B0B0B0",
+          });
 
-      cy.get("@vueWrapper").should((vueWrapper) => {
-        const lastValid = vueWrapper.emitted("valid").slice(-1)[0];
+          cy.get("@vueWrapper").should((vueWrapper) => {
+            const lastValid = vueWrapper.emitted("valid").slice(-1)[0];
 
-        // The last valid event was emitted with true.
-        expect(lastValid[0]).to.equal(true);
-      })
-    });
+            // The last valid event was emitted with true.
+            expect(lastValid[0]).to.equal(true);
+          });
+        });
+      }),
+    );
   });
 });
