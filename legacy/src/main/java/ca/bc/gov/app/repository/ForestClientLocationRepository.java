45e8ff3e350c98a355cdbf207ef37f419cecc8ea
Maria Martinez <maria.martinez@gov.bc.ca>
2024-01-23 20:49:40 -0800

    Made code reviews

--- legacy/src/main/java/ca/bc/gov/app/repository/ForestClientLocationRepository.java
+++ legacy/src/main/java/ca/bc/gov/app/repository/ForestClientLocationRepository.java
@@ -2,25 +2,23 @@ package ca.bc.gov.app.repository;
 
 import ca.bc.gov.app.entity.ForestClientLocationEntity;
 import ca.bc.gov.app.entity.ForestClientLocationEntityKey;
-import reactor.core.publisher.Flux;
-
 import org.springframework.data.r2dbc.repository.Query;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Flux;
 
 @Repository
 public interface ForestClientLocationRepository
-		extends ReactiveCrudRepository<ForestClientLocationEntity, ForestClientLocationEntityKey> {
+    extends ReactiveCrudRepository<ForestClientLocationEntity, ForestClientLocationEntityKey> {
 
-	@Query("""
-			select *
-			from the.client_location
-			where
-			upper(address_1) like upper(concat(:address, '%')) 
-			or upper(address_2) like upper(concat(:address, '%')) 
-			or upper(address_3) like upper(concat(:address, '%')) 
-			or upper(postal_code) = upper(replace(:postalCode, ' ', ''))""")
-	Flux<ForestClientLocationEntity> matchBy(String address,
-											 String postalCode);
+  @Query("""
+      select *
+      from the.client_location
+      where
+      upper(address_1) like upper(concat(:address, '%'))
+      or upper(address_2) like upper(concat(:address, '%'))
+      or upper(address_3) like upper(concat(:address, '%'))
+      or upper(postal_code) = upper(replace(:postalCode, ' ', ''))""")
+  Flux<ForestClientLocationEntity> matchBy(String address, String postalCode);
 
 }
