b90fb17b5fe261119ca0210c3e9164db8294c76a
Fernando Terra <fernando.terra@encora.com>
2024-02-08 08:31:49 -0300

    feat: change style

--- frontend/src/pages/FormBCeIDPage.vue
+++ frontend/src/pages/FormBCeIDPage.vue
@@ -445,9 +445,7 @@ const scrollToNewContact = () => {
         </h4>
         
         <div class="form-steps-section-01">
-            <p class="body-01 heading-compact-01-dark">
-              This is the primary address where you will receive mail.
-            </p>
+          <h5>This is the primary address where you will receive mail.</h5>
         </div>
       
         <address-wizard-step
