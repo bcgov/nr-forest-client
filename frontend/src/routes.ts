7cad512aa4e7ea1872695479f319756a0dca8e8b
Maria Martinez <maria.martinez@gov.bc.ca>
2024-02-20 15:38:20 -0800

    Gave format to the code

--- frontend/src/routes.ts
+++ frontend/src/routes.ts
@@ -2,10 +2,10 @@
  * Router configuration for the application.
  */
 import { createRouter, createWebHistory } from "vue-router";
-import { useLocalStorage } from '@vueuse/core';
+import { useLocalStorage } from "@vueuse/core";
 
-import SubmissionList from '@/pages/SubmissionListPage.vue'
-import SubmissionReview from '@/pages/SubmissionReviewPage.vue'
+import SubmissionList from "@/pages/SubmissionListPage.vue";
+import SubmissionReview from "@/pages/SubmissionReviewPage.vue";
 import BCeIDForm from "@/pages/FormBCeIDPage.vue";
 import BCSCForm from "@/pages/FormBCSCPage.vue";
 import FormSubmittedPage from "@/pages/FormSubmittedPage.vue";
@@ -36,8 +36,8 @@ const routes = [
         bceidbusiness: "form",
         bcsc: "bcsc-form",
       },
-      style: 'content-landing',
-      headersStyle: 'headers',
+      style: "content-landing",
+      headersStyle: "headers",
       sideMenu: false,
       profile: false,
     },
@@ -59,7 +59,7 @@ const routes = [
         bcsc: "bcsc-form",
       },
       style: "content",
-      headersStyle: 'headers',
+      headersStyle: "headers",
       sideMenu: false,
       profile: false,
     },
@@ -81,7 +81,7 @@ const routes = [
         bceidbusiness: "form",
       },
       style: "content",
-      headersStyle: 'headers',
+      headersStyle: "headers",
       sideMenu: false,
       profile: false,
     },
@@ -101,49 +101,49 @@ const routes = [
         idir: "internal",
       },
       style: "content",
-      headersStyle: 'headers',
+      headersStyle: "headers",
       sideMenu: false,
       profile: false,
     },
   },
   {
-    path: '/submissions',
-    name: 'internal',
+    path: "/submissions",
+    name: "internal",
     component: SubmissionList,
     props: true,
     meta: {
-      format: 'full',
+      format: "full",
       hideHeader: false,
       requireAuth: true,
       showLoggedIn: true,
-      visibleTo: ['idir'],
+      visibleTo: ["idir"],
       redirectTo: {
-        bceidbusiness: 'form',
-        bcsc: 'form',
+        bceidbusiness: "form",
+        bcsc: "form",
       },
-      style: 'content-stretched',
-      headersStyle: 'headers-compact',
+      style: "content-stretched",
+      headersStyle: "headers-compact",
       sideMenu: true,
       profile: true,
     },
   },
   {
-    path: '/submissions/:id',
-    name: 'review',
+    path: "/submissions/:id",
+    name: "review",
     component: SubmissionReview,
     props: true,
     meta: {
-      format: 'full',
+      format: "full",
       hideHeader: false,
       requireAuth: true,
       showLoggedIn: true,
-      visibleTo: ['idir'],
+      visibleTo: ["idir"],
       redirectTo: {
-        bceidbusiness: 'form',
-        bcsc: 'form',
+        bceidbusiness: "form",
+        bcsc: "form",
       },
-      style: 'content-stretched',
-      headersStyle: 'headers-compact',
+      style: "content-stretched",
+      headersStyle: "headers-compact",
       sideMenu: true,
       profile: true,
     },
@@ -165,7 +165,7 @@ const routes = [
         bcsc: "bcsc-form",
       },
       style: "content",
-      headersStyle: 'headers',
+      headersStyle: "headers",
       sideMenu: false,
       profile: false,
     },
@@ -183,7 +183,7 @@ const routes = [
       visibleTo: ["idir", "bceidbusiness", "bcsc"],
     },
     style: "content",
-    headersStyle: 'headers',
+    headersStyle: "headers",
     sideMenu: false,
     profile: false,
   },
@@ -200,7 +200,7 @@ const routes = [
       visibleTo: ["idir", "bceidbusiness", "bcsc"],
     },
     style: "content",
-    headersStyle: 'headers',
+    headersStyle: "headers",
     sideMenu: false,
     profile: false,
   },
@@ -224,34 +224,33 @@ const router = createRouter({
 
 router.beforeEach(async (to, from, next) => {
   const user = ForestClientUserSession.loadDetails();
-  const targetPathStorage = useLocalStorage('targetPath', '');
-  
-  if(to.query.fd_to){
+  const targetPathStorage = useLocalStorage("targetPath", "");
+
+  if (to.query.fd_to) {
     targetPathStorage.value = to.query.fd_to as string;
   }
 
   // Page requires auth
-  if (to.meta.requireAuth) {    
+  if (to.meta.requireAuth) {
     // User is logged in
-    if (user) {      
+    if (user) {
       // If user can see this page, continue, otherwise go to specific page or error
-      if(to.meta.visibleTo.includes(user.provider)){
+      if (to.meta.visibleTo.includes(user.provider)) {
         // If there is a target path, redirect to it and clear the storage
-        if(targetPathStorage.value){  
+        if (targetPathStorage.value) {
           next({ path: targetPathStorage.value });
-          targetPathStorage.value = '';
-        }else{
+          targetPathStorage.value = "";
+        } else {
           // Otherwise, continue to the page
           next();
         }
-      }else{
+      } else {
         // If user is not allowed to see this page, redirect to specific page or error
         next({ name: to.meta.redirectTo?.[user.provider] || "error" });
       }
-
     } else {
       // User is not logged in, redirect to home for login
-      next({ name: "home",query: { fd_to: to.path }});
+      next({ name: "home", query: { fd_to: to.path } });
     }
     // Page does not require auth
   } else {
@@ -269,18 +268,18 @@ router.beforeEach(async (to, from, next) => {
 
 export { routes, router, CONFIRMATION_ROUTE_NAME };
 
-declare module 'vue-router' {
+declare module "vue-router" {
   // eslint-disable-next-line no-unused-vars
   interface RouteMeta {
-    format: string // Main body style class
-    hideHeader: boolean // Show/Hide the header
-    requireAuth: boolean // Force user to be logged in to see this page
-    showLoggedIn: boolean // Show/Hide the page for a logged user
-    visibleTo: Array<string> // Which user types/providers can see this page
-    redirectTo?: Record<string, string> // Where to redirect the user if they are not allowed to see this page
-    style: string // Main body style class
-    headersStyle: string // Header style class
-    sideMenu: boolean // Show/Hide the side menu
-    profile: boolean // Show/Hide the profile menu
+    format: string; // Main body style class
+    hideHeader: boolean; // Show/Hide the header
+    requireAuth: boolean; // Force user to be logged in to see this page
+    showLoggedIn: boolean; // Show/Hide the page for a logged user
+    visibleTo: Array<string>; // Which user types/providers can see this page
+    redirectTo?: Record<string, string>; // Where to redirect the user if they are not allowed to see this page
+    style: string; // Main body style class
+    headersStyle: string; // Header style class
+    sideMenu: boolean; // Show/Hide the side menu
+    profile: boolean; // Show/Hide the profile menu
   }
 }
