071663e1d9b34cf27940ed63d4475191abf819bc
Paulo Gomes da Cruz Junior <paulo.cruz@gov.bc.ca>
2024-03-04 09:11:14 -0800

    fix: disabling csrf for now (#828)

--- backend/src/main/java/ca/bc/gov/app/configuration/SecurityConfiguration.java
+++ backend/src/main/java/ca/bc/gov/app/configuration/SecurityConfiguration.java
@@ -2,7 +2,6 @@ package ca.bc.gov.app.configuration;
 
 import ca.bc.gov.app.ApplicationConstant;
 import ca.bc.gov.app.security.ForestCorsCustomizer;
-import ca.bc.gov.app.security.ForestCsrfCustomizer;
 import ca.bc.gov.app.security.ForestExchangeCustomizer;
 import ca.bc.gov.app.security.ForestOauth2Customizer;
 import java.util.List;
@@ -11,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity.CsrfSpec;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
 import org.springframework.security.core.userdetails.User;
@@ -20,23 +20,24 @@ import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
 /**
- * This class configures the security settings for the application.
- * It uses the @Configuration annotation to indicate that it is a configuration class.
- * It uses the @EnableWebFluxSecurity annotation to enable Spring Security's reactive features.
+ * This class configures the security settings for the application. It uses the @Configuration
+ * annotation to indicate that it is a configuration class. It uses the @EnableWebFluxSecurity
+ * annotation to enable Spring Security's reactive features.
  */
 @Configuration
 @EnableWebFluxSecurity
 public class SecurityConfiguration {
 
   /**
-   * This method configures the SecurityWebFilterChain, which is the main security filter for the application.
-   * It customizes the ServerHttpSecurity object by setting the authorization rules, OAuth2 resource server settings, CORS settings, CSRF settings, and HTTP Basic settings.
-   * It then builds the ServerHttpSecurity object into a SecurityWebFilterChain and returns it.
+   * This method configures the SecurityWebFilterChain, which is the main security filter for the
+   * application. It customizes the ServerHttpSecurity object by setting the authorization rules,
+   * OAuth2 resource server settings, CORS settings, CSRF settings, and HTTP Basic settings. It then
+   * builds the ServerHttpSecurity object into a SecurityWebFilterChain and returns it.
    *
-   * @param http The ServerHttpSecurity object to be customized.
-   * @param corsSpecCustomizer The customizer for the CORS settings.
-   * @param exchangeCustomizer The customizer for the authorization rules.
-   * @param csrfSpecCustomizer The customizer for the CSRF settings.
+   * @param http                 The ServerHttpSecurity object to be customized.
+   * @param corsSpecCustomizer   The customizer for the CORS settings.
+   * @param exchangeCustomizer   The customizer for the authorization rules.
+   * @param csrfSpecCustomizer   The customizer for the CSRF settings.
    * @param oauth2SpecCustomizer The customizer for the OAuth2 resource server settings.
    * @return The configured SecurityWebFilterChain.
    */
@@ -45,22 +46,20 @@ public class SecurityConfiguration {
       ServerHttpSecurity http,
       ForestCorsCustomizer corsSpecCustomizer,
       ForestExchangeCustomizer exchangeCustomizer,
-      ForestCsrfCustomizer csrfSpecCustomizer,
       ForestOauth2Customizer oauth2SpecCustomizer
   ) {
     http
         .authorizeExchange(exchangeCustomizer)
         .oauth2ResourceServer(oauth2SpecCustomizer)
         .cors(corsSpecCustomizer)
-        .csrf(csrfSpecCustomizer)
+        .csrf(CsrfSpec::disable)
         .httpBasic(Customizer.withDefaults());
     return http.build();
   }
 
   /**
-   * This method creates a ReactiveJwtDecoder bean.
-   * The ReactiveJwtDecoder is used to decode JWTs in a reactive context.
-   * It is configured with the JWKS URL from the ForestClientConfiguration.
+   * This method creates a ReactiveJwtDecoder bean. The ReactiveJwtDecoder is used to decode JWTs in
+   * a reactive context. It is configured with the JWKS URL from the ForestClientConfiguration.
    *
    * @param configuration The configuration object that contains the JWKS URL.
    * @return The configured ReactiveJwtDecoder.
@@ -73,9 +72,9 @@ public class SecurityConfiguration {
   }
 
   /**
-   * This method creates a MapReactiveUserDetailsService bean.
-   * The MapReactiveUserDetailsService is used to load user details in a reactive context.
-   * It is configured with the service accounts from the ForestClientConfiguration.
+   * This method creates a MapReactiveUserDetailsService bean. The MapReactiveUserDetailsService is
+   * used to load user details in a reactive context. It is configured with the service accounts
+   * from the ForestClientConfiguration.
    *
    * @param configuration The configuration object that contains the service accounts.
    * @return The configured MapReactiveUserDetailsService.
