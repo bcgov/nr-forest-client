4d6790bcaf15257020bb44e4dc583e8f9dd3286e
Paulo Gomes da Cruz Junior <paulo.cruz@gov.bc.ca>
2024-03-04 13:05:39 -0800

    chore: fixing nonce

--- frontend/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# Replace the placeholder in index.html with the actual NONCE value.
-sed -i "s/{NONCE}/$RANDOM_NONCE/g" /srv/index.html
-
-# Execute the original entrypoint command of Caddy.
-exec /usr/bin/caddy run --config /etc/caddy/Caddyfile --adapter caddyfile
