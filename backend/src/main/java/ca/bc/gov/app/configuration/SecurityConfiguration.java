727391edad0689cd0c19f92c88f2c6118cdc5376
Maria Martinez <maria.martinez@gov.bc.ca>
2024-03-11 09:49:10 -0700

    refactor: Rename class

--- backend/src/main/java/ca/bc/gov/app/configuration/SecurityConfiguration.java
+++ backend/src/main/java/ca/bc/gov/app/configuration/SecurityConfiguration.java
@@ -2,7 +2,7 @@ package ca.bc.gov.app.configuration;
 
 import ca.bc.gov.app.ApplicationConstant;
 import ca.bc.gov.app.security.ForestCorsCustomizer;
-import ca.bc.gov.app.security.ForestExchangeCustomizer;
+import ca.bc.gov.app.security.ApiAuthorizationCustomizer;
 import ca.bc.gov.app.security.ForestOauth2Customizer;
 import java.util.List;
 import org.springframework.context.annotation.Bean;
@@ -36,7 +36,7 @@ public class SecurityConfiguration {
    *
    * @param http                 The ServerHttpSecurity object to be customized.
    * @param corsSpecCustomizer   The customizer for the CORS settings.
-   * @param exchangeCustomizer   The customizer for the authorization rules.
+   * @param apiAuthorizationCustomizer   The customizer for the authorization rules.
    * @param csrfSpecCustomizer   The customizer for the CSRF settings.
    * @param oauth2SpecCustomizer The customizer for the OAuth2 resource server settings.
    * @return The configured SecurityWebFilterChain.
@@ -45,11 +45,11 @@ public class SecurityConfiguration {
   SecurityWebFilterChain springSecurityFilterChain(
       ServerHttpSecurity http,
       ForestCorsCustomizer corsSpecCustomizer,
-      ForestExchangeCustomizer exchangeCustomizer,
+      ApiAuthorizationCustomizer apiAuthorizationCustomizer,
       ForestOauth2Customizer oauth2SpecCustomizer
   ) {
     http
-        .authorizeExchange(exchangeCustomizer)
+        .authorizeExchange(apiAuthorizationCustomizer)
         .oauth2ResourceServer(oauth2SpecCustomizer)
         .cors(corsSpecCustomizer)
         .csrf(CsrfSpec::disable)
