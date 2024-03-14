257d983a403246e22dee710bef4083457d527483
Paulo Gomes da Cruz Junior <paulo.cruz@gov.bc.ca>
2024-03-14 12:14:42 -0700

    chore: code cleaning

--- backend/src/main/java/ca/bc/gov/app/converters/GrantedAuthoritiesConverter.java
+++ backend/src/main/java/ca/bc/gov/app/converters/GrantedAuthoritiesConverter.java
@@ -8,14 +8,28 @@ import org.springframework.core.convert.converter.Converter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.jwt.Jwt;
-
+/**
+ * This class is a converter that takes a Jwt token and converts it into a collection of GrantedAuthority objects.
+ * It implements the Converter interface provided by Spring Framework.
+ */
 @Slf4j
 public class GrantedAuthoritiesConverter implements Converter<Jwt, Collection<GrantedAuthority>> {
 
+  /**
+   * This method takes a Jwt token and converts it into a collection of GrantedAuthority objects.
+   * It first reads the "custom:idp_name" claim from the Jwt token.
+   * If the claim starts with "ca.bc.gov.flnr.fam.", it is converted to "bcsc".
+   * The claim is then converted to uppercase and prefixed with "ROLE_" and suffixed with "_USER".
+   * A new SimpleGrantedAuthority object is created with this string and added to the collection.
+   * If the claim is null or empty, an empty list is returned.
+   *
+   * @param jwt the Jwt token to be converted
+   * @return a collection of GrantedAuthority objects
+   */
   public Collection<GrantedAuthority> convert(Jwt jwt) {
     return
         Optional
-            // Reads the claim
+            // Reads the "custom:idp_name" claim from the Jwt token
             .ofNullable(jwt
                 .getClaims()
                 .getOrDefault("custom:idp_name", "none")
