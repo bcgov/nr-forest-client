4db827a0ceaa428afdd5de280267ec835c4403a2
Paulo Gomes da Cruz Junior <paulo.cruz@gov.bc.ca>
2024-03-18 09:24:21 -0700

    fix(FSADT1-1212): fixing stubs (#878)

--- frontend/src/CoreConstants.ts
+++ frontend/src/CoreConstants.ts
@@ -3,38 +3,47 @@ import { version } from "../package.json";
 export const checkEnv = (key: string) =>
   window.localStorage.getItem(key) || import.meta.env[key];
 
-export const featureFlags = JSON.parse(checkEnv("VITE_FEATURE_FLAGS") || "{}");
-export const backendUrl = checkEnv("VITE_BACKEND_URL");
-export const frontendUrl = checkEnv("VITE_FRONTEND_URL");
-export const greenDomain = checkEnv('VITE_GREEN_DOMAIN')
-export const nodeEnv = checkEnv("VITE_NODE_ENV");
+export const featureFlags: Record<string, any> = JSON.parse(
+  checkEnv("VITE_FEATURE_FLAGS") || "{}"
+);
+export const backendUrl: string = checkEnv("VITE_BACKEND_URL");
+export const frontendUrl: string = checkEnv("VITE_FRONTEND_URL");
+export const greenDomain: string = checkEnv("VITE_GREEN_DOMAIN");
+export const nodeEnv: string = checkEnv("VITE_NODE_ENV");
 
-export const appVersion = version;
+export const appVersion: string = version;
 
-export const cognitoRegion = checkEnv("VITE_AWS_COGNITO_REGION");
-export const cognitoDomain = checkEnv("VITE_AWS_COGNITO_DOMAIN");
-export const cognitoClientId = checkEnv("VITE_AWS_COGNITO_CLIENT_ID");
-export const cognitoPoolId = checkEnv("VITE_AWS_COGNITO_POOL_ID");
-export const cognitoLogoutChainUrl = checkEnv("VITE_AWS_COGNITO_LOGOUT_CHAIN_URL");
-export const cognitoEnvironment = checkEnv("VITE_AWS_COGNITO_ENVIRONMENT");
+export const cognitoRegion: string = checkEnv("VITE_AWS_COGNITO_REGION");
+export const cognitoDomain: string = checkEnv("VITE_AWS_COGNITO_DOMAIN");
+export const cognitoClientId: string = checkEnv("VITE_AWS_COGNITO_CLIENT_ID");
+export const cognitoPoolId: string = checkEnv("VITE_AWS_COGNITO_POOL_ID");
+export const cognitoLogoutChainUrl: string = checkEnv(
+  "VITE_AWS_COGNITO_LOGOUT_CHAIN_URL"
+);
+export const cognitoEnvironment: string = checkEnv(
+  "VITE_AWS_COGNITO_ENVIRONMENT"
+);
 
-export const awsconfig  = {
-  Auth:{
-    Cognito:{
+export const awsconfig = {
+  Auth: {
+    Cognito: {
       userPoolId: `${cognitoPoolId}`,
       userPoolClientId: `${cognitoClientId}`,
       loginWith: {
         oauth: {
-          domain: `${cognitoDomain}.auth.${cognitoRegion}.amazoncognito.com`,
-          scopes: ["openid","profile","email"],
+          domain:
+            nodeEnv === "test"
+              ? backendUrl.replace("http://", "").replace(":8080", ":8081")
+              : `${cognitoDomain}.auth.${cognitoRegion}.amazoncognito.com`,
+          scopes: ["openid", "profile", "email"],
           redirectSignIn: [`${frontendUrl}/dashboard`],
           redirectSignOut: [`${frontendUrl}/logout`],
           responseType: "code",
         },
-        username: 'true'
-      }
-    }
-  }
+        username: "true",
+      },
+    },
+  },
 };
 
 // constant
