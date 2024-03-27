a5c4307e0695bd2bb3df265b66efb03dae07761d
Maria Martinez <maria.martinez@gov.bc.ca>
2024-03-27 15:15:31 -0700

    Formatted code for better readibility

--- backend/src/main/java/ca/bc/gov/app/security/ApiAuthorizationCustomizer.java
+++ backend/src/main/java/ca/bc/gov/app/security/ApiAuthorizationCustomizer.java
@@ -25,73 +25,102 @@ public class ApiAuthorizationCustomizer implements Customizer<AuthorizeExchangeS
 
     // Allow all access to metrics and health endpoints
     // This is due to the internal platform checks that require access to these endpoints
-    authorize.pathMatchers(HttpMethod.GET, "/metrics/**", "/health/**").permitAll();
+    authorize
+      .pathMatchers(HttpMethod.GET, "/metrics/**", "/health/**").permitAll();
 
     // Only service users can POST to the email endpoint
-    authorize.pathMatchers(HttpMethod.POST, "/api/ches/email")
-        .hasAnyRole(ApplicationConstant.USERTYPE_SERVICE_USER);
+    authorize
+      .pathMatchers(HttpMethod.POST, "/api/ches/email")
+      .hasAnyRole(ApplicationConstant.USERTYPE_SERVICE_USER);
 
     // Only service users can send OPTIONS request to the email endpoint
-    authorize.pathMatchers(HttpMethod.OPTIONS, "/api/ches/email")
-        .hasAnyRole(ApplicationConstant.USERTYPE_SERVICE_USER);
+    authorize
+      .pathMatchers(HttpMethod.OPTIONS, "/api/ches/email")
+      .hasAnyRole(ApplicationConstant.USERTYPE_SERVICE_USER);
 
     // Only BCeIDBusiness and BCSC users can POST to the duplicate endpoint
-    authorize.pathMatchers(HttpMethod.POST, "/api/ches/duplicate")
-        .hasAnyRole(ApplicationConstant.USERTYPE_BCEIDBUSINESS_USER,
-            ApplicationConstant.USERTYPE_BCSC_USER);
+    authorize
+      .pathMatchers(HttpMethod.POST, "/api/ches/duplicate")
+      .hasAnyRole(ApplicationConstant.USERTYPE_BCEIDBUSINESS_USER, 
+                  ApplicationConstant.USERTYPE_BCSC_USER);
 
     // Only BCeIDBusiness and BCSC users can send OPTIONS request to the duplicate endpoint
-    authorize.pathMatchers(HttpMethod.OPTIONS, "/api/ches/duplicate")
-        .hasAnyRole(ApplicationConstant.USERTYPE_BCEIDBUSINESS_USER,
-            ApplicationConstant.USERTYPE_BCSC_USER);
+    authorize
+      .pathMatchers(HttpMethod.OPTIONS, "/api/ches/duplicate")
+      .hasAnyRole(ApplicationConstant.USERTYPE_BCEIDBUSINESS_USER, 
+                  ApplicationConstant.USERTYPE_BCSC_USER);
 
     // Only BCeIDBusiness and BCSC users can GET from the addresses endpoint
-    authorize.pathMatchers(HttpMethod.GET, "/api/addresses/**")
-        .hasAnyRole(ApplicationConstant.USERTYPE_BCEIDBUSINESS_USER,
-            ApplicationConstant.USERTYPE_BCSC_USER);
+    authorize
+      .pathMatchers(HttpMethod.GET, "/api/addresses/**")
+      .hasAnyRole(ApplicationConstant.USERTYPE_BCEIDBUSINESS_USER, 
+                  ApplicationConstant.USERTYPE_BCSC_USER);
 
     // Viewer, editor, admin, BCeIDBusiness and BCSC users can GET from the codes endpoint
-    authorize.pathMatchers(HttpMethod.GET, "/api/codes/**")
-        .hasAnyRole(ApplicationConstant.ROLE_VIEWER, ApplicationConstant.ROLE_EDITOR,
-            ApplicationConstant.ROLE_ADMIN, ApplicationConstant.USERTYPE_BCEIDBUSINESS_USER,
-            ApplicationConstant.USERTYPE_BCSC_USER);
+    authorize
+      .pathMatchers(HttpMethod.GET, "/api/codes/**")
+      .hasAnyRole(ApplicationConstant.ROLE_VIEWER,
+                  ApplicationConstant.ROLE_EDITOR,
+                  ApplicationConstant.ROLE_ADMIN,
+                  ApplicationConstant.USERTYPE_BCEIDBUSINESS_USER,
+                  ApplicationConstant.USERTYPE_BCSC_USER);
 
-    // Viewer, editor, admin, BCeIDBusiness, BCSC and service users can GET from the districts endpoint
-    authorize.pathMatchers(HttpMethod.GET, "/api/districts/**")
-        .hasAnyRole(ApplicationConstant.ROLE_VIEWER, ApplicationConstant.ROLE_EDITOR,
-            ApplicationConstant.ROLE_ADMIN, ApplicationConstant.USERTYPE_BCEIDBUSINESS_USER,
-            ApplicationConstant.USERTYPE_BCSC_USER, ApplicationConstant.USERTYPE_SERVICE_USER);
+    // Viewer, editor, admin, BCeIDBusiness, BCSC and service users can GET from the districts
+    // endpoint
+    authorize
+      .pathMatchers(HttpMethod.GET, "/api/districts/**")
+      .hasAnyRole(ApplicationConstant.ROLE_VIEWER, 
+                  ApplicationConstant.ROLE_EDITOR,
+                  ApplicationConstant.ROLE_ADMIN, 
+                  ApplicationConstant.USERTYPE_BCEIDBUSINESS_USER,
+                  ApplicationConstant.USERTYPE_BCSC_USER, 
+                  ApplicationConstant.USERTYPE_SERVICE_USER);
 
-    // Viewer, editor, admin, BCeIDBusiness, BCSC and service users can GET from the countries endpoint
-    authorize.pathMatchers(HttpMethod.GET, "/api/countries/**")
-        .hasAnyRole(ApplicationConstant.ROLE_VIEWER, ApplicationConstant.ROLE_EDITOR,
-            ApplicationConstant.ROLE_ADMIN, ApplicationConstant.USERTYPE_BCEIDBUSINESS_USER,
-            ApplicationConstant.USERTYPE_BCSC_USER, ApplicationConstant.USERTYPE_SERVICE_USER);
+    // Viewer, editor, admin, BCeIDBusiness, BCSC and service users can GET from the countries
+    // endpoint
+    authorize
+      .pathMatchers(HttpMethod.GET, "/api/countries/**")
+      .hasAnyRole(ApplicationConstant.ROLE_VIEWER, 
+                  ApplicationConstant.ROLE_EDITOR,
+                  ApplicationConstant.ROLE_ADMIN, 
+                  ApplicationConstant.USERTYPE_BCEIDBUSINESS_USER,
+                  ApplicationConstant.USERTYPE_BCSC_USER, 
+                  ApplicationConstant.USERTYPE_SERVICE_USER);
 
     // Only editor and admin can POST to the clients submissions endpoint with a specific id
-    authorize.pathMatchers(HttpMethod.POST, "/api/clients/submissions/{id:[0-9]+}")
-        .hasAnyRole(ApplicationConstant.ROLE_EDITOR, ApplicationConstant.ROLE_ADMIN);
+    authorize
+      .pathMatchers(HttpMethod.POST, "/api/clients/submissions/{id:[0-9]+}")
+       .hasAnyRole(ApplicationConstant.ROLE_EDITOR, 
+                   ApplicationConstant.ROLE_ADMIN);
 
     // Viewer, editor and admin can GET from the clients submissions endpoint with a specific id
-    authorize.pathMatchers(HttpMethod.GET, "/api/clients/submissions/{id:[0-9]+}")
-        .hasAnyRole(ApplicationConstant.ROLE_VIEWER, ApplicationConstant.ROLE_EDITOR,
-            ApplicationConstant.ROLE_ADMIN);
+    authorize
+      .pathMatchers(HttpMethod.GET, "/api/clients/submissions/{id:[0-9]+}")
+      .hasAnyRole(ApplicationConstant.ROLE_VIEWER, 
+                  ApplicationConstant.ROLE_EDITOR,
+                  ApplicationConstant.ROLE_ADMIN);
 
     // Only BCeIDBusiness and BCSC users can POST to the clients submissions endpoint
-    authorize.pathMatchers(HttpMethod.POST, "/api/clients/submissions/**")
-        .hasAnyRole(ApplicationConstant.USERTYPE_BCEIDBUSINESS_USER,
-            ApplicationConstant.USERTYPE_BCSC_USER);
+    authorize
+      .pathMatchers(HttpMethod.POST, "/api/clients/submissions/**")
+      .hasAnyRole(ApplicationConstant.USERTYPE_BCEIDBUSINESS_USER, 
+                  ApplicationConstant.USERTYPE_BCSC_USER);
 
     // Viewer, editor and admin can GET from the clients submissions endpoint
-    authorize.pathMatchers(HttpMethod.GET, "/api/clients/submissions/**")
-        .hasAnyRole(ApplicationConstant.ROLE_VIEWER, ApplicationConstant.ROLE_EDITOR,
-            ApplicationConstant.ROLE_ADMIN);
+    authorize
+      .pathMatchers(HttpMethod.GET, "/api/clients/submissions/**")
+      .hasAnyRole(ApplicationConstant.ROLE_VIEWER, 
+                  ApplicationConstant.ROLE_EDITOR,
+                  ApplicationConstant.ROLE_ADMIN);
 
     // BCeIDBusiness, BCSC, viewer, editor and admin users can GET from the clients endpoint
-    authorize.pathMatchers(HttpMethod.GET, "/api/clients/**")
-        .hasAnyRole(ApplicationConstant.USERTYPE_BCEIDBUSINESS_USER,
-            ApplicationConstant.USERTYPE_BCSC_USER, ApplicationConstant.ROLE_VIEWER,
-            ApplicationConstant.ROLE_EDITOR, ApplicationConstant.ROLE_ADMIN);
+    authorize
+      .pathMatchers(HttpMethod.GET, "/api/clients/**")
+      .hasAnyRole(ApplicationConstant.USERTYPE_BCEIDBUSINESS_USER, 
+                  ApplicationConstant.USERTYPE_BCSC_USER,
+                  ApplicationConstant.ROLE_VIEWER, 
+                  ApplicationConstant.ROLE_EDITOR,
+                  ApplicationConstant.ROLE_ADMIN);
 
     // Deny all other requests
     authorize.anyExchange().denyAll();
