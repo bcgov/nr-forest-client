fa6c58281e52e62935845268344b31d2d56ade31
Paulo Gomes da Cruz Junior <paulo.cruz@gov.bc.ca>
2024-04-09 15:06:13 -0700

    fix: mail sent and mail status (#917)

---
+++ backend/src/main/java/ca/bc/gov/app/health/ChesHealthIndicator.java
@@ -0,0 +1,80 @@
+package ca.bc.gov.app.health;
+
+import ca.bc.gov.app.dto.ches.CommonExposureJwtDto;
+import io.micrometer.observation.ObservationRegistry;
+import io.micrometer.observation.annotation.Observed;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.observability.micrometer.Micrometer;
+import reactor.core.publisher.Mono;
+
+@Component
+@Slf4j
+@Observed
+public class ChesHealthIndicator implements HealthIndicator {
+
+  private final WebClient chesApi;
+  private final WebClient authApi;
+  private final ObservationRegistry registry;
+  private Health apiHealth = Health.unknown().build();
+
+  public ChesHealthIndicator(
+      @Qualifier("chesApi") WebClient chesApi,
+      @Qualifier("authApi") WebClient authApi,
+      ObservationRegistry registry
+  ) {
+    this.chesApi = chesApi;
+    this.authApi = authApi;
+    this.registry = registry;
+  }
+
+  @Override
+  public Health health() {
+    getToken()
+        .flatMap(token ->
+            chesApi
+                .get()
+                .uri("/health")
+                .header(HttpHeaders.AUTHORIZATION, "Bearer " + token)
+                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .exchangeToMono(clientResponse -> {
+                  if (clientResponse.statusCode().is2xxSuccessful()) {
+                    return Mono.just(Health.up().build());
+                  } else {
+                    return Mono.just(Health.down().build());
+                  }
+                })
+        )
+        .name("request.ches")
+        .tag("kind", "health")
+        .tap(Micrometer.observation(registry))
+        .doOnNext(health -> log.info("CHES API health: {}", health))
+        .subscribe(
+            health -> apiHealth = health,
+            error -> apiHealth = Health.down().withException(error).build()
+        );
+    log.info("Checking CHES API health");
+    return apiHealth;
+  }
+
+  private Mono<String> getToken() {
+    return
+        authApi
+            .post()
+            .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+            .body(BodyInserters.fromFormData("grant_type", "client_credentials"))
+            .retrieve()
+            .bodyToMono(CommonExposureJwtDto.class)
+            .name("request.ches")
+            .tag("kind", "token")
+            .tap(Micrometer.observation(registry))
+            .map(CommonExposureJwtDto::accessToken);
+  }
+}
