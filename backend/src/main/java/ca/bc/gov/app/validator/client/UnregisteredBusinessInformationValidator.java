bc0948ec080e1a24b8c558fe65d7bf2640997196
Maria Martinez <maria.martinez@gov.bc.ca>
2024-04-09 15:54:35 -0700

    Improved validation

--- backend/src/main/java/ca/bc/gov/app/validator/client/UnregisteredBusinessInformationValidator.java
+++ backend/src/main/java/ca/bc/gov/app/validator/client/UnregisteredBusinessInformationValidator.java
@@ -7,7 +7,6 @@ import ca.bc.gov.app.dto.client.ClientBusinessInformationDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
-import org.springframework.validation.ValidationUtils;
 import org.springframework.validation.Validator;
 
 @Component
@@ -24,18 +23,24 @@ public class UnregisteredBusinessInformationValidator implements Validator {
     errors.pushNestedPath("businessInformation");
 
     String businessNameField = "businessName";
-    ValidationUtils.rejectIfEmpty(errors, businessNameField,
-        fieldIsMissingErrorMessage(businessNameField));
+    String businessName = (String) errors.getFieldValue(businessNameField);
+
+    if (businessName == null || businessName.isEmpty()) {
+      errors.rejectValue(businessNameField, fieldIsMissingErrorMessage(businessNameField));
+      errors.popNestedPath();
+      return;
+    }
 
     // fails if businessName does not contain whitespace, Ex: forest1 should fail, but forest 1 should pass
-    String businessName = (String) errors.getFieldValue(businessNameField);
-    if (businessName != null && !businessName.matches(".*\\s+.*")) {
+    if (!businessName.matches(".*\\s+.*")) {
       errors.rejectValue(businessNameField, "Business name must be composed of first and last name");
     }
-    
+
     if (!US7ASCII_PATTERN.matcher(businessName).matches()) {
       errors.rejectValue(businessNameField, String.format("%s has an invalid character.", businessName));
     }
+
     errors.popNestedPath();
   }
+
 }
