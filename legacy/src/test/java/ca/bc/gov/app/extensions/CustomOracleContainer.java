a7c4e828dca06fa77d2031b2ff65e9983f5cc019
Paulo Gomes da Cruz Junior <paulo.cruz@gov.bc.ca>
2024-06-04 16:34:35 -0700

    chore(FSADT1-1363): changing testcontainer for oracle
    changing it due to the issues with Mac M2 chip. Increased the timeout to allow the oracle instance to spin up properly

---
+++ legacy/src/test/java/ca/bc/gov/app/extensions/CustomOracleContainer.java
@@ -0,0 +1,24 @@
+package ca.bc.gov.app.extensions;
+
+import java.time.Duration;
+import java.util.UUID;
+import org.testcontainers.containers.OracleContainer;
+
+public class CustomOracleContainer extends OracleContainer {
+
+  public CustomOracleContainer() {
+    super("gvenzl/oracle-xe:21.3.0-slim-faststart");
+
+    this.withDatabaseName("legacyfsa")
+        .withUsername("THE")
+        .withPassword(UUID.randomUUID().toString().substring(24));
+  }
+
+  @Override
+  protected void waitUntilContainerStarted() {
+    getWaitStrategy()
+        .withStartupTimeout(Duration.ofMinutes(10))
+        .waitUntilReady(this);
+  }
+
+}
