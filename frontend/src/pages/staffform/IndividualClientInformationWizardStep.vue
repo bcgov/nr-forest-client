0ecdd9ada71f338fde7b6fe1dc2a7f62a421f21c
Fernando Terra <fernando.terra@encora.com>
2024-06-06 11:29:04 -0300

    add the individual client component

---
+++ frontend/src/pages/staffform/IndividualClientInformationWizardStep.vue
@@ -0,0 +1,131 @@
+<script setup lang="ts">
+import { watch, computed, ref, reactive, onMounted } from "vue";
+// Carbon
+import "@carbon/web-components/es/components/inline-loading/index";
+import "@carbon/web-components/es/components/notification/index";
+// Importing composables
+import { useEventBus } from "@vueuse/core";
+import { useFetch, useFetchTo } from "@/composables/useFetch";
+import { useFocus } from "@/composables/useFocus";
+// Importing types
+import type { FormDataDto } from "@/dto/ApplyClientNumberDto";
+// Importing validators
+import { getValidations } from "@/helpers/validators/GlobalValidators";
+import { submissionValidation } from "@/helpers/validators/SubmissionValidators";
+
+// Defining the props and emiter to reveice the data and emit an update
+const props = defineProps<{
+  data: FormDataDto;
+  active: boolean;
+  autoFocus?: boolean;
+}>();
+
+const emit = defineEmits<{
+  (e: "update:data", value: FormDataDto): void;
+  (e: "valid", value: boolean): void;
+}>();
+
+// Set the prop as a ref, and then emit when it changes
+const formData = ref<FormDataDto>(props.data);
+watch(
+  () => formData.value,
+  () => emit("update:data", formData.value),
+);
+
+// -- Validation of the component --
+const validation = reactive<Record<string, boolean>>({
+  firstName: false,
+  middleName: true,
+  lastName: false,
+  business: !!formData.value.businessInformation.businessName,
+  birthdate: false,
+});
+
+const checkValid = () =>
+  Object.values(validation).reduce(
+    (accumulator: boolean, currentValue: boolean) => accumulator && currentValue,
+    true,
+  );
+
+watch([validation], () => emit("valid", checkValid()));
+emit("valid", checkValid());
+
+const { setFocusedComponent } = useFocus();
+onMounted(() => {
+  if (props.autoFocus) {
+    setFocusedComponent("focus-0", 0);
+  }
+});
+</script>
+
+<template>
+  <div class="frame-01">
+    <text-input-component
+      id="firstName"
+      label="First name"
+      placeholder=""
+      autocomplete="off"
+      v-model="formData.location.contacts[0].firstName"
+      :validations="[
+        ...getValidations('location.contacts.*.firstName'),
+        submissionValidation(`location.contacts[0].firstName`),
+      ]"
+      enabled
+      required
+      required-label
+      @empty="validation.firstName = !$event"
+      @error="validation.firstName = !$event"
+    />
+
+    <text-input-component
+      id="middleName"
+      label="Middle name"
+      placeholder=""
+      autocomplete="off"
+      v-model="formData.businessInformation.middleName"
+      :validations="[
+        ...getValidations('businessInformation.middleName'),
+        submissionValidation(`businessInformation.middleName`),
+      ]"
+      enabled
+      @empty="validation.middleName = !$event"
+      @error="validation.middleName = !$event"
+    />
+
+    <text-input-component
+      id="lastName"
+      label="Last name"
+      placeholder=""
+      autocomplete="off"
+      v-model="formData.location.contacts[0].lastName"
+      :validations="[
+        ...getValidations('location.contacts.*.lastName'),
+        submissionValidation(`location.contacts[0].lastName`),
+      ]"
+      enabled
+      required
+      required-label
+      @empty="validation.lastName = !$event"
+      @error="validation.lastName = !$event"
+    />
+
+    <div>
+      <p class="body-02 date-label">
+        <span class="cds-text-input-required-label">* </span>
+        We need the applicant's birthdate to confirm their identity
+      </p>
+      <date-input-component
+        id="birthdate"
+        title="Date of birth"
+        :autocomplete="['bday-year', 'bday-month', 'bday-day']"
+        v-model="formData.businessInformation.birthdate"
+        :enabled="true"
+        :validations="[...getValidations('businessInformation.birthdate')]"
+        :year-validations="[...getValidations('businessInformation.birthdate.year')]"
+        @error="validation.birthdate = !$event"
+        @possibly-valid="validation.birthdate = $event"
+        required
+      />
+    </div>
+  </div>
+</template>
