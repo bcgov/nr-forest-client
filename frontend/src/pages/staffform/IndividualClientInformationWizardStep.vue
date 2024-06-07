8f90fb4dc854d9f55b048675db600cf9417f7430
Fernando Terra <fernando.terra@encora.com>
2024-06-07 09:49:31 -0300

    add the person id fields

--- frontend/src/pages/staffform/IndividualClientInformationWizardStep.vue
+++ frontend/src/pages/staffform/IndividualClientInformationWizardStep.vue
@@ -10,6 +10,7 @@ import { useFetch, useFetchTo } from "@/composables/useFetch";
 import { useFocus } from "@/composables/useFocus";
 // Importing types
 import type { FormDataDto } from "@/dto/ApplyClientNumberDto";
+import type { CodeNameType } from "@/dto/CommonTypesDto";
 // Importing validators
 import { getValidations } from "@/helpers/validators/GlobalValidators";
 import { submissionValidation } from "@/helpers/validators/SubmissionValidators";
@@ -35,13 +36,56 @@ watch(
   () => emit("update:data", formData.value),
 );
 
+// TODO: get this list from the back-end
+const idTypeList: CodeNameType[] = [
+  { code: "CBC", name: "Canadian birth certificate" },
+  { code: "CDL", name: "Canadian driver's licence" },
+  { code: "CM", name: "Canadian military ID" },
+  { code: "CP", name: "Canadian passport" },
+  { code: "CCC", name: "Canadian citizenship card" },
+  { code: "FNS", name: "First Nation status ID" },
+  { code: "USDL", name: "US driver's licence" },
+];
+const idType = ref<CodeNameType>();
+const issuingProvince = ref<CodeNameType>();
+
+const updateIdType = (value: CodeNameType | undefined) => {
+  idType.value = value;
+};
+
+watch(idType, (idTypeValue) => {
+  formData.value.businessInformation.idType = idTypeValue?.code;
+});
+
+const provinceUrl = computed(() => {
+  if (idType.value) {
+    const countryCode = idType.value.code === "USDL" ? "US" : "CA";
+    return `/api/countries/${countryCode}/provinces?page=0&size=250`;
+  }
+  return "";
+});
+
+const issuingProvinceNaming = computed(() => {
+  const countryCode = idType.value?.code === "USDL" ? "US" : "CA";
+  return countryCode === "US" ? "Issuing state" : "Issuing province";
+});
+
+const updateIssuingProvince = (value: CodeNameType | undefined) => {
+  issuingProvince.value = value;
+};
+
+watch(issuingProvince, (issuingProvinceValue) => {
+  formData.value.businessInformation.issuingProvince = issuingProvinceValue?.code;
+});
+
 // -- Validation of the component --
 const validation = reactive<Record<string, boolean>>({
   firstName: false,
   middleName: true,
   lastName: false,
-  business: !!formData.value.businessInformation.businessName,
+  business: false,
   birthdate: false,
+  idType: false,
 });
 
 const checkValid = () =>
@@ -136,6 +180,69 @@ onMounted(() => {
         @error="validation.birthdate = !$event"
         @possibly-valid="validation.birthdate = $event"
         required
+      />
+    </div>
+
+    <div class="horizontal-input-grouping">
+      <dropdown-input-component
+        id="idType"
+        label="ID type"
+        :initial-value="idType?.name"
+        required
+        required-label
+        :model-value="idTypeList"
+        :enabled="true"
+        tip=""
+        :validations="[
+          ...getValidations('businessInformation.idType'),
+          submissionValidation('businessInformation.idType'),
+        ]"
+        @update:selected-value="updateIdType($event)"
+        @empty="validation.district = !$event"
+      />
+
+      <data-fetcher
+        v-if="['CDL', 'USDL'].includes(idType?.code)"
+        v-model:url="provinceUrl"
+        :min-length="0"
+        :init-value="[]"
+        :init-fetch="true"
+        :params="{ method: 'GET' }"
+        #="{ content }"
+      >
+        <dropdown-input-component
+          id="issuingProvince"
+          :label="issuingProvinceNaming"
+          required
+          required-label
+          :initial-value="issuingProvince?.name"
+          :model-value="content"
+          :enabled="true"
+          tip=""
+          :validations="[
+            ...getValidations('businessInformation.issuingProvince'),
+            submissionValidation('businessInformation.issuingProvince'),
+          ]"
+          @update:selected-value="updateIssuingProvince($event)"
+          @empty="validation.province = !$event"
+        />
+      </data-fetcher>
+
+      <text-input-component
+        id="idNumber"
+        label="ID number"
+        placeholder=""
+        autocomplete="off"
+        v-model="formData.businessInformation.idNumber"
+        :validations="[
+          ...getValidations('businessInformation.idNumber'),
+          submissionValidation('businessInformation.idNumber'),
+        ]"
+        enabled
+        required
+        required-label
+        @empty="validation.idNumber = !$event"
+        @error="validation.idNumber = !$event"
       />
     </div>
   </div>
