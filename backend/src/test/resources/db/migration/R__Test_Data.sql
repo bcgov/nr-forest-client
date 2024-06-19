9a53d01530e30ece3e9a6d5c9e3b4c1de765dbe7
Paulo Gomes da Cruz Junior <paulo.cruz@gov.bc.ca>
2024-06-19 14:05:10 -0700

    feat(FSADT1-1362): staff created entries
    added the new type along with the new enum

--- backend/src/test/resources/db/migration/R__Test_Data.sql
+++ backend/src/test/resources/db/migration/R__Test_Data.sql
@@ -19,11 +19,21 @@ insert into nrfc.client_type_code (client_type_code, description, effective_date
 
 update nrfc.client_type_code set effective_date = '1970-01-01 00:00:00.000';
 
--- Test case data
+-- Test case data Review new client submission
 
-INSERT INTO nrfc.submission VALUES (365, 'R', 'RNC', '2024-04-16 15:57:57.065704', '2024-04-16 16:00:02.690147', 'BCEIDBUSINESS\\UAT', ' Martinez') ON CONFLICT DO NOTHING;
+INSERT INTO nrfc.submission VALUES (365, 'R', 'RNC', current_timestamp, current_timestamp, 'BCEIDBUSINESS\\UAT', ' BCEIDBUSINESS\\UAT') ON CONFLICT DO NOTHING;
 INSERT INTO nrfc.submission_contact VALUES (365, 365, 'DI', 'Load', 'NRS', '(777) 777-7777', 'uattestingmail@uat.testing.lo', 'BCEIDBUSINESS\\UAT') ON CONFLICT DO NOTHING;
-INSERT INTO nrfc.submission_detail VALUES (365, 365, NULL, 'R', 'XX0000006', 'HYPERION CORP', 'C', 'Y', NULL, 'DCR') ON CONFLICT DO NOTHING;
+INSERT INTO nrfc.submission_detail VALUES (365, 365, NULL, 'R', 'BC0000006', 'HYPERION CORP', 'C', 'Y', NULL, 'DCR') ON CONFLICT DO NOTHING;
 INSERT INTO nrfc.submission_location VALUES (365, 365, '712 Canyon View Dr', 'US', 'KS', 'Lansing', '66043-6271', 'Mailing address') ON CONFLICT DO NOTHING;
 INSERT INTO nrfc.submission_location_contact_xref VALUES (365, 365) ON CONFLICT DO NOTHING;
 INSERT INTO nrfc.submission_matching_detail VALUES (365, 365, '{"contact": "00000000,00000000,00000001,00000001,00000001", "registrationNumber": "00000002,00000002"}', 'N', ' already has one. The number is: tyututu. Be sure to keep it for your records.', '2024-04-16 16:00:06.678395', 'idir\\ottomated', true) ON CONFLICT DO NOTHING;
+
+-- Test case data Staff submitted data - Individual
+
+INSERT INTO nrfc.submission VALUES (364, 'A', 'SSD', current_timestamp, current_timestamp, 'BCEIDBUSINESS\\UAT', ' BCEIDBUSINESS\\UAT') ON CONFLICT DO NOTHING;
+INSERT INTO nrfc.submission_contact VALUES (364, 364, 'DI', 'Jhon', 'Wick', '(999) 888-7766', 'jhonwick@uat.testing.lo', 'BCEIDBUSINESS\\UAT') ON CONFLICT DO NOTHING;
+INSERT INTO nrfc.submission_detail (submission_detail_id, submission_id, client_number, business_type_code, incorporation_number, organization_name, client_type_code, good_standing_ind, birthdate, district_code, work_safe_bc_number, doing_business_as, client_acronym, first_name, middle_name, last_name, notes, identification_type_code, client_identification, country_code, province_code)
+VALUES (364, 364, NULL, 'U', NULL, 'ROLAND SOLDIER', 'I', 'Y', '1972-12-04', 'DCR', NULL, NULL, NULL,'ROLAND', NULL,'SOLDIER','GOOD AT SUPPORT','CDDL','9999911','CA','BC') ON CONFLICT DO NOTHING;
+INSERT INTO nrfc.submission_location VALUES (364, 364, '999 Canyon View Dr', 'US', 'CO', 'Denver', '66043-6271', 'Mailing address') ON CONFLICT DO NOTHING;
+INSERT INTO nrfc.submission_location_contact_xref VALUES (364, 364) ON CONFLICT DO NOTHING;
+INSERT INTO nrfc.submission_matching_detail VALUES (364, 364, '{}', 'N', NULL, '2024-04-16 16:00:06.678395', 'idir\\ottomated', true) ON CONFLICT DO NOTHING;
