76d17babc2531600fc47dac9354a0606e2ef8a31
Paulo Gomes da Cruz Junior <paulo.cruz@gov.bc.ca>
2024-07-02 15:55:24 -0700

    feat: converting processor to native

--- processor/src/main/java/ca/bc/gov/app/configuration/GlobalServiceConfiguration.java
+++ processor/src/main/java/ca/bc/gov/app/configuration/GlobalServiceConfiguration.java
@@ -2,7 +2,25 @@ package ca.bc.gov.app.configuration;
 
 import static org.springframework.web.reactive.function.client.ExchangeFilterFunctions.basicAuthentication;
 
+import ca.bc.gov.app.dto.DistrictDto;
+import ca.bc.gov.app.dto.EmailRequestDto;
+import ca.bc.gov.app.dto.MatcherResult;
+import ca.bc.gov.app.dto.MessagingWrapper;
 import ca.bc.gov.app.dto.SubmissionInformationDto;
+import ca.bc.gov.app.dto.bcregistry.BcRegistryDocumentAccessRequestDto;
+import ca.bc.gov.app.dto.bcregistry.BcRegistryDocumentAccessTypeDto;
+import ca.bc.gov.app.dto.bcregistry.BcRegistryDocumentDto;
+import ca.bc.gov.app.dto.bcregistry.BcRegistryDocumentRequestBodyDto;
+import ca.bc.gov.app.dto.bcregistry.BcRegistryDocumentRequestDocumentDto;
+import ca.bc.gov.app.dto.bcregistry.BcRegistryDocumentRequestResponseDto;
+import ca.bc.gov.app.dto.bcregistry.BcRegistryExceptionMessageDto;
+import ca.bc.gov.app.dto.bcregistry.BcRegistryOfficerDto;
+import ca.bc.gov.app.dto.bcregistry.BcRegistryPartyDto;
+import ca.bc.gov.app.dto.bcregistry.BcRegistryRoleDto;
+import ca.bc.gov.app.dto.legacy.ClientDoingBusinessAsDto;
+import ca.bc.gov.app.dto.legacy.ForestClientContactDto;
+import ca.bc.gov.app.dto.legacy.ForestClientDto;
+import ca.bc.gov.app.dto.legacy.ForestClientLocationDto;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.aot.hint.annotation.RegisterReflectionForBinding;
 import org.springframework.context.annotation.Bean;
@@ -13,7 +31,25 @@ import org.springframework.web.reactive.function.client.WebClient;
 @Slf4j
 @RegisterReflectionForBinding({
     SubmissionInformationDto.class,
-    Integer.class
+    Integer.class,
+    MessagingWrapper.class,
+    MatcherResult.class,
+    EmailRequestDto.class,
+    DistrictDto.class,
+    ForestClientLocationDto.class,
+    ForestClientDto.class,
+    ForestClientContactDto.class,
+    ClientDoingBusinessAsDto.class,
+    BcRegistryRoleDto.class,
+    BcRegistryPartyDto.class,
+    BcRegistryOfficerDto.class,
+    BcRegistryExceptionMessageDto.class,
+    BcRegistryDocumentRequestBodyDto.class,
+    BcRegistryDocumentRequestDocumentDto.class,
+    BcRegistryDocumentRequestResponseDto.class,
+    BcRegistryDocumentDto.class,
+    BcRegistryDocumentAccessTypeDto.class,
+    BcRegistryDocumentAccessRequestDto.class
 })
 public class GlobalServiceConfiguration {
 
