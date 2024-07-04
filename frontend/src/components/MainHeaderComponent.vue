f3878dc860e65dff89646163c7877130f7dd02b1
Paulo Gomes da Cruz Junior <paulo.cruz@gov.bc.ca>
2024-07-04 15:26:46 -0700

    chore: adding test code

--- frontend/src/components/MainHeaderComponent.vue
+++ frontend/src/components/MainHeaderComponent.vue
@@ -234,6 +234,11 @@ const userHasAuthority = ["CLIENT_EDITOR", "CLIENT_ADMIN"].some(authority => For
         <Result16 slot="title-icon" />
       </cds-side-nav-link>
 
+      <cds-side-nav-link active href="/submissions" large id="menu-list-submission-list">
+        <span>Submissions {{ userHasAuthority }} | {{ $session.authorities }}</span>
+        <Result16 slot="title-icon" />
+      </cds-side-nav-link>
+
       <cds-side-nav-link active href="/new-client-staff" large v-if="userHasAuthority" v-shadow=1 id="menu-list-staff-form">
         <span>Create client</span>
         <TaskAdd16 slot="title-icon" />
