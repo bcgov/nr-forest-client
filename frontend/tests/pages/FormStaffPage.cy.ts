86b80162036b17e762c9a76edd56f1ddb814c7dc
Fernando Terra <fernando.terra@encora.com>
2024-07-04 16:19:17 -0300

    test max locations to be 25

---
+++ frontend/tests/pages/FormStaffPage.cy.ts
@@ -0,0 +1,62 @@
+import FormStaffPage from "@/pages/FormStaffPage.vue";
+
+const individualBaseData = {
+  firstName: "John",
+  middleName: "Michael",
+  lastName: "Silver",
+  birthdateYear: "2001",
+  birthdateMonth: "05",
+  birthdateDay: "30",
+  identificationTypeValue: "Canadian passport",
+  identificationProvinceValue: undefined,
+  clientIdentification: "AB345678",
+};
+
+const fillIndividual = (data = individualBaseData) => {
+  cy.get("#firstName").shadow().find("input").type(data.firstName);
+
+  cy.get("#middleName").shadow().find("input").type(data.middleName);
+
+  cy.get("#lastName").shadow().find("input").type(data.lastName);
+
+  cy.get("#birthdateYear").shadow().find("input").type(data.birthdateYear);
+  cy.get("#birthdateMonth").shadow().find("input").type(data.birthdateMonth);
+  cy.get("#birthdateDay").shadow().find("input").type(data.birthdateDay);
+
+  cy.get("#identificationType").find("[part='trigger-button']").click();
+  cy.get("#identificationType")
+    .find(`cds-combo-box-item[data-value="${data.identificationTypeValue}"]`)
+    .click();
+
+  if (data.identificationProvinceValue) {
+    cy.get("#identificationProvince").find("[part='trigger-button']").click();
+    cy.get("#identificationProvince")
+      .find(`cds-combo-box-item[data-value="${data.identificationProvinceValue}"]`)
+      .click();
+  }
+
+  cy.get("#clientIdentification").shadow().find("input").type(data.clientIdentification);
+
+  cy.get("#clientIdentification").shadow().find("input").blur();
+};
+
+describe("Step 2 - Locations", () => {
+  it("should render the LocationsWizardStep with maxLocations set to 25", () => {
+    cy.mount(FormStaffPage, {}).its("wrapper").as("wrapper");
+
+    cy.get("#clientType").find("[part='trigger-button']").click();
+
+    cy.get("#clientType").find('cds-combo-box-item[data-id="I"]').click();
+
+    fillIndividual();
+
+    cy.get("[data-test='wizard-next-button']").click();
+
+    cy.get("@wrapper")
+      .then(async (wrapper) => {
+        const locations = wrapper.findComponent({ name: "LocationsWizardStep" });
+        return locations.props("maxLocations");
+      })
+      .should("eq", 25);
+  });
+});
