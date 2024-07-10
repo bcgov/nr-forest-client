6b51b6e0448e8c80d3c4f30926880b1e812887a0
Maria Martinez <maria.martinez@gov.bc.ca>
2024-07-10 14:25:34 -0700

    Revert "no message"
    This reverts commit 6f4b90b48a81b5aaa1d06efa8d25585f92d512ab.

--- frontend/cypress.config.ts
+++ frontend/cypress.config.ts
@@ -29,12 +29,7 @@ export default defineConfig({
     setupNodeEvents: (on, config) => {      
       require('@cypress/code-coverage/task')(on, config)
       return config
-    },
-    defaultCommandTimeout: 10000,
-    retries: {
-      runMode: 2,
-      openMode: 1,
-    },
+    }
   },
 
   component: {
