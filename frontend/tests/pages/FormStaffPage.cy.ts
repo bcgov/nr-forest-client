f5631f90fcf4b429778c6a0f5ab5d67edf9f14d7
Maria Martinez <maria.martinez@gov.bc.ca>
2024-07-10 15:17:27 -0700

    Debugging test...

--- frontend/tests/pages/FormStaffPage.cy.ts
+++ frontend/tests/pages/FormStaffPage.cy.ts
@@ -24,10 +24,13 @@ const fillIndividual = (data = individualBaseData) => {
   cy.get("#identificationType").find("[part='trigger-button']").click();
   cy.wait(2000);
   cy.get("#identificationType")
-    .find(`cds-combo-box-item[data-id="${data.identificationTypeCode}"]`)
-    .should('be.visible')
-    .click()
-    .should("have.value", data.identificationTypeValue, { timeout: 10000 });
+  .find(`cds-combo-box-item[data-id="${data.identificationTypeCode}"]`)
+  .should('be.visible')
+  .log('Combo-box item visible')
+  .click()
+  .log('Combo-box item clicked')
+  .should("have.value", data.identificationTypeValue)
+  .log('Value assertion passed');
 
   cy.get("#clientIdentification").find("input").clear();
   cy.get("#clientIdentification").find("input").should('be.focused').blur();
