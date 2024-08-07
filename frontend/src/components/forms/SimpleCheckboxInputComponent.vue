54394776f2714feeadab89c1c6840bdc56501aa3
Fernando Terra <fernando.terra@encora.com>
2024-08-07 15:19:37 -0300

    feat: add a simple checkbox component

---
+++ frontend/src/components/forms/SimpleCheckboxInputComponent.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+// Carbon
+import "@carbon/web-components/es/components/checkbox/index";
+
+const props = defineProps<{
+  id: string;
+  label: string;
+  checkboxLabel: string;
+  tip?: string;
+  requiredLabel?: boolean;
+}>();
+
+const model = defineModel<boolean>({ required: true });
+</script>
+
+<template>
+  <cds-checkbox-group
+    :id="id"
+    :legend-text="label"
+    :data-required-label="requiredLabel"
+    :helper-text="tip"
+    v-shadow="2"
+  >
+    <cds-checkbox
+      v-shadow="1"
+      :checked="model"
+      @cds-checkbox-changed="model = $event.detail.checked"
+      >{{ checkboxLabel }}</cds-checkbox
+    >
+  </cds-checkbox-group>
+</template>
