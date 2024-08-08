b683601a2aa105d9617cfe0af978452548ad286b
Paulo Gomes da Cruz Junior <paulo.cruz@gov.bc.ca>
2024-08-08 15:46:38 -0700

    feat(FSADT1-1399): added validators

---
+++ backend/src/test/java/ca/bc/gov/app/validator/business/BusinessInformationDoingBusinessValidatorTest.java
@@ -0,0 +1,81 @@
+package ca.bc.gov.app.validator.business;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import ca.bc.gov.app.dto.ValidationError;
+import ca.bc.gov.app.dto.client.ClientBusinessInformationDto;
+import ca.bc.gov.app.dto.client.ValidationSourceEnum;
+import java.util.stream.Stream;
+import org.apache.commons.lang3.StringUtils;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import reactor.test.StepVerifier;
+
+@DisplayName("Unit Tests | BusinessInformationDoingBusinessValidator")
+class BusinessInformationDoingBusinessValidatorTest {
+
+  private final BusinessInformationDoingBusinessValidator validator = new BusinessInformationDoingBusinessValidator();
+
+  @ParameterizedTest
+  @MethodSource("ca.bc.gov.app.validator.address.AddressAddressValidatorTest#staffSource")
+  @DisplayName("Should support all validation sources")
+  void shouldSupportAllValidationSources(ValidationSourceEnum source, boolean support) {
+    assertEquals(support, validator.supports(source));
+  }
+
+  @ParameterizedTest
+  @MethodSource("validation")
+  @DisplayName("Should run validate")
+  void shouldValidate(String doingBusinessAs, String expectedMessage) {
+
+    StepVerifier.FirstStep<ValidationError> validation =
+        validator.validate(
+                new ClientBusinessInformationDto(
+                    StringUtils.EMPTY,
+                    StringUtils.EMPTY,
+                    StringUtils.EMPTY,
+                    StringUtils.EMPTY,
+                    StringUtils.EMPTY,
+                    StringUtils.EMPTY,
+                    null,
+                    StringUtils.EMPTY,
+                    StringUtils.EMPTY,
+                    doingBusinessAs,
+                    StringUtils.EMPTY,
+                    StringUtils.EMPTY,
+                    StringUtils.EMPTY,
+                    StringUtils.EMPTY,
+                    StringUtils.EMPTY,
+                    null,
+                    StringUtils.EMPTY,
+                    StringUtils.EMPTY,
+                    StringUtils.EMPTY
+                ), 0
+            )
+            .as(StepVerifier::create);
+
+    if (StringUtils.isNotBlank(expectedMessage)) {
+      validation.expectNext(new ValidationError(
+              "businessInformation.doingBusinessAs",
+              expectedMessage
+          )
+      );
+    }
+
+    validation.verifyComplete();
+
+  }
+
+  private static Stream<Arguments> validation() {
+    return
+        Stream.of(
+            Arguments.of(StringUtils.EMPTY, StringUtils.EMPTY),
+            Arguments.of("Mainé", "Doing Business As has an invalid character."),
+            Arguments.of("Word".repeat(50), "This field has a 120 character limit."),
+            Arguments.of("That Corp.", StringUtils.EMPTY)
+        );
+  }
+
+}
