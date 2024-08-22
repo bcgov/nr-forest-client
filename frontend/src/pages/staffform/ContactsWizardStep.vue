52a6fc1808f510c9061d631c48f4c3551b0b5e50
Fernando Terra <fernando.terra@encora.com>
2024-08-22 10:47:08 -0300

    feat: auto expand/collapse contacts

--- frontend/src/pages/staffform/ContactsWizardStep.vue
+++ frontend/src/pages/staffform/ContactsWizardStep.vue
@@ -3,6 +3,8 @@ import { watch, ref, computed, reactive, onMounted } from "vue";
 // Carbon
 import "@carbon/web-components/es/components/button/index";
 import "@carbon/web-components/es/components/accordion/index";
+// Components
+import FuzzyMatchNotificationGroupingComponent from "@/components/grouping/FuzzyMatchNotificationGroupingComponent.vue";
 // Composables
 import { useEventBus } from "@vueuse/core";
 import { useFocus } from "@/composables/useFocus";
@@ -52,9 +54,6 @@ const updateContact = (value: Contact | undefined, index: number) => {
   }
 };
 
-//Country related data
-const countryList = ref([]);
-
 //Role related data
 const roleList = ref([]);
 const fetch = () => {
@@ -72,11 +71,47 @@ const addresses = computed<CodeNameType[]>(() =>
   })
 );
 
+const fuzzyNotification = reactive<Record<string, boolean>>({});
+
+const open = reactive<Record<string, boolean>>(
+  formData.location.contacts.reduce((accumulator: Record<string, boolean>, current: Contact) => {
+    const contactId = current.index;
+    accumulator[contactId] = true;
+    return accumulator;
+  }, {}),
+);
+
+const setFuzzyNotification = (index: number, show: boolean) => {
+  const contact = formData.location.contacts[index];
+  const contactId = contact.index;
+  fuzzyNotification[contactId] = show;
+};
+
+const setFuzzyNotificationByElement = (
+  el: InstanceType<typeof FuzzyMatchNotificationGroupingComponent>,
+  index: number,
+) => {
+  if (el) {
+    setFuzzyNotification(index, el.fuzzyMatchedError.show);
+  }
+};
+
+watch(fuzzyNotification, () => {
+  const hasFuzzyNotification = Object.values(fuzzyNotification).some((value) => value);
+  if (hasFuzzyNotification) {
+    formData.location.contacts.forEach((contact) => {
+      const id = contact.index;
+      open[id] = fuzzyNotification[id];
+    });
+  }
+});
+
 let lastContactId = formData.location.contacts.length - 1; // The first contactId to be generated minus 1.
 const getNewContactId = () => ++lastContactId;
 
 //New contact being added
 const otherContacts = computed(() => formData.location.contacts.slice(1));
+
 const addContact = () => {
   const newLength = formData.location.contacts.push(
     indexedEmptyContact(getNewContactId())
@@ -84,6 +119,7 @@ const addContact = () => {
   const contact = formData.location.contacts[newLength - 1];
   setScrollPoint(`contact-${contact.index}-heading`);
   setFocusedComponent(`contact-${contact.index}-heading`);
+  open[contact.index] = true;
   return newLength;
 };
 
@@ -132,7 +168,7 @@ const removeContact = (index: number) => () => {
 const handleRemove = (index: number) => {
   const selectedContact = getContactDescription(
     formData.location.contacts[index],
-    index
+    index + 1, // 1-based index to display in the message
   );
   bus.emit({
     name: selectedContact,
@@ -145,6 +181,18 @@ const handleRemove = (index: number) => {
 };
 
 onMounted(() => setFocusedComponent("focus-1", 0));
+
+const cdsAccordionItemBeingtoggled = (event: any, contact: Contact) => {
+  const id = contact.index;
+  open[id] = event.detail.open;
+  /*
+  This somehow prevents some UI issues when there are manual expand/collapse by the user followed
+  by automatic expand/collapse or vice-versa.
+  */
+  if (event.detail.open) {
+    event.preventDefault();
+  }
+};
 
 const contactName = (contact: Contact) => {
   if (!contact) return "Additional contact";
@@ -171,6 +219,16 @@ const contactName = (contact: Contact) => {
       Applicant must be a contact for this client.
     </p>
   </cds-inline-notification>
+  <div class="errors-container hide-when-less-than-two-children">
+    <!--
+    The parent div is necessary to avoid the div.header-offset below from interfering in the flex flow.
+    -->
+    <div data-scroll="location-contacts-0" class="header-offset"></div>
+    <fuzzy-match-notification-grouping-component
+      id="location-contacts-0"
+      :ref="(el: any) => setFuzzyNotificationByElement(el, 0)"
+    />
+  </div>
   <staff-contact-group-component
     :id="0"
     v-model="formData.location.contacts[0]"
@@ -189,12 +247,25 @@ const contactName = (contact: Contact) => {
     <cds-accordion v-for="(contact, index) in otherContacts" :key="contact.index">
       <div :data-scroll="`contact-${index + 1}-heading`" class="header-offset"></div>
       <cds-accordion-item
-        open
+        :open="open[contact.index]"
         :title="contactName(contact)"
         size="lg"
         class="grouping-13"
         :data-focus="`contact-${index + 1}-heading`"
+        @cds-accordion-item-beingtoggled="
+          (event: any) => cdsAccordionItemBeingtoggled(event, contact)
+        "
       >
+        <div class="errors-container hide-when-less-than-two-children">
+          <!--
+          The parent div is necessary to avoid the div.header-offset below from interfering in the flex flow.
+          -->
+          <div :data-scroll="`location-contacts-${index + 1}`" class="header-offset"></div>
+          <fuzzy-match-notification-grouping-component
+            :id="`location-contacts-${index + 1}`"
+            :ref="(el: any) => setFuzzyNotificationByElement(el, index + 1)"
+          />
+        </div>
         <staff-contact-group-component
           :id="contact.index"
           v-bind:model-value="contact"
