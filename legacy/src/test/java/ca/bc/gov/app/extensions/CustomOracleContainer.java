0485b106cdc3ff819ee1568fd3b3efda2d26a7db
Paulo Gomes da Cruz Junior <paulo.cruz@gov.bc.ca>
2024-09-03 14:59:37 -0700

    chore: updating configurations

--- legacy/src/test/java/ca/bc/gov/app/extensions/CustomOracleContainer.java
+++ legacy/src/test/java/ca/bc/gov/app/extensions/CustomOracleContainer.java
@@ -3,11 +3,16 @@ package ca.bc.gov.app.extensions;
 import java.time.Duration;
 import java.util.UUID;
 import org.testcontainers.containers.OracleContainer;
+import org.testcontainers.utility.DockerImageName;
 
 public class CustomOracleContainer extends OracleContainer {
 
   public CustomOracleContainer() {
-    super("gvenzl/oracle-free:23.5-slim-faststart");
+    super(
+        DockerImageName
+            .parse("gvenzl/oracle-free:23.5-slim-faststart")
+            .asCompatibleSubstituteFor("gvenzl/oracle-xe")
+    );
 
     this.withDatabaseName("legacyfsa")
         .withUsername("THE")
