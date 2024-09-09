e86c0543d9f71f8801d410803df6fc7e349e02a6
Maria Martinez <maria.martinez@gov.bc.ca>
2024-09-09 09:41:55 -0700

    Removed param annotation

--- backend/src/main/java/ca/bc/gov/app/repository/client/SubmissionRepository.java
+++ backend/src/main/java/ca/bc/gov/app/repository/client/SubmissionRepository.java
@@ -4,21 +4,20 @@ import ca.bc.gov.app.entity.client.SubmissionEntity;
 import reactor.core.publisher.Mono;
 import java.time.LocalDateTime;
 import org.springframework.data.r2dbc.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface SubmissionRepository extends ReactiveCrudRepository<SubmissionEntity, Integer> {
-  
-  @Query(""" 
-         select count(*) from nrfc.submission
-         where submission_date >= :startTime
-         and submission_date <= :endTime
-         and upper(create_user) = upper(:createdBy)
-         """)
-  Mono<Long> countSubmissionsByTimeRangeAndCreatedBy(@Param("startTime") LocalDateTime startTime,
-                                                     @Param("endTime") LocalDateTime endTime,
-                                                     @Param("createdBy") String createdBy);
-  
+
+  @Query("""
+      select count(*) from nrfc.submission
+      where submission_date >= :startTime
+      and submission_date <= :endTime
+      and upper(create_user) = upper(:createdBy)
+      """)
+  Mono<Long> countSubmissionsByTimeRangeAndCreatedBy(LocalDateTime startTime, 
+                                                     LocalDateTime endTime,
+                                                     String createdBy);
+
 }
