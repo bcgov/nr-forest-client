78c0a8e56f18b8b7d941de1c9c86d8893ede6a3f
Fernando Terra <79578735+fterra-encora@users.noreply.github.com>
2024-10-01 14:58:08 -0300

    fix(fe: FSADT1-1421): fix date validation for pasted/autocompleted values (#1187)
    * fix: fix date validation for pasted/autocompleted values
    
    * test: remove .only
    
    ---------
    
    Co-authored-by: Maria Martinez <77364706+mamartinezmejia@users.noreply.github.com>

--- frontend/src/components/forms/DateInputComponent/index.vue
+++ frontend/src/components/forms/DateInputComponent/index.vue
@@ -204,15 +204,17 @@ const emitValueChange = (newValue: string): void => {
   emit("update:model-value", newValue);
   const someEmpty = isAnyPartEmpty();
   emit("empty", someEmpty);
-  let possiblyValid = false;
-  if (!someEmpty && focusedPart.value !== null) {
-    const otherParts = getPartsExcept(focusedPart.value);
-    const allOtherAreValid = otherParts.every((part) => validation[part]);
-    if (allOtherAreValid) {
-      possiblyValid = true;
+  if (focusedPart.value !== null) {
+    let possiblyValid = false;
+    if (!someEmpty) {
+      const otherParts = getPartsExcept(focusedPart.value);
+      const allOtherAreValid = otherParts.every((part) => validation[part]);
+      if (allOtherAreValid) {
+        possiblyValid = true;
+      }
     }
+    emit("possibly-valid", possiblyValid);
   }
-  emit("possibly-valid", possiblyValid);
 };
 
 // Watch for changes on the input
@@ -325,8 +327,8 @@ const partValidators = computed(() => ({
 
 // Update validation status on setup
 validation[DatePart.year] = selectedYear.value && validatePart(DatePart.year);
-validation[DatePart.month] = selectedYear.value && validatePart(DatePart.month);
-validation[DatePart.day] = selectedYear.value && validatePart(DatePart.day);
+validation[DatePart.month] = selectedMonth.value && validatePart(DatePart.month);
+validation[DatePart.day] = selectedDay.value && validatePart(DatePart.day);
 
 if (areAllPartsValid()) {
   validationFullDate.value = validateFullDate(selectedValue.value);
@@ -347,10 +349,10 @@ const onBlurPart = (datePart: DatePart) => (partNewValue: string) => {
     validation[DatePart.day] = validatePart(DatePart.day);
   }
 
+  selectedValue.value = buildFullDate();
   if (areAllPartsValid()) {
     validateFullDate(selectedValue.value);
   }
-  isUserEvent.value = true;
 };
 
 const onBlurYear = onBlurPart(DatePart.year);
