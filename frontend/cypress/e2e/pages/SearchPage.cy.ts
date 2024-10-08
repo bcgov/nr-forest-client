4375ad1aa897df2be0317a79cedb79ce408134bc
Fernando Terra <fernando.terra@encora.com>
2024-10-08 18:29:54 -0300

    test: validate keywords

--- frontend/cypress/e2e/pages/SearchPage.cy.ts
+++ frontend/cypress/e2e/pages/SearchPage.cy.ts
@@ -1,4 +1,15 @@
+import type { ClientSearchResult } from "@/dto/CommonTypesDto";
+
 describe("Search Page", () => {
+  let predictiveSearchCount = 0;
+
+  const checkDisplayedResults = (clientList: ClientSearchResult[]) => {
+    clientList.forEach((client) => {
+      cy.get("#search-box")
+        .find(`cds-combo-box-item[data-value^="${client.clientNumber}"]`)
+        .should("exist");
+    });
+  };
   beforeEach(() => {
     cy.intercept("https://green-domain.com/**", {
       body: `<html>
@@ -7,6 +18,14 @@ describe("Search Page", () => {
         </body>
       </html>`,
     });
+
+    // reset counter
+    predictiveSearchCount = 0;
+
+    cy.intercept("/api/clients/predictive-search?keyword=*", (req) => {
+      predictiveSearchCount++;
+      req.continue();
+    }).as("predictiveSearch");
 
     cy.viewport(1920, 1080);
     cy.visit("/");
@@ -23,15 +42,67 @@ describe("Search Page", () => {
     cy.get("h1").should("be.visible").should("contain", "Client search");
   });
 
-  describe("when user fills in the search box", () => {
+  describe("when user fills in the search box with a valid value", () => {
     beforeEach(() => {
       cy.fillFormEntry("#search-box", "car", { skipBlur: true });
     });
+
+    it("makes the API call", () => {
+      cy.wait("@predictiveSearch").then((interception) => {
+        expect(interception.request.query.keyword).to.eq("car");
+      });
+      cy.wrap(predictiveSearchCount).should("eq", 1);
+    });
+
     it("displays autocomplete results", () => {
       cy.get("#search-box")
         .find("cds-combo-box-item")
         .should("have.length", 3)
         .should("be.visible");
+
+      cy.wait("@predictiveSearch").then((interception) => {
+        const data = interception.response.body;
+
+        cy.wrap(data).should("be.an", "array").and("have.length", 3);
+
+        cy.get("#search-box")
+          .find("cds-combo-box-item")
+          .should("have.length", data.length)
+          .should("be.visible");
+
+        checkDisplayedResults(data);
+      });
+    });
+
+    describe("and types more characters", () => {
+      beforeEach(() => {
+        cy.wait("@predictiveSearch");
+        cy.fillFormEntry("#search-box", "d", { skipBlur: true });
+      });
+
+      it("makes another the API call", () => {
+        cy.wait("@predictiveSearch").then((interception) => {
+          expect(interception.request.query.keyword).to.eq("card");
+        });
+        cy.wrap(predictiveSearchCount).should("eq", 2);
+      });
+
+      it("updates the autocomplete results", () => {
+        cy.wait("@predictiveSearch").then((interception) => {
+          const data = interception.response.body;
+          cy.log(interception.response.body);
+          console.log(interception.response.body);
+
+          cy.wrap(data).should("be.an", "array").and("have.length.greaterThan", 3);
+
+          cy.get("#search-box")
+            .find("cds-combo-box-item")
+            .should("have.length", data.length)
+            .should("be.visible");
+
+          checkDisplayedResults(data);
+        });
+      });
     });
 
     describe("and user clicks a result", () => {
@@ -52,6 +123,20 @@ describe("Search Page", () => {
           );
         });
       });
+    });
+  });
+
+  describe("when user fills in the search box with an invalid value", () => {
+    beforeEach(() => {
+      cy.fillFormEntry("#search-box", "até", { skipBlur: true });
+    });
+
+    it("shows an error message", () => {
+      cy.contains("The search terms can only contain: A-Z, a-z, 0-9, space or common symbols");
+    });
+    it("makes no API call", () => {
+      cy.wait(500); // This time has to be greater than the debouncing time
+      cy.wrap(predictiveSearchCount).should("eq", 0);
     });
   });
 });
