2273b8b5506315c4467cae3a6eecaf5d7e92889a
Fernando Terra <fernando.terra@encora.com>
2024-10-18 19:13:22 -0300

    test: improve comment

--- frontend/cypress/e2e/pages/SearchPage.cy.ts
+++ frontend/cypress/e2e/pages/SearchPage.cy.ts
@@ -51,7 +51,7 @@ describe("Search Page", () => {
         pathname: "/api/clients/search",
         query: {
           keyword: "*",
-          size: "5", // This guarantees we only ask for 5 results in the predictive search
+          size: "5", // Asserts we only ask for 5 results in the predictive search
         },
       },
       (req) => {
