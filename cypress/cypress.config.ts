caf989d7703332b7329edff84cd16f008ef4014f
Maria Martinez <maria.martinez@gov.bc.ca>
2024-11-12 16:20:42 -0800

    no message

--- cypress/cypress.config.ts
+++ cypress/cypress.config.ts
@@ -2,57 +2,50 @@ import { defineConfig } from "cypress";
 import * as webpack from "@cypress/webpack-preprocessor";
 import { addCucumberPreprocessorPlugin } from "@badeball/cypress-cucumber-preprocessor";
 
-async function setupNodeEvents(
-  on: Cypress.PluginEvents,
-  config: Cypress.PluginConfigOptions
-): Promise<Cypress.PluginConfigOptions> {
-  // This is required for the preprocessor to be able to generate JSON reports after each run, and more,
+async function setupNodeEvents(on, config) {
   await addCucumberPreprocessorPlugin(on, config);
 
-  on(
-    "file:preprocessor",
-    webpack({
-      webpackOptions: {
-        resolve: {
-          extensions: [".ts", ".js"],
-        },
-        module: {
-          rules: [
-            {
-              test: /\.ts$/,
-              exclude: [/node_modules/],
-              use: [
-                {
-                  loader: "ts-loader",
-                },
-              ],
-            },
-            {
-              test: /\.feature$/,
-              use: [
-                {
-                  loader: "@badeball/cypress-cucumber-preprocessor/webpack",
-                  options: config,
-                },
-              ],
-            },
-          ],
-        },
+  on("file:preprocessor", webpack({
+    webpackOptions: {
+      resolve: {
+        extensions: [".ts", ".js"],
       },
-    })
-  );
+      module: {
+        rules: [
+          {
+            test: /\.ts$/,
+            exclude: [/node_modules/],
+            use: [{ loader: "ts-loader" }],
+          },
+          {
+            test: /\.feature$/,
+            use: [{
+              loader: "@badeball/cypress-cucumber-preprocessor/webpack",
+              options: config,
+            }],
+          },
+        ],
+      },
+    },
+  }));
 
-  // Make sure to return the config object as it might have been modified by the plugin.
+  on("before:browser:launch", (browser, launchOptions) => {
+    if (browser.name === "chrome" || browser.name === "electron") {
+      launchOptions.args.push("--js-flags=--expose-gc");
+      launchOptions.args.push("--enable-precise-memory-info");
+    }
+    return launchOptions;
+  });
+
   return config;
 }
 
 export default defineConfig({
   e2e: {
-    reporter: require.resolve("@badeball/cypress-cucumber-preprocessor/pretty-reporter"),
-    specPattern: "**/*.feature",
     setupNodeEvents,
-    defaultCommandTimeout: 10000,
-    pageLoadTimeout: 60000,
+    defaultCommandTimeout: 15000,
+    pageLoadTimeout: 45000,
+    browser: "chrome",
     experimentalMemoryManagement: true,
     numTestsKeptInMemory: 1,
   },
