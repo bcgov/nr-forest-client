b7cc337a6a1d712165a9e90257b832982db6c299
Fernando Terra <fernando.terra@encora.com>
2024-11-22 16:02:25 -0300

    feat: add summary fields (corporation)

--- frontend/src/pages/ClientDetailsPage.vue
+++ frontend/src/pages/ClientDetailsPage.vue
@@ -1,9 +1,16 @@
 <script setup lang="ts">
+import { ref, watch } from "vue";
+
 // Carbon
 import "@carbon/web-components/es/components/breadcrumb/index";
 import "@carbon/web-components/es/components/notification/index";
 import "@carbon/web-components/es/components/button/index";
 import "@carbon/web-components/es/components/tabs/index";
+import "@carbon/web-components/es/components/tag/index";
+
+// Composables
+import { useFetchTo } from "@/composables/useFetch";
+import { useRouter } from "vue-router";
 
 // @ts-ignore
 import Location16 from "@carbon/icons-vue/es/location/16";
@@ -13,7 +20,47 @@ import User16 from "@carbon/icons-vue/es/user/16";
 import NetworkEnterprise16 from "@carbon/icons-vue/es/network--enterprise/16";
 // @ts-ignore
 import RecentlyViewed16 from "@carbon/icons-vue/es/recently-viewed/16";
+// @ts-ignore
+import Check20 from "@carbon/icons-vue/es/checkmark--filled/20";
+// @ts-ignore
+import Warning20 from "@carbon/icons-vue/es/warning--filled/20";
+
 import { toTitleCase } from "@/services/ForestClientService";
+
+//Route related
+const router = useRouter();
+const clientNumber = router.currentRoute.value.params.id;
+
+const data = ref(undefined);
+
+const { error: fetchError } = useFetchTo(`/api/clients/details/${clientNumber}`, data);
+watch([fetchError], () => {
+  if (fetchError.value.message) {
+    networkErrorMsg.value = fetchError.value.message;
+  }
+});
+
+const goodStanding = (goodStanding: string): string => {
+  if (goodStanding) return goodStanding === "Y" ? "Good standing" : "Not in good standing";
+  return "Unknown";
+};
+
+const tagColor = (status: string) => {
+  switch (status) {
+    case "Active":
+      return "green";
+    case "Deactivated":
+      return "purple";
+    case "Receivership":
+      return "magenta";
+    case "Suspended":
+      return "red";
+    case "Deceased":
+      return "gray";
+    default:
+      return "";
+  }
+};
 </script>
 
 <template>
@@ -28,8 +75,7 @@ import { toTitleCase } from "@/services/ForestClientService";
 
         <h1 class="resource-details--title">
           <span>
-            <!-- TODO: Replace with real value -->
-            {{ toTitleCase("Client Name") }}
+            {{ toTitleCase(data?.business.organizationName) }}
           </span>
         </h1>
         <div>
@@ -46,12 +92,59 @@ import { toTitleCase } from "@/services/ForestClientService";
         <div data-scroll="top-notification" class="header-offset"></div>
       </div>
 
-      <div class="grouping-14">
+      <div class="grouping-14" v-if="data">
         <div class="grouping-05-short">
           <div>
             <h2 class="mg-tl-2 heading-06">Client summary</h2>
 
-            <div class="grouping-10"></div>
+            <div class="grouping-10">
+              <read-only-component label="Client number">
+                <span class="body-compact-01">{{ data.business.clientNumber }}</span>
+              </read-only-component>
+
+              <read-only-component label="Acronym">
+                <span class="body-compact-01">{{ data.business.acronym }}</span>
+              </read-only-component>
+
+              <read-only-component label="Client type">
+                <span class="body-compact-01">{{ data.business.clientTypeDesc }}</span>
+              </read-only-component>
+
+              <read-only-component label="Birthdate" v-if="data.business.clientType === 'I'">
+                <span class="body-compact-01">{{ data.business.birthdate }}</span>
+              </read-only-component>
+
+              <read-only-component
+                label="Registration number"
+                v-if="data.business.registrationNumber"
+              >
+                <span class="body-compact-01">{{ data.business.registrationNumber }}</span>
+              </read-only-component>
+
+              <read-only-component label="BC Registries standing" id="goodStanding">
+                <div class="internal-grouping-01">
+                  <span class="body-compact-01">{{
+                    goodStanding(data.business.goodStandingInd)
+                  }}</span>
+                  <Check20 v-if="data.business.goodStandingInd === 'Y'" class="good" />
+                  <Warning20 v-if="data.business.goodStandingInd !== 'Y'" class="warning" />
+                </div>
+              </read-only-component>
+
+              <read-only-component label="Status">
+                <span class="body-compact-01">
+                  <cds-tag :type="tagColor(data.business.status)">
+                    <span>{{ data.business.status }}</span>
+                  </cds-tag>
+                </span>
+              </read-only-component>
+            </div>
+
+            <div class="grouping-10" v-if="data.business.note">
+              <read-only-component label="Note">
+                <span class="body-compact-01">{{ data.business.note }}</span>
+              </read-only-component>
+            </div>
           </div>
         </div>
 
@@ -83,7 +176,7 @@ import { toTitleCase } from "@/services/ForestClientService";
         </cds-tabs>
       </div>
     </div>
-    <div class="tab-panel">
+    <div class="tab-panel" v-if="data">
       <div id="panel-locations" role="tabpanel" aria-labelledby="tab-locations" hidden></div>
       <div id="panel-contacts" role="tabpanel" aria-labelledby="tab-contacts" hidden></div>
       <div id="panel-related" role="tabpanel" aria-labelledby="tab-related" hidden></div>
@@ -91,3 +184,14 @@ import { toTitleCase } from "@/services/ForestClientService";
     </div>
   </div>
 </template>
+
+<style scoped>
+.internal-grouping-01 {
+  display: flex;
+  gap: 0.5rem;
+}
+
+.grouping-10 .grouping-11:first-child {
+  width: unset;
+}
+</style>
