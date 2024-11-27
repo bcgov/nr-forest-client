3c2df9ec549e450b03fb7c2680868c896041d1b1
Maria Martinez <maria.martinez@gov.bc.ca>
2024-11-27 15:33:35 -0800

    Added missing dto

---
+++ legacy/src/main/java/ca/bc/gov/app/dto/ForestClientDetailsDto.java
@@ -0,0 +1,33 @@
+package ca.bc.gov.app.dto;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public record ForestClientDetailsDto (
+  String clientNumber,
+  String clientName,
+  String legalFirstName,
+  String legalMiddleName,
+  String clientStatusCode,
+  String clientStatusDesc,
+  String clientTypeCode,
+  String clientTypeDesc,
+  String clientIdTypeCode,
+  String clientIdTypeDesc,
+  String clientIdentification,
+  String registryCompanyTypeCode,
+  String corpRegnNmbr,
+  String clientAcronym,
+  String wcbFirmNumber,
+  String ocgSupplierNmbr,
+  String clientComment,
+  LocalDate clientCommentUpdateDate,
+  String clientCommentUpdateUser,
+  String goodStandingInd,
+  LocalDate birthdate,
+  
+  List<ForestClientLocationDto> addresses,
+  List<ForestClientContactDto> contacts
+) {
+  
+}
