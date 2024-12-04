403b589f2e9300717cc8def527f77780749aeaf4
Paulo Gomes da Cruz Junior <paulo.cruz@gov.bc.ca>
2024-12-04 13:51:26 -0800

    chore: forcing a new build

--- legacy/src/main/java/ca/bc/gov/app/ApplicationConstants.java
+++ legacy/src/main/java/ca/bc/gov/app/ApplicationConstants.java
@@ -5,7 +5,9 @@ import lombok.NoArgsConstructor;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class ApplicationConstants {
+
   public static final String ORACLE_ATTRIBUTE_SCHEMA = "THE";
   public static final String CLIENT_NUMBER = "CLIENT_NUMBER";
   public static final String CLIENT_NUMBER_LITERAL = "clientNumber";
+
 }
