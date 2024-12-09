48644c67b4d8494ed42f3244bbc7867008986368
Fernando Terra <fernando.terra@encora.com>
2024-12-09 16:51:59 -0300

    add location interface

--- frontend/src/dto/CommonTypesDto.ts
+++ frontend/src/dto/CommonTypesDto.ts
@@ -254,6 +254,28 @@ export interface ClientDoingBusinessAs {
   doingBusinessAsName: string;
 }
 
+export interface ClientLocation {
+  clientNumber: string;
+  clientLocnCode: string;
+  clientLocnName: string;
+  addressOne: string;
+  addressTwo: string;
+  addressThree: string;
+  city: string;
+  province: string;
+  postalCode: string;
+  country: string;
+  businessPhone: string;
+  homePhone: string;
+  cellPhone: string;
+  faxNumber: string;
+  emailAddress: string;
+  locnExpiredInd: string;
+  returnedMailDate: string;
+  trustLocationInd: string;
+  cliLocnComment: string;
+}
+
 export interface ClientDetails {
   clientNumber: string;
   clientName: string;
@@ -277,4 +299,5 @@ export interface ClientDetails {
   goodStandingInd: string;
   birthdate: string;
   doingBusinessAs: ClientDoingBusinessAs[];
+  addresses: ClientLocation[];
 }
