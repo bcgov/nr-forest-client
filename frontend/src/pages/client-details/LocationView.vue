31fccd8271c4f436926f48b75fddc52dde9b18e7
Fernando Terra <fernando.terra@encora.com>
2024-12-12 14:25:28 -0300

    update html IDs

--- frontend/src/pages/client-details/LocationView.vue
+++ frontend/src/pages/client-details/LocationView.vue
@@ -19,24 +19,26 @@ const deliveryInformation = computed(() => {
   });
   return list.join("\n");
 });
+
+const indexString = props.data.clientLocnCode;
 </script>
 
 <template>
   <div class="flex-column-1_5rem">
-    <div :id="`location-${data.clientLocnCode}-address-section`" class="grouping-23">
+    <div :id="`location-${indexString}-address-section`" class="grouping-23">
       <span class="body-compact-01">{{ data.addressOne }}</span>
       <span class="body-compact-01">{{ data.city }}, {{ data.provinceDesc }}</span>
       <span class="body-compact-01">{{ data.country }}</span>
       <span class="body-compact-01">{{ data.postalCode }}</span>
     </div>
     <div
-      :id="`location-${data.clientLocnCode}-delivery-section`"
+      :id="`location-${indexString}-delivery-section`"
       class="grouping-23"
       v-if="deliveryInformation"
     >
       <read-only-component
         label="Delivery information"
-        :id="`deliveryInformation-${data.clientLocnCode}`"
+        :id="`location-${indexString}-deliveryInformation`"
       >
         <span
           class="body-compact-01"
@@ -44,51 +46,47 @@ const deliveryInformation = computed(() => {
         ></span>
       </read-only-component>
     </div>
-    <div
-      :id="`location-${data.clientLocnCode}-email-section`"
-      class="grouping-23"
-      v-if="data.emailAddress"
-    >
-      <read-only-component label="Email address" :id="`emailAddress-${data.clientLocnCode}`">
+    <div :id="`location-${indexString}-email-section`" class="grouping-23" v-if="data.emailAddress">
+      <read-only-component label="Email address" :id="`location-${indexString}-emailAddress`">
         <span class="body-compact-01">{{ data.emailAddress }}</span>
       </read-only-component>
     </div>
     <div
-      :id="`location-${data.clientLocnCode}-phone-section`"
+      :id="`location-${indexString}-phone-section`"
       class="grouping-10 no-padding margin-left-1_75rem"
       v-if="data.businessPhone || data.cellPhone || data.faxNumber"
     >
       <read-only-component
         label="Primary phone number"
-        :id="`primaryPhoneNumber-${data.clientLocnCode}`"
+        :id="`location-${indexString}-primaryPhoneNumber`"
         v-if="data.businessPhone"
       >
         <span class="body-compact-01">{{ data.businessPhone }}</span>
       </read-only-component>
       <read-only-component
         label="Secondary phone number"
-        :id="`secondaryPhoneNumber-${data.clientLocnCode}`"
+        :id="`location-${indexString}-secondaryPhoneNumber`"
         v-if="data.cellPhone"
       >
         <span class="body-compact-01">{{ data.cellPhone }}</span>
       </read-only-component>
       <read-only-component
         label="Tertiary phone number"
-        :id="`tertiaryPhoneNumber-${data.clientLocnCode}`"
+        :id="`location-${indexString}-tertiaryPhoneNumber`"
         v-if="data.homePhone"
       >
         <span class="body-compact-01">{{ data.homePhone }}</span>
       </read-only-component>
-      <read-only-component label="Fax" :id="`fax-${data.clientLocnCode}`" v-if="data.faxNumber">
+      <read-only-component label="Fax" :id="`location-${indexString}-fax`" v-if="data.faxNumber">
         <span class="body-compact-01">{{ data.faxNumber }}</span>
       </read-only-component>
     </div>
     <div
-      :id="`location-${data.clientLocnCode}-note-section`"
+      :id="`location-${indexString}-note-section`"
       class="grouping-23"
       v-if="data.cliLocnComment"
     >
-      <read-only-component label="Note" :id="`locationNote-${data.clientLocnCode}`">
+      <read-only-component label="Note" :id="`location-${indexString}-note`">
         <span
           class="body-compact-01"
           v-dompurify-html="getFormattedHtml(data.cliLocnComment)"
