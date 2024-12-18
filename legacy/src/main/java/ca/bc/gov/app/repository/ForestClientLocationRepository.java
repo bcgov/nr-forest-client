5da9dbea06f40fa130e2a0ef9362a8c443122c66
Paulo Gomes da Cruz Junior <paulo.cruz@gov.bc.ca>
2024-12-18 11:32:28 -0800

    chore: fixing sonar issues

--- legacy/src/main/java/ca/bc/gov/app/repository/ForestClientLocationRepository.java
+++ legacy/src/main/java/ca/bc/gov/app/repository/ForestClientLocationRepository.java
@@ -11,6 +11,13 @@ import reactor.core.publisher.Flux;
 public interface ForestClientLocationRepository
     extends ReactiveCrudRepository<ForestClientLocationEntity, ForestClientLocationEntityId> {
 
+  /**
+   * Finds client locations by matching the address and postal code.
+   *
+   * @param address    the address to match
+   * @param postalCode the postal code to match
+   * @return a Flux containing the matching ForestClientLocationEntity objects
+   */
   @Query("""
       select *
       from the.client_location
@@ -23,6 +30,16 @@ public interface ForestClientLocationRepository
       and upper(postal_code) = upper(replace(:postalCode, ' ', ''))""")
   Flux<ForestClientLocationEntity> matchBy(String address, String postalCode);
 
+  /**
+   * Finds client locations by matching the address, city, province, country, and postal code.
+   *
+   * @param address    the address to match
+   * @param postalCode the postal code to match
+   * @param city       the city to match
+   * @param province   the province to match
+   * @param country    the country to match
+   * @return a Flux containing the matching ForestClientLocationEntity objects
+   */
   @Query("""
       select *
       from the.client_location
@@ -44,6 +61,12 @@ public interface ForestClientLocationRepository
       String country
   );
 
+  /**
+   * Finds all client locations by client number.
+   *
+   * @param clientNumber the client number to match
+   * @return a Flux containing the matching ForestClientLocationEntity objects
+   */
   Flux<ForestClientLocationEntity> findAllByClientNumber(String clientNumber);
 
 }
