59868a2f7e3d36036b6a8702a6fcfc48188d09c5
Fernando Terra <79578735+fterra-encora@users.noreply.github.com>
2024-12-27 19:06:37 -0300

    feat(fe:FSADT1-1595): CLIENT_VIEWER Client Contacts Section - Front-End (#1369)
    * add the contacts tab contents
    
    * clear outdated data on stubs
    
    * add empty contacts view
    
    * hide email when empty
    
    * test: display contacts tab content
    
    * test: display contact information
    
    * test: fix phone numbers on tests
    
    * center empty list view (contacts)
    
    * test: display/hide instruction text
    
    * add contact stub businessPhone only
    
    * update svg size
    
    * replace pictogram

--- frontend/src/dto/CommonTypesDto.ts
+++ frontend/src/dto/CommonTypesDto.ts
@@ -276,6 +276,23 @@ export interface ClientLocation {
   returnedMailDate: string;
   trustLocationInd: string;
   cliLocnComment: string;
+  createdBy: string;
+  updatedBy: string;
+}
+
+export interface ClientContact {
+  clientNumber: string;
+  clientLocnCode: string[];
+  contactCode: string;
+  contactName: string;
+  contactTypeCode: string;
+  contactTypeDesc: string;
+  businessPhone: string;
+  secondaryPhone: string;
+  faxNumber: string;
+  emailAddress: string;
+  createdBy: string;
+  updatedBy: string;
 }
 
 export interface ClientDetails {
@@ -302,4 +319,5 @@ export interface ClientDetails {
   birthdate: string;
   doingBusinessAs: ClientDoingBusinessAs[];
   addresses: ClientLocation[];
+  contacts: ClientContact[];
 }
