0724c96d7a266a89eaecdaf7682ce203ca0a969f
Maria Martinez <maria.martinez@gov.bc.ca>
2025-02-21 09:02:12 -0800

    Fixed SonarCloud issues

--- legacy/src/main/java/ca/bc/gov/app/repository/ClientStatusCodeRepository.java
+++ legacy/src/main/java/ca/bc/gov/app/repository/ClientStatusCodeRepository.java
@@ -1,16 +1,16 @@
 package ca.bc.gov.app.repository;
 
+import ca.bc.gov.app.dto.CodeNameDto;
+import ca.bc.gov.app.entity.ClientStatusCodeEntity;
 import java.time.LocalDate;
 import org.springframework.data.r2dbc.repository.Query;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import org.springframework.stereotype.Repository;
-import ca.bc.gov.app.dto.CodeNameDto;
-import ca.bc.gov.app.entity.ClientStatusCodeEntity;
 import reactor.core.publisher.Flux;
 
 @Repository
 public interface ClientStatusCodeRepository
-  extends ReactiveCrudRepository<ClientStatusCodeEntity, String> {
+    extends ReactiveCrudRepository<ClientStatusCodeEntity, String> {
 
   @Query("""
       SELECT CLIENT_STATUS_CODE, DESCRIPTION AS NAME
