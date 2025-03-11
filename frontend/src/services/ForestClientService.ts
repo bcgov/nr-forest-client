55f44ddf3f134bdc3b302146e45ed5e1f6b4a4df
Fernando Terra <fernando.terra@encora.com>
2025-03-11 14:56:06 -0300

    test: fix tests

--- frontend/src/services/ForestClientService.ts
+++ frontend/src/services/ForestClientService.ts
@@ -337,36 +337,6 @@ export const getActionLabel = (action: string) => {
   return fieldLabelsByAction.get(action || "") || "Unknown";
 };
 
-export const getOldValue = (path: string, data: Ref<ClientDetails> | ClientDetails) => {
-  if (!data) {
-    console.warn("Old value was called with undefined data!", path);
-    return "N/A";
-  }
-
-  const clientData = unref(data);
-  const fieldName = path.split("/").pop() || "";
-
-  if (fieldName in clientData) {
-    return clientData[fieldName as keyof ClientDetails] || "N/A";
-  }
-
-  for (const [, value] of Object.entries(clientData)) {
-    if (Array.isArray(value)) {
-      for (const item of value) {
-        if (fieldName in item) {
-          if (Array.isArray(item[fieldName])) {
-            const arrayValue = item[fieldName as keyof typeof item];
-            return arrayValue?.[0] || "N/A";
-          }
-          return item[fieldName as keyof typeof item] || "N/A";
-        }
-      }
-    }
-  }
-
-  return "N/A";
-};
-
 export const updateSelectedReason = (
   selectedOption: CodeNameType,
   index: number,
