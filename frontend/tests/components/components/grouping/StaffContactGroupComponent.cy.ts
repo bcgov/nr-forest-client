1fe21fbd0b5bc5bdf3ebfe0a3d29ee81f863e968
Fernando Terra <fernando.terra@encora.com>
2025-03-18 11:20:30 -0300

    test: component full name

--- frontend/tests/components/components/grouping/StaffContactGroupComponent.cy.ts
+++ frontend/tests/components/components/grouping/StaffContactGroupComponent.cy.ts
@@ -10,6 +10,51 @@ describe("<StaffLocationGroupComponent />", () => {
     cy.fixture("roles.json").as("rolesFixture");
   });
 
+  const firstNameSelector0 = "#firstName_0";
+  const lastNameSelector0 = "#lastName_0";
+  const fullNameSelector0 = "#fullName_0";
+
+  it("should render the component with fields First and Last names by default", () => {
+    cy.get("@contactFixture").then((contact: any) => {
+      cy.get("@rolesFixture").then((roles) => {
+        cy.mount(StaffContactGroupComponent, {
+          props: {
+            id: 0,
+            modelValue: contact,
+            roleList: roles,
+            validations: [],
+          },
+        });
+      });
+    });
+
+    cy.get(firstNameSelector0).should("be.visible");
+    cy.get(lastNameSelector0).should("be.visible");
+
+    cy.get(fullNameSelector0).should("not.exist");
+  });
+
+  it("should render the component with field Full name instead of First and Last names", () => {
+    cy.get("@contactFixture").then((contact: any) => {
+      cy.get("@rolesFixture").then((roles) => {
+        cy.mount(StaffContactGroupComponent, {
+          props: {
+            id: 0,
+            modelValue: contact,
+            roleList: roles,
+            validations: [],
+            singleInputForName: true,
+          },
+        });
+      });
+    });
+
+    cy.get(fullNameSelector0).should("be.visible");
+
+    cy.get(firstNameSelector0).should("not.exist");
+    cy.get(lastNameSelector0).should("not.exist");
+  });
+
   const deleteSelector1 = "#deleteContact_1";
 
   it("should render the component with a Delete button by default when id > 0", () => {
