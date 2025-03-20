64683f5cde9751616f929e8a065c56c87c91c429
maria.martinez@gov.bc.ca <maria.martinez@gov.bc.ca>
2025-03-20 13:50:30 -0700

    feat(be:FSADT1-1703): Changed predictive query to use like instead

--- legacy/src/main/java/ca/bc/gov/app/repository/ForestClientRepository.java
+++ legacy/src/main/java/ca/bc/gov/app/repository/ForestClientRepository.java
@@ -1,6 +1,5 @@
 package ca.bc.gov.app.repository;
 
-import ca.bc.gov.app.dto.ForestClientDetailsDto;
 import ca.bc.gov.app.dto.ForestClientInformationDto;
 import ca.bc.gov.app.dto.PredictiveSearchResultDto;
 import ca.bc.gov.app.entity.ForestClientEntity;
@@ -111,131 +110,55 @@ public interface ForestClientRepository extends ReactiveCrudRepository<ForestCli
 
   @Query("""
       SELECT
-          c.client_number,
-          c.CLIENT_ACRONYM AS client_acronym,
-          c.client_name,
-          c.legal_first_name AS client_first_name,
-          dba.doing_business_as_name AS doing_business_as,
-          c.client_identification,
-          c.legal_middle_name AS client_middle_name,
-          cl.city AS city,
-          ctc.description AS client_type,
-          csc.description AS client_status,
-          (
-              CASE WHEN c.client_number = :value THEN 112 ELSE 0 END +
-              CASE WHEN c.CLIENT_ACRONYM = :value THEN 111 ELSE 0 END +
-              (UTL_MATCH.JARO_WINKLER_SIMILARITY(
-                  c.client_name || ' ' || c.legal_first_name, :value
-              ) + 10) +
-              (UTL_MATCH.JARO_WINKLER_SIMILARITY(dba.doing_business_as_name, :value) + 7) +
-              CASE WHEN c.client_identification = :value THEN 106 ELSE 0 END +
-              UTL_MATCH.JARO_WINKLER_SIMILARITY(c.legal_middle_name, :value)
-          ) AS score
-      FROM the.forest_client c
-      LEFT JOIN the.CLIENT_DOING_BUSINESS_AS dba ON c.client_number = dba.client_number
-      LEFT JOIN the.CLIENT_TYPE_CODE ctc ON c.client_type_code = ctc.client_type_code
-      LEFT JOIN the.CLIENT_LOCATION cl ON c.client_number = cl.client_number
-      LEFT JOIN the.CLIENT_STATUS_CODE csc ON c.client_status_code = csc.client_status_code
+          C.CLIENT_NUMBER,
+          C.CLIENT_ACRONYM AS CLIENT_ACRONYM,
+          C.CLIENT_NAME,
+          C.LEGAL_FIRST_NAME AS CLIENT_FIRST_NAME,
+          DBA.DOING_BUSINESS_AS_NAME AS DOING_BUSINESS_AS,
+          C.CLIENT_IDENTIFICATION,
+          C.LEGAL_MIDDLE_NAME AS CLIENT_MIDDLE_NAME,
+          CL.CITY AS CITY,
+          CTC.DESCRIPTION AS CLIENT_TYPE,
+          CSC.DESCRIPTION AS CLIENT_STATUS,
+          100 AS SCORE
+      FROM THE.FOREST_CLIENT C
+      LEFT JOIN THE.CLIENT_DOING_BUSINESS_AS DBA ON C.CLIENT_NUMBER = DBA.CLIENT_NUMBER
+      LEFT JOIN THE.CLIENT_TYPE_CODE CTC ON C.CLIENT_TYPE_CODE = CTC.CLIENT_TYPE_CODE
+      LEFT JOIN THE.CLIENT_LOCATION CL ON C.CLIENT_NUMBER = CL.CLIENT_NUMBER
+      LEFT JOIN THE.CLIENT_STATUS_CODE CSC ON C.CLIENT_STATUS_CODE = CSC.CLIENT_STATUS_CODE
       WHERE
           (
-              c.client_number = :value
-              OR c.CLIENT_ACRONYM = :value
-              OR UTL_MATCH.JARO_WINKLER_SIMILARITY(c.client_name, :value) >= 90
-              OR c.client_name LIKE '%' || :value || '%'
-              OR UTL_MATCH.JARO_WINKLER_SIMILARITY(c.legal_first_name, :value) >= 90
-              OR UTL_MATCH.JARO_WINKLER_SIMILARITY(dba.doing_business_as_name, :value) >= 90
-              OR dba.doing_business_as_name LIKE '%' || :value || '%'
-              OR c.client_identification = :value
-              OR UTL_MATCH.JARO_WINKLER_SIMILARITY(c.legal_middle_name, :value) >= 90
-              OR c.legal_middle_name LIKE '%' || :value || '%'
-              OR (
-                  c.client_type_code = 'I' AND (
-                      UTL_MATCH.JARO_WINKLER_SIMILARITY(
-                          TRIM(
-                              COALESCE(c.legal_first_name, '') || ' ' || 
-                              COALESCE(c.legal_middle_name, '') || 
-                              COALESCE(c.client_name, '')
-                          ), :value
-                      ) >= 90
-                      OR UTL_MATCH.JARO_WINKLER_SIMILARITY(
-                          TRIM(
-                              COALESCE(c.legal_first_name, '') || ' ' || 
-                              COALESCE(c.client_name, '')
-                          ), :value
-                      ) >= 90
-                      OR UTL_MATCH.JARO_WINKLER_SIMILARITY(
-                          TRIM(
-                              COALESCE(c.client_name, '') || ' ' || 
-                              COALESCE(c.legal_middle_name, '') || 
-                              COALESCE(c.legal_first_name, '')
-                          ), :value
-                      ) >= 90
-                      OR UTL_MATCH.JARO_WINKLER_SIMILARITY(
-                          TRIM(
-                              COALESCE(c.client_name, '') || ' ' || 
-                              COALESCE(c.legal_first_name, '')
-                          ), :value
-                      ) >= 90
-                  )
-              )
+              UPPER(C.CLIENT_NUMBER) LIKE UPPER('%' || :value || '%')
+              OR UPPER(C.CLIENT_ACRONYM) LIKE UPPER('%' || :value || '%')
+              OR UPPER(C.CLIENT_NAME) LIKE UPPER('%' || :value || '%')
+              OR UPPER(C.LEGAL_FIRST_NAME) LIKE UPPER('%' || :value || '%')
+              OR UPPER(C.LEGAL_MIDDLE_NAME) LIKE UPPER('%' || :value || '%')
+              OR UPPER(DBA.DOING_BUSINESS_AS_NAME) LIKE UPPER('%' || :value || '%')
+              OR UPPER(C.CLIENT_IDENTIFICATION) LIKE UPPER('%' || :value || '%')
           )
-          AND cl.CLIENT_LOCN_CODE = '00'
-      ORDER BY score DESC
+          AND CL.CLIENT_LOCN_CODE = '00'
       OFFSET :offset ROWS FETCH NEXT :limit ROWS ONLY""")
   Flux<PredictiveSearchResultDto> findByPredictiveSearch(String value, int limit, long offset);
 
   @Query("""
       SELECT
-          count(c.client_number)
-      FROM the.forest_client c
-      LEFT JOIN the.CLIENT_DOING_BUSINESS_AS dba ON c.client_number = dba.client_number
-      LEFT JOIN the.CLIENT_TYPE_CODE ctc ON c.client_type_code = ctc.client_type_code
-      LEFT JOIN the.CLIENT_LOCATION cl ON c.client_number = cl.client_number
-      LEFT JOIN the.CLIENT_STATUS_CODE csc ON c.client_status_code = csc.client_status_code
+          COUNT(C.CLIENT_NUMBER)
+      FROM THE.FOREST_CLIENT C
+      LEFT JOIN THE.CLIENT_DOING_BUSINESS_AS DBA ON C.CLIENT_NUMBER = DBA.CLIENT_NUMBER
+      LEFT JOIN THE.CLIENT_TYPE_CODE CTC ON C.CLIENT_TYPE_CODE = CTC.CLIENT_TYPE_CODE
+      LEFT JOIN THE.CLIENT_LOCATION CL ON C.CLIENT_NUMBER = CL.CLIENT_NUMBER
+      LEFT JOIN THE.CLIENT_STATUS_CODE CSC ON C.CLIENT_STATUS_CODE = CSC.CLIENT_STATUS_CODE
       WHERE
-          (
-              c.client_number = :value
-              OR c.CLIENT_ACRONYM = :value
-              OR UTL_MATCH.JARO_WINKLER_SIMILARITY(c.client_name, :value) >= 90
-              OR c.client_name LIKE '%' || :value || '%'
-              OR UTL_MATCH.JARO_WINKLER_SIMILARITY(c.legal_first_name, :value) >= 90
-              OR UTL_MATCH.JARO_WINKLER_SIMILARITY(dba.doing_business_as_name, :value) >= 90
-              OR dba.doing_business_as_name LIKE '%' || :value || '%'
-              OR c.client_identification = :value
-              OR UTL_MATCH.JARO_WINKLER_SIMILARITY(c.legal_middle_name, :value) >= 90
-              OR c.legal_middle_name LIKE '%' || :value || '%'
-              OR (
-                  c.client_type_code = 'I' AND (
-                      UTL_MATCH.JARO_WINKLER_SIMILARITY(
-                          TRIM(
-                              COALESCE(c.legal_first_name, '') || ' ' || 
-                              COALESCE(c.legal_middle_name, '') || 
-                              COALESCE(c.client_name, '')
-                          ), :value
-                      ) >= 90
-                      OR UTL_MATCH.JARO_WINKLER_SIMILARITY(
-                          TRIM(
-                              COALESCE(c.legal_first_name, '') || ' ' || 
-                              COALESCE(c.client_name, '')
-                          ), :value
-                      ) >= 90
-                      OR UTL_MATCH.JARO_WINKLER_SIMILARITY(
-                          TRIM(
-                              COALESCE(c.client_name, '') || ' ' || 
-                              COALESCE(c.legal_middle_name, '') || 
-                              COALESCE(c.legal_first_name, '')
-                          ), :value
-                      ) >= 90
-                      OR UTL_MATCH.JARO_WINKLER_SIMILARITY(
-                          TRIM(
-                              COALESCE(c.client_name, '') || ' ' || 
-                              COALESCE(c.legal_first_name, '')
-                          ), :value
-                      ) >= 90
-                  )
-              )
-          )
-          AND cl.CLIENT_LOCN_CODE = '00'""")
+      (
+          UPPER(C.CLIENT_NUMBER) LIKE UPPER('%' || :value || '%')
+          OR UPPER(C.CLIENT_ACRONYM) LIKE UPPER('%' || :value || '%')
+          OR UPPER(C.CLIENT_NAME) LIKE UPPER('%' || :value || '%')
+          OR UPPER(C.LEGAL_FIRST_NAME) LIKE UPPER('%' || :value || '%')
+          OR UPPER(C.LEGAL_MIDDLE_NAME) LIKE UPPER('%' || :value || '%')
+          OR UPPER(DBA.DOING_BUSINESS_AS_NAME) LIKE UPPER('%' || :value || '%')
+          OR UPPER(C.CLIENT_IDENTIFICATION) LIKE UPPER('%' || :value || '%')
+      )
+      AND CL.CLIENT_LOCN_CODE = '00'""")
   Mono<Long> countByPredictiveSearch(String value);
 
   @Query("""
