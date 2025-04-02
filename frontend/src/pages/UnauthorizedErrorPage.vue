03fcb163bdf8e7152d1cc2bdf646efc3b5108161
Fernando Terra <fernando.terra@encora.com>
2025-04-02 11:21:07 -0300

    add unauthorized page

---
+++ frontend/src/pages/UnauthorizedErrorPage.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+// Carbon
+import "@carbon/web-components/es/components/notification/index";
+</script>
+
+<template>
+  <div id="screen" class="no-menu-content">
+    <cds-actionable-notification
+      v-shadow="true"
+      low-contrast="true"
+      hide-close-button="true"
+      open="true"
+      kind="error"
+      title="You are not authorized to access this page"
+    >
+      <div>
+        Please create an "Application Access"
+        <a target="_blank" href="https://apps.nrs.gov.bc.ca/int/jira/servicedesk/customer/portals">
+          request ticket</a
+        >
+        to gain access to this system.
+      </div>
+    </cds-actionable-notification>
+  </div>
+</template>
