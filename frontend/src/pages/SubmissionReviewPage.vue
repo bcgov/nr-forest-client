aca3c258ac0787b1c00b827ca6a50362b31c54ae
maria.martinez@gov.bc.ca <maria.martinez@gov.bc.ca>
2025-04-10 14:26:52 -0700

    fix(fe:FSADT1-1782): Added suspend role in the submission review page

--- frontend/src/pages/SubmissionReviewPage.vue
+++ frontend/src/pages/SubmissionReviewPage.vue
@@ -300,7 +300,12 @@ const renderListItem = (label, clientNumbers) => {
   );
 };
 
-const userhasAuthority = ["CLIENT_VIEWER", "CLIENT_EDITOR", "CLIENT_ADMIN"].some(authority => ForestClientUserSession.authorities.includes(authority));
+const userhasAuthority = [
+  "CLIENT_VIEWER",
+  "CLIENT_EDITOR",
+  "CLIENT_ADMIN",
+  "CLIENT_SUSPEND",
+].some((authority) => ForestClientUserSession.authorities.includes(authority));
 const isNotEditor = !ForestClientUserSession.authorities.includes('CLIENT_EDITOR') && !ForestClientUserSession.authorities.includes('CLIENT_ADMIN');
 
 if (isNotEditor) {
