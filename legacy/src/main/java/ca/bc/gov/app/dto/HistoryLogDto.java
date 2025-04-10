6ab60943ed5759d959f0e914a173a20dc523a10b
maria.martinez@gov.bc.ca <maria.martinez@gov.bc.ca>
2025-04-10 11:03:48 -0700

    no message

--- legacy/src/main/java/ca/bc/gov/app/dto/HistoryLogDto.java
+++ legacy/src/main/java/ca/bc/gov/app/dto/HistoryLogDto.java
@@ -2,20 +2,26 @@ package ca.bc.gov.app.dto;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.With;
 
 @With
 public record HistoryLogDto(
     String tableName, 
     String idx, 
-    String identifierLabel, 
+    String identifierLabel,
+    @JsonIgnore
     String columnName,
-    String oldValue, 
+    @JsonIgnore
+    String oldValue,
+    @JsonIgnore
     String newValue,
     LocalDateTime updateTimestamp, 
     String updateUserid,
     String changeType, 
+    @JsonIgnore
     String actionCode,
+    @JsonIgnore
     String reason,
     List<HistoryLogDetailsDto> details,
     List<HistoryLogReasonsDto> reasons
