16e6393a4eaf8d3f9f2ea78a70a877c81b2e2b4c
Paulo Gomes da Cruz Junior <paulo.cruz@gov.bc.ca>
2025-04-28 16:53:54 -0700

    test: fixing test

--- legacy/src/test/java/ca/bc/gov/app/controller/ClientSearchControllerIntegrationTest.java
+++ legacy/src/test/java/ca/bc/gov/app/controller/ClientSearchControllerIntegrationTest.java
@@ -349,10 +349,8 @@ class ClientSearchControllerIntegrationTest extends
       .expectHeader()
       .value("X-Total-Count", count -> assertThat(count).isEqualTo("2"))
       .expectBody()
-      .consumeWith(result -> {
-          String body = new String(result.getResponseBody());
-          assertThat(body).isEqualTo("[]");
-      });
+      .jsonPath("$[0].clientNumber").isNotEmpty()
+      .jsonPath("$[0].clientNumber").isEqualTo("00000158");
   }
   
   private static Stream<Arguments> byEmail() {
