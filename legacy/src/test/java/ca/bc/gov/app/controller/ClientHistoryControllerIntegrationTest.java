8524298bd3e5d213bf38e2e62520268b351559b6
maria.martinez@gov.bc.ca <maria.martinez@gov.bc.ca>
2025-04-30 12:21:26 -0700

    Increased coverage

--- legacy/src/test/java/ca/bc/gov/app/controller/ClientHistoryControllerIntegrationTest.java
+++ legacy/src/test/java/ca/bc/gov/app/controller/ClientHistoryControllerIntegrationTest.java
@@ -8,7 +8,6 @@ import ca.bc.gov.app.exception.NoValueFoundException;
 import ca.bc.gov.app.extensions.AbstractTestContainerIntegrationTest;
 import java.util.stream.Stream;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang3.StringUtils;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
@@ -32,9 +31,8 @@ public class ClientHistoryControllerIntegrationTest extends
   void shouldReturnHistoryLogsByClientNumber(
       String clientNumber,
       String expectedClientNumber,
-      Class<RuntimeException> exception
+      Class<? extends RuntimeException> expectedExceptionClass
   ) {
-    
     ResponseSpec response =
         client
             .get()
@@ -45,15 +43,18 @@ public class ClientHistoryControllerIntegrationTest extends
             .header("Content-Type", MediaType.APPLICATION_JSON_VALUE)
             .exchange();
 
-    if (StringUtils.isNotBlank(expectedClientNumber)) {
+    if (expectedClientNumber != null) {
       response
-        .expectStatus().isOk()
-        .expectBodyList(HistoryLogDto.class)
-        .value(logs -> assertThat(logs).isNotEmpty());
-    }
-
-    if (exception != null) {
-      response.expectStatus().is4xxClientError();
+          .expectStatus().isOk()
+          .expectBodyList(HistoryLogDto.class)
+          .value(logs -> assertThat(logs).isNotNull());
+    } else if (expectedExceptionClass != null) {
+      response
+          .expectStatus().is4xxClientError()
+          .expectBody()
+          .jsonPath("$.message").value(
+              msg -> assertThat((String) msg)
+              .contains("clientNumber"));
     }
   }
 
@@ -65,8 +66,15 @@ public class ClientHistoryControllerIntegrationTest extends
         // Invalid case: missing client number
         Arguments.of(null, null, MissingRequiredParameterException.class),
 
-        // Invalid case: client not found
-        Arguments.of("99999999", null, NoValueFoundException.class));
+        // Invalid case: empty client number
+        Arguments.of("", null, MissingRequiredParameterException.class),
+
+        // Invalid case: malformed client number
+        Arguments.of("!@#$%", null, MissingRequiredParameterException.class),
+
+        // Invalid case: not found
+        Arguments.of("99999999", null, NoValueFoundException.class)
+    );
   }
   
 }
