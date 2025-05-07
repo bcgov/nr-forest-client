50d347aaf9a802ea0dc6cc2eaeb13d11764e82fc
Fernando Terra <fernando.terra@encora.com>
2025-05-07 16:49:33 -0300

    test: reduce wiremock delays for e2e tests

---
+++ frontend/stub-tests.ts
@@ -0,0 +1,39 @@
+import fs from "fs";
+import path from "path";
+
+const isFile = (fileName: string) => {
+  return fs.lstatSync(fileName).isFile();
+};
+const isJson = (fileName: string) => {
+  return fileName.endsWith(".json");
+};
+const sourceFolderPath = "stub";
+const destinationFolderPath = "stub-tests";
+
+if (fs.existsSync(destinationFolderPath)) {
+  fs.rmSync(destinationFolderPath, { recursive: true, force: true });
+}
+
+fs.cpSync(sourceFolderPath, destinationFolderPath, { recursive: true });
+
+const mappingsFolderName = "mappings";
+const mappingsFolderPath = path.join(destinationFolderPath, mappingsFolderName);
+
+const defaultDelay = 50;
+
+fs.readdirSync(mappingsFolderPath)
+  .map((fileName) => {
+    return path.join(mappingsFolderPath, fileName);
+  })
+  .filter(isFile)
+  .filter(isJson)
+  .forEach((fileName) => {
+    const rawData = fs.readFileSync(fileName, "utf8");
+    const data = JSON.parse(rawData);
+    data.mappings.forEach((mapping) => {
+      if (mapping.response.fixedDelayMilliseconds) {
+        mapping.response.fixedDelayMilliseconds = defaultDelay;
+      }
+    });
+    fs.writeFileSync(fileName, JSON.stringify(data, null, 2), "utf8");
+  });
