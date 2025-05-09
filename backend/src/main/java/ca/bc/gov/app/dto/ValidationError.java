a056e1c55cec36386149aef476a8b32e46c8723a
Paulo Gomes da Cruz Junior <paulo.cruz@gov.bc.ca>
2025-05-09 09:47:35 -0700

    chore: adding match value to validator

--- backend/src/main/java/ca/bc/gov/app/dto/ValidationError.java
+++ backend/src/main/java/ca/bc/gov/app/dto/ValidationError.java
@@ -1,18 +1,46 @@
 package ca.bc.gov.app.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import java.io.Serializable;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 import org.apache.commons.lang3.StringUtils;
 
 /**
- * Represents a validation error for a specific field in a data structure.
- * This class is used to capture validation failures, providing information
- * about the field that failed validation and the corresponding error message.
- *
- * @param fieldId The identifier of the field that failed validation.
- * @param errorMsg The error message describing the validation failure.
+ * Represents a validation error for a specific field in a data structure. This class is used to
+ * capture validation failures, providing information about the field that failed validation and the
+ * corresponding error message.
  */
-public record ValidationError(String fieldId, String errorMsg) implements Serializable {
+@AllArgsConstructor
+@Getter
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(Include.NON_NULL)
+@EqualsAndHashCode
+@ToString
+public class ValidationError implements Serializable {
+
+  private final String fieldId;
+  private final String errorMsg;
+  private final String match;
+
+  public ValidationError(){
+    this.fieldId = null;
+    this.errorMsg = null;
+    this.match = null;
+  }
+
+  public ValidationError(String fieldId, String errorMsg) {
+    this.fieldId = fieldId;
+    this.errorMsg = errorMsg;
+    this.match = null;
+  }
+
   @JsonIgnore
   public boolean isValid() {
     return !StringUtils.isAllBlank(fieldId, errorMsg);
