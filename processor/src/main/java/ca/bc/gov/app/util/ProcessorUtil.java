54a6055c34f0b7b7595e19d8d8181e37332d18a7
Maria Martinez <77364706+mamartinezmejia@users.noreply.github.com>
2025-06-09 12:39:12 -0700

    chore(be): cleaned up code and moved queries (#1627)
    * no message
    
    * Moved queries

--- processor/src/main/java/ca/bc/gov/app/util/ProcessorUtil.java
+++ processor/src/main/java/ca/bc/gov/app/util/ProcessorUtil.java
@@ -98,7 +98,7 @@ public class ProcessorUtil {
   }
 
   private static String getStringFromPattern(String input, Pattern pattern) {
-    if(StringUtils.isBlank(input))
+    if (StringUtils.isBlank(input))
       return StringUtils.EMPTY;
     Matcher matcher = pattern.matcher(input);
     if (matcher.find()) {
