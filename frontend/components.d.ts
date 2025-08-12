3a5b729739eabd49097961e0d4fee9bbb180f413
maria.martinez@gov.bc.ca <maria.martinez@gov.bc.ca>
2025-08-12 14:36:33 -0700

    feat(FSADT1-1961): Location – Remove EMAIL and PHONE section (only for internal)

--- frontend/components.d.ts
+++ frontend/components.d.ts
@@ -9,10 +9,8 @@ declare module 'vue' {
   export interface GlobalComponents {
     AddressGroupComponent: typeof import('./src/components/grouping/AddressGroupComponent.vue')['default']
     AutoCompleteInputComponent: typeof import('./src/components/forms/AutoCompleteInputComponent.vue')['default']
-    ClientDetailsLocationGroupComponent: typeof import('./src/components/grouping/ClientDetailsLocationGroupComponent.vue')['default']
     ComboBoxInputComponent: typeof import('./src/components/forms/ComboBoxInputComponent.vue')['default']
     ContactGroupComponent: typeof import('./src/components/grouping/ContactGroupComponent.vue')['default']
-    copy: typeof import('./src/components/grouping/StaffLocationGroupComponent copy.vue')['default']
     DataFetcher: typeof import('./src/components/DataFetcher.vue')['default']
     DateInputComponent: typeof import('./src/components/forms/DateInputComponent/index.vue')['default']
     DateInputPart: typeof import('./src/components/forms/DateInputComponent/DateInputPart.vue')['default']
