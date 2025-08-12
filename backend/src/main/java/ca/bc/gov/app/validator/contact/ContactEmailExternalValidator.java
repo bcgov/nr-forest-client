335856a3cc89c10745ebd5edae31bbc0add2cce7
maria.martinez@gov.bc.ca <maria.martinez@gov.bc.ca>
2025-08-11 18:48:33 -0700

    feat(FSADT1-1958): Remove mandatory requirements for Contact EMAIL and PHONE (only for internal)

--- backend/src/main/java/ca/bc/gov/app/validator/contact/ContactEmailExternalValidator.java
+++ backend/src/main/java/ca/bc/gov/app/validator/contact/ContactEmailExternalValidator.java
@@ -5,33 +5,27 @@ import ca.bc.gov.app.dto.client.ClientContactDto;
 import ca.bc.gov.app.dto.client.ValidationSourceEnum;
 import ca.bc.gov.app.util.ClientValidationUtils;
 import ca.bc.gov.app.validator.ForestClientValidator;
-import java.util.Optional;
 import io.micrometer.observation.annotation.Observed;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Mono;
 
 @Component
 @Observed
 @Slf4j
-public class ContactEmailValidator implements ForestClientValidator<ClientContactDto> {
+public class ContactEmailExternalValidator implements ForestClientValidator<ClientContactDto> {
 
   @Override
   public boolean supports(ValidationSourceEnum source) {
-    return true;
+    return ValidationSourceEnum.EXTERNAL.equals(source);
   }
 
   @Override
   public Mono<ValidationError> validate(ClientContactDto target, Integer index) {
 
     String fieldName = "location.contacts[" + index + "].email";
-    
-    return
-        Mono
-            .justOrEmpty(Optional.ofNullable(target.email()))
-            .filter(StringUtils::isNotBlank)
-            .flatMap(value -> ClientValidationUtils.validatePhoneNumber(fieldName, value));
+
+    return ClientValidationUtils.validateEmail(target.email(), fieldName);
   }
 
 }
