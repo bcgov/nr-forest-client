a3b4015833b3302363522449165b0aa1dfc0075c
maria.martinez@gov.bc.ca <maria.martinez@gov.bc.ca>
2025-11-03 13:29:29 -0800

    no message

--- backend/src/test/java/ca/bc/gov/app/controller/client/ClientSubmissionControllerIntegrationTest.java
+++ backend/src/test/java/ca/bc/gov/app/controller/client/ClientSubmissionControllerIntegrationTest.java
@@ -512,8 +512,9 @@ class ClientSubmissionControllerIntegrationTest
           .mutateWith(csrf())
           .mutateWith(
               mockJwt()
-                  .jwt(jwt -> jwt.claims(
-                      claims -> claims.putAll(TestConstants.getClaims("fakeprovider"))))
+              .jwt(jwt -> jwt.claims(claims -> claims.putAll(
+                      Map.of("userId", "FAKE\\user123")
+                  )))
                   .authorities(new SimpleGrantedAuthority(
                       "ROLE_" + ApplicationConstant.USERTYPE_BCEIDBUSINESS_USER))
           )
