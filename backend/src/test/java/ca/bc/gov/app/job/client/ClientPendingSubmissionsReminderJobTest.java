74211de4352d7b03704097d1e0739e4f4d38c20a
maria.martinez@gov.bc.ca <maria.martinez@gov.bc.ca>
2025-11-18 21:05:41 -0800

    no message

--- backend/src/test/java/ca/bc/gov/app/job/client/ClientPendingSubmissionsReminderJobTest.java
+++ backend/src/test/java/ca/bc/gov/app/job/client/ClientPendingSubmissionsReminderJobTest.java
@@ -5,14 +5,18 @@ import static org.mockito.ArgumentMatchers.isNull;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+import ca.bc.gov.app.configuration.ForestClientConfiguration;
 import ca.bc.gov.app.dto.client.ClientSubmissionDistrictListDto;
+import ca.bc.gov.app.repository.client.SubmissionRepository;
 import ca.bc.gov.app.service.ches.ChesService;
 import ca.bc.gov.app.service.client.ClientSubmissionService;
+import java.time.Duration;
 import java.util.Map;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
@@ -20,63 +24,69 @@ import reactor.test.StepVerifier;
 @DisplayName("Unit Test | ClientPendingSubmissionsReminderJob sendPendingSubmissionEmails")
 public class ClientPendingSubmissionsReminderJobTest {
 
-    private ChesService chesService = mock(ChesService.class);
+  private ChesService chesService = mock(ChesService.class);
+  private ForestClientConfiguration configuration = mock(ForestClientConfiguration.class);
+  private SubmissionRepository submissionRepository = mock(SubmissionRepository.class);
 
-    private ClientSubmissionService service;
+  private ClientSubmissionService service;
 
-    @BeforeEach
-    void setup() {
-      service = new ClientSubmissionService(
-          null, 
-          null, 
-          null, 
-          null, 
-          null, 
-          null, 
-          null, 
-          null,
-          chesService, 
-          null, 
-          null, 
-          null);
-    }
+  @BeforeEach
+  void setup() {
+    service = new ClientSubmissionService(
+        null, 
+        null, 
+        submissionRepository,
+        null, 
+        null, 
+        null, 
+        null, 
+        null, 
+        chesService, 
+        null, 
+        configuration, 
+        null);
+  }
 
-    @Test
-    @DisplayName("Should send email for pending submissions")
-    void shouldSendEmailForPendingSubmissions() {
-      
+  @Test
+  @DisplayName("Should send email for pending submissions")
+  void shouldSendEmailForPendingSubmissions() {
+
       ClientSubmissionDistrictListDto item =
           new ClientSubmissionDistrictListDto(123L, "Test District", "test@example.com");
-      String interval = "7 days";
 
-        when(chesService.sendEmail(
-                eq("pendingSubmission"),
-                eq(item.emails()),
-                eq("New client number application pending for over seven days"),
-                eq(Map.of(
-                    "districtName", item.district(),
-                    "submissionId", item.id(),
-                    "interval", interval
-                )),
-                isNull()
-        )).thenReturn(Mono.just("OK"));
+      when(configuration.getSubmissionLimit()).thenReturn(Duration.ofDays(7));
 
-        service.pendingSubmissions()
-            .as(StepVerifier::create)
-            .expectNext(item)
-            .verifyComplete();
+      when(submissionRepository.retrievePendingSubmissions("7 days"))
+          .thenReturn(Flux.just(item));
 
-        verify(chesService).sendEmail(
-            eq("pendingSubmission"),
-            eq(item.emails()),
-            eq("New client number application pending for over seven days"),
-            eq(Map.of(
-                "districtName", item.district(),
-                "submissionId", item.id(),
-                "interval", interval
-            )),
-            isNull()
-        );
-    }
+      when(chesService.sendEmail(
+              eq("pendingSubmission"),
+              eq(item.emails()),
+              eq("New client number application pending for over seven days"),
+              eq(Map.of(
+                  "districtName", item.district(),
+                  "submissionId", item.id(),
+                  "interval", "7 days"
+              )),
+              isNull()
+      )).thenReturn(Mono.just("OK"));
+
+      service.pendingSubmissions()
+          .as(StepVerifier::create)
+          .expectNext(item)
+          .verifyComplete();
+
+      verify(chesService).sendEmail(
+          eq("pendingSubmission"),
+          eq(item.emails()),
+          eq("New client number application pending for over seven days"),
+          eq(Map.of(
+              "districtName", item.district(),
+              "submissionId", item.id(),
+              "interval", "7 days"
+          )),
+          isNull()
+      );
+  }
     
 }
