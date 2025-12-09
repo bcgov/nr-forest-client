25bf8a203c3a1a8e9a6a0e149336aeb35fc93830
maria.martinez@gov.bc.ca <maria.martinez@gov.bc.ca>
2025-12-09 09:47:37 -0800

    feat: added data clean up script

---
+++ backend/src/main/resources/db/migration/V1__x_data_cleanup.sql
@@ -0,0 +1,7 @@
+delete from nrfc.submission_location_contact_xref;
+delete from nrfc.submission_location;
+delete from nrfc.submission_contact;
+delete from nrfc.submission_matching_detail;
+delete from nrfc.submission_detail;
+delete from nrfc.submission;
+delete from nrfc.email_log;
