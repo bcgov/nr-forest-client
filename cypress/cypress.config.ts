54def25895e2a40af130bbfa692b0cd5b544d824
maria.martinez@gov.bc.ca <maria.martinez@gov.bc.ca>
2026-01-20 20:36:26 -0800

    no message

--- cypress/cypress.config.ts
+++ cypress/cypress.config.ts
@@ -1,20 +1,25 @@
 import { defineConfig } from "cypress";
 import createBundler from "@bahmutov/cypress-esbuild-preprocessor";
-import { addCucumberPreprocessorPlugin } from "@badeball/cypress-cucumber-preprocessor";
-import { createEsbuildPlugin } from "@badeball/cypress-cucumber-preprocessor/esbuild";
+import {
+  addCucumberPreprocessorPlugin,
+} from "@badeball/cypress-cucumber-preprocessor";
+import {
+  createEsbuildPlugin,
+} from "@badeball/cypress-cucumber-preprocessor/esbuild";
 
 async function setupNodeEvents(
   on: Cypress.PluginEvents,
-  config: Cypress.PluginConfigOptions,
+  config: Cypress.PluginConfigOptions
 ): Promise<Cypress.PluginConfigOptions> {
-  // Required for JSON reports, tags, etc.
+
+  // IMPORTANT: must be first
   await addCucumberPreprocessorPlugin(on, config);
 
   on(
     "file:preprocessor",
     createBundler({
       plugins: [createEsbuildPlugin(config)],
-    }),
+    })
   );
 
   return config;
@@ -22,18 +27,10 @@ async function setupNodeEvents(
 
 export default defineConfig({
   e2e: {
-    reporter:
-      require.resolve("@badeball/cypress-cucumber-preprocessor/pretty-reporter"),
     specPattern: "**/*.feature",
     setupNodeEvents,
-    defaultCommandTimeout: 10000,
-    pageLoadTimeout: 60000,
-  },
-  includeShadowDom: true,
-  viewportHeight: 1080,
-  viewportWidth: 1920,
-  retries: {
-    runMode: 3,
-    openMode: 0,
+    reporter: require.resolve(
+      "@badeball/cypress-cucumber-preprocessor/pretty-reporter"
+    ),
   },
 });
