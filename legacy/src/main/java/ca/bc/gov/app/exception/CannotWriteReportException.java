b036e03dde74f47f57682804ae5e8025cbb18366
maria.martinez@gov.bc.ca <maria.martinez@gov.bc.ca>
2026-04-15 10:14:56 -0700

    no message

--- legacy/src/main/java/ca/bc/gov/app/exception/CannotWriteReportException.java
+++ legacy/src/main/java/ca/bc/gov/app/exception/CannotWriteReportException.java
@@ -4,9 +4,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.server.ResponseStatusException;
 
-@ResponseStatus(HttpStatus.UNPROCESSABLE_CONTENT)
+@ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
 public class CannotWriteReportException extends ResponseStatusException {
   public CannotWriteReportException(String errorMessage) {
-    super(HttpStatus.UNPROCESSABLE_CONTENT, errorMessage);
+    super(HttpStatus.UNPROCESSABLE_ENTITY, errorMessage);
   }
 }
