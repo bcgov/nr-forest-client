dd4086a9ff1a61ccdde4277cf0bc326cf8b48356
maria.martinez@gov.bc.ca <maria.martinez@gov.bc.ca>
2026-07-16 10:41:13 -0700

    no message

--- cypress/cypress/support/e2e.ts
+++ cypress/cypress/support/e2e.ts
@@ -16,7 +16,8 @@ Cypress.on('window:before:load', (win) => {
 Cypress.on('uncaught:exception', (err, runnable) => {
   if (
     err.name === 'DOMException' || 
-    err.message.includes('Cannot set property message of [object DOMException]')
+    err.message.includes('Cannot set property message of [object DOMException]') ||
+    err.message.includes('ResizeObserver loop completed with undelivered notifications')
   ) {
     return false;
   }
