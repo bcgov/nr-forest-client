be90e3f58958dab7d45e23fbb02435de92ef6cbc
maria.martinez@gov.bc.ca <maria.martinez@gov.bc.ca>
2026-07-23 18:25:44 -0700

    AbstractTestContainerIntegrationTest

--- backend/src/main/java/ca/bc/gov/app/configuration/ForestClientMetricConfiguration.java
+++ backend/src/main/java/ca/bc/gov/app/configuration/ForestClientMetricConfiguration.java
@@ -8,7 +8,7 @@ import io.micrometer.core.instrument.distribution.DistributionStatisticConfig;
 import io.micrometer.prometheusmetrics.PrometheusMeterRegistry;
 import java.time.Duration;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
+import org.springframework.boot.micrometer.metrics.autoconfigure.MeterRegistryCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
